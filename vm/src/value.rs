d211daa2a72899ff5656542ea36948c1237846c4
Yota Toyama <raviqqe@gmail.com>
2023-07-03 15:33:58 +1000

    Fix

--- vm/src/value.rs
+++ vm/src/value.rs
@@ -3,20 +3,12 @@ use crate::{cons::Cons, number::Number};
 #[allow(dead_code)]
 #[derive(Copy, Clone, Eq, Debug, PartialEq)]
 pub enum Value {
-    Number(Number),
     Cons(Cons),
+    Number(Number),
 }
 
 #[allow(dead_code)]
 impl Value {
-    pub const fn to_number(self) -> Option<Number> {
-        if let Self::Number(x) = self {
-            Some(x)
-        } else {
-            None
-        }
-    }
-
     pub const fn to_reference(self) -> Option<Cons> {
         if let Self::Cons(x) = self {
             Some(x)
@@ -25,11 +17,31 @@ impl Value {
         }
     }
 
-    pub const fn is_number(&self) -> bool {
-        matches!(self, Value::Number(_))
+    pub const fn to_number(self) -> Option<Number> {
+        if let Self::Number(x) = self {
+            Some(x)
+        } else {
+            None
+        }
     }
 
     pub const fn is_cons(&self) -> bool {
         matches!(self, Value::Cons(_))
+    }
+
+    pub const fn is_number(&self) -> bool {
+        matches!(self, Value::Number(_))
+    }
+}
+
+impl From<Cons> for Value {
+    fn from(cons: Cons) -> Self {
+        Self::Cons(cons)
+    }
+}
+
+impl From<Number> for Value {
+    fn from(number: Number) -> Self {
+        Self::Number(number)
     }
 }
