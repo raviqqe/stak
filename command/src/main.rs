b7a7eb809d8cf2f25be98caff4cc82af06b20daf
Yota Toyama <raviqqe@gmail.com>
2023-07-03 14:44:43 +1000

    Fix

--- command/src/main.rs
+++ command/src/main.rs
@@ -1,19 +1,14 @@
-use std::{env::args, fs::read_to_string, process::exit};
+use std::process::exit;
 use vm::{Error, Vm};
 
 fn main() {
-    let input = match read_to_string(args().nth(1).expect("command line argument")) {
-        Ok(input) => input,
-        Err(error) => {
-            eprintln!("{}", error);
-            return;
+    if let Err(error) = Vm::new(b"").run() {
+        match error {
+            Error::ArgumentCount => todo!(),
+            Error::IllegalInstruction => todo!(),
+            Error::IllegalPrimitive => todo!(),
         }
-    };
 
-    if let Err(error) = Vm::new(input.trim().as_bytes()).run() {
-        exit(match error {
-            Error::IllegalInstruction | Error::IllegalPrimitive => 6,
-            _ => 1,
-        })
+        exit(1)
     }
 }
