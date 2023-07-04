230c181f731f717410313144ff2d8a1fe079fff2
Yota Toyama <raviqqe@gmail.com>
2023-07-04 16:25:06 +1000

    Fix

--- vm/src/instruction.rs
+++ vm/src/instruction.rs
@@ -8,9 +8,9 @@ pub enum Instruction {
 }
 
 impl Instruction {
-    pub const APPLY: u64 = Self::Apply as u64;
-    pub const SET: u64 = Self::Set as u64;
-    pub const GET: u64 = Self::Get as u64;
-    pub const CONSTANT: u64 = Self::Constant as u64;
-    pub const IF: u64 = Self::If as u64;
+    pub const APPLY: u8 = Self::Apply as u8;
+    pub const SET: u8 = Self::Set as u8;
+    pub const GET: u8 = Self::Get as u8;
+    pub const CONSTANT: u8 = Self::Constant as u8;
+    pub const IF: u8 = Self::If as u8;
 }
