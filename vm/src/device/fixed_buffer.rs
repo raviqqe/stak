0580417b826faf285e87e5f12f8828204e7bbbb2
Yota Toyama <raviqqe@gmail.com>
2023-07-04 20:07:41 +1000

    Fix

--- vm/src/device/fixed_buffer.rs
+++ vm/src/device/fixed_buffer.rs
@@ -19,6 +19,12 @@ impl<const I: usize, const O: usize> FixedBufferDevice<I, O> {
     }
 }
 
+impl<const I: usize, const O: usize> Default for FixedBufferDevice<I, O> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl<const I: usize, const O: usize> Device for FixedBufferDevice<I, O> {
     type Error = ();
 
@@ -33,7 +39,9 @@ impl<const I: usize, const O: usize> Device for FixedBufferDevice<I, O> {
     }
 
     fn write(&mut self, byte: u8) -> Result<(), Self::Error> {
-        let Some(output) = self.output.get_mut(self.output_index) else { return Err(()) };
+        let Some(output) = self.output.get_mut(self.output_index) else {
+            return Err(());
+        };
 
         *output = byte;
         self.output_index += 1;
