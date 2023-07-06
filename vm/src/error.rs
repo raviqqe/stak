925b462264b89680ff314379fe2d1759b988845c
Yota Toyama <raviqqe@gmail.com>
2023-07-06 23:04:28 +1000

    Refactor (#29)

--- vm/src/error.rs
+++ vm/src/error.rs
@@ -1,3 +1,5 @@
+use core::fmt::{self, Display, Formatter};
+
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Error {
     ArgumentCount,
@@ -9,4 +11,20 @@ pub enum Error {
     NumberExpected,
     OutOfMemory,
     StackUnderflow,
+}
+
+impl Display for Error {
+    fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
+        match self {
+            Self::ArgumentCount => write!(formatter, "invalid argument count"),
+            Self::ConsExpected => write!(formatter, "cons expected"),
+            Self::EndOfInput => write!(formatter, "unexpected end of input"),
+            Self::IllegalInstruction => write!(formatter, "illegal instruction"),
+            Self::IllegalPrimitive => write!(formatter, "illegal primitive"),
+            Self::MissingOperand => write!(formatter, "missing operand"),
+            Self::NumberExpected => write!(formatter, "number expected"),
+            Self::OutOfMemory => write!(formatter, "out of memory"),
+            Self::StackUnderflow => write!(formatter, "stack underflow"),
+        }
+    }
 }
