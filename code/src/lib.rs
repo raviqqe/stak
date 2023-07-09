d098a524f27fcfb2bd65ecb3941a7020d5d47a05
Yota Toyama <raviqqe@gmail.com>
2023-07-10 00:09:01 +1000

    Fix

--- code/src/lib.rs
+++ code/src/lib.rs
@@ -1,15 +1,26 @@
 #![no_std]
 
+#[cfg(feature = "alloc")]
 extern crate alloc;
 #[cfg(test)]
 extern crate std;
 
+#[cfg(feature = "alloc")]
 mod decode;
+#[cfg(feature = "alloc")]
 mod encode;
+#[cfg(feature = "alloc")]
 mod error;
+#[cfg(feature = "alloc")]
 mod ir;
 
+#[cfg(feature = "alloc")]
 pub use decode::decode;
-pub use encode::{encode, INTEGER_BASE};
+#[cfg(feature = "alloc")]
+pub use encode::encode;
+#[cfg(feature = "alloc")]
 pub use error::Error;
+#[cfg(feature = "alloc")]
 pub use ir::*;
+
+pub const INTEGER_BASE: u64 = i8::MAX as u64 + 1;
