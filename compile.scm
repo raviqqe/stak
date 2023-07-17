625baeb3d3a2a055b97ca9b0c923a2435e9a1eb1
Yota Toyama <raviqqe@gmail.com>
2023-07-17 14:58:50 +1000

    Fix

--- compile.scm
+++ compile.scm
@@ -1,7 +1,17 @@
 #!/usr/bin/env gsi
 
+(define (read-all)
+  (let ((x (read)))
+    (if (eof-object? x)
+      '()
+      (cons x (read-all)))))
+
 (define (compile source)
-  (let ((x read ()))
-    foo))
+  (cond
+    ((null? source) (display "compile end"))
+    ((list? (car source)) (display #f))
+    (else (compile (cdr source)))))
+
+(compile (read-all))
 
 (write-u8 65)
