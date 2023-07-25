cefecc70fafaf4d61cc7ae884221acf5c29cc0ee
Yota Toyama <raviqqe@gmail.com>
2023-07-25 18:18:29 +1000

    Fix constant instruction compilation (#79)

--- compile.scm
+++ compile.scm
@@ -141,7 +141,10 @@
 
 (define (compile-constant constant continuation)
   (cond
-    ((or (boolean? constant) (null? constant) (number? constant))
+    ((or (boolean? constant) (null? constant))
+      (rib get-instruction constant continuation))
+
+    ((number? constant)
       (rib constant-instruction constant continuation))
 
     ((string? constant)
