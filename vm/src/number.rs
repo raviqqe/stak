8f06b84149a89ad1850d0bdd529a5381be3c9f8b
Yota Toyama <raviqqe@gmail.com>
2023-07-30 20:59:29 +1000

    Fix

--- vm/src/number.rs
+++ vm/src/number.rs
@@ -22,6 +22,10 @@ impl TryFrom<Value> for Number {
     type Error = Error;
 
     fn try_from(value: Value) -> Result<Self, Self::Error> {
+        #[cfg(feature = "boost")]
+
+        #[cfg(not(feature = "boost"))]
+        value.to_number().ok_or(Error::NumberExpected)
         value.to_number().ok_or(Error::NumberExpected)
     }
 }
