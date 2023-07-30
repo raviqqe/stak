8477b415e078c801bf5c5926b2251ea6b9313f6d
Yota Toyama <raviqqe@gmail.com>
2023-07-30 22:10:34 +1000

    Fix

--- tools/bench.sh
+++ tools/bench.sh
@@ -2,12 +2,18 @@
 
 set -ex
 
-. $(dirname $0)/utility.sh
+build_feature() {
+  cargo build --release --features $feature
+  cp target/release/stak target/release/stak-$feature
+}
 
 brew install gambit-scheme
 cargo install hyperfine
 
-cargo build --release $(feature_flags)
+build_feature boost
+build_feature gc_always
+
+cargo build --release
 
 (
   cd $(dirname $0)/..
@@ -20,6 +26,8 @@ cargo build --release $(feature_flags)
     hyperfine \
       --sort command \
       "target/release/stak $base.out" \
+      "target/release/stak-boost $base.out" \
+      "target/release/stak-gc_always $base.out" \
       "gsi $file" \
       "python3 $base.py"
   done
