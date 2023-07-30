3bd3b84438334d8841b40248423672f306e48f4a
Yota Toyama <raviqqe@gmail.com>
2023-07-30 16:13:08 +1000

    Fix

--- tools/bench.sh
+++ tools/bench.sh
@@ -5,8 +5,12 @@ set -ex
 cargo install hyperfine
 
 for file in $(find bench -type f -name '*.scm'); do
-  target_file=${file%.scm}.out
+  base=${file%.scm}
 
-  cat prelude.scm $file | tools/compile.sh >$target_file
-  hyperfine "target/release/stak $target_file"
+  cat prelude.scm $file | tools/compile.sh >$base.out
+
+  hyperfine \
+    "target/release/stak $base.out" \
+    "gsi $file" \
+    "python3 $base.py"
 done
