cc84968137a617ac5569a4277efcafcbd4a2b202
Yota Toyama <raviqqe@gmail.com>
2023-08-01 09:55:59 +1000

    Fix

--- tools/bench.sh
+++ tools/bench.sh
@@ -27,7 +27,7 @@ cargo build --release
       "target/release/stak $base.out" \
       "target/release/stak-boost $base.out" \
       "gsi $file" \
-      "csi $file" \
+      "csi -s $file" \
       "python3 $base.py"
   done
 )
