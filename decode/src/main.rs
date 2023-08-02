e739ac331510f6fbc8f259cde00847c2593a24b5
Yota Toyama <raviqqe@gmail.com>
2023-08-03 08:46:24 +1000

    Add decode crate

---
+++ decode/src/main.rs
@@ -0,0 +1,24 @@
+use device::StdioDevice;
+use std::{env::args, error::Error, fs::read, process::exit};
+use vm::Vm;
+
+// TODO Make this 1 << 9 by default.
+const HEAP_SIZE: usize = 1 << 17;
+
+fn main() {
+    if let Err(error) = run() {
+        eprintln!("{}", error);
+        exit(1);
+    }
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let mut vm = Vm::<HEAP_SIZE, StdioDevice>::new(Default::default())?;
+
+    vm.initialize(read(args().nth(1).ok_or(format!(
+        "Usage: {} <bytecode_file>",
+        args().next().expect("command name")
+    ))?)?)?;
+
+    Ok(vm.run()?)
+}
