f8ecbb151d5a307ca1240d143f0f7b1f82b5592e
Yota Toyama <raviqqe@gmail.com>
2023-08-03 19:55:10 +1000

    Run petite scheme (#134)

--- tools/bench.sh
+++ tools/bench.sh
@@ -3,8 +3,13 @@
 set -ex
 
 brew install chicken gambit-scheme
-cargo install hyperfine
 
+if [ $(uname -m) = x86_64 ]; then
+  # spell-checker: disable-next-line
+  brew install chezscheme
+fi
+
+cargo install hyperfine
 cargo build --release
 
 (
@@ -15,11 +20,12 @@ cargo build --release
 
     cat prelude.scm $file | ./main.scm >$base.out
 
-    hyperfine \
-      --sort command \
-      "target/release/stak $base.out" \
-      "gsi $file" \
-      "python3 $base.py" \
-      "csi -s $file"
+    scripts="target/release/stak $base.out,gsi $file,python3 $base.py,csi -s $file"
+
+    if which petite; then
+      scripts="$scripts,petite --script $file"
+    fi
+
+    hyperfine --sort command -L script "$scripts" "{script}"
   done
 )
