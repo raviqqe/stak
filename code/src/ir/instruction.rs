9ee4d67f47efb2a3e983085c1b4d6c47d74d7050
Yota Toyama <raviqqe@gmail.com>
2023-08-13 16:50:59 +1000

    Fix

--- code/src/ir/instruction.rs
+++ code/src/ir/instruction.rs
@@ -17,22 +17,6 @@ pub enum Instruction {
 }
 
 impl Instruction {
-    pub fn body(&self) -> &[Instruction] {
-        match self {
-            Self::Call(operand) => write!(formatter, "call {}", operand),
-            Self::Set(operand) => write!(formatter, "set {}", operand),
-            Self::Get(operand) => write!(formatter, "get {}", operand),
-            Self::Constant(operand) => write!(formatter, "constant {}", operand),
-            #[cfg(feature = "alloc")]
-            Self::If(instructions) => write!(formatter, "if"),
-            #[cfg(feature = "alloc")]
-            Self::Closure(arity, instructions) => writeln!(formatter, "closure {}", arity),
-            Self::Skip(count) => write!(formatter, "skip {}", count),
-        }
-    }
-}
-
-impl Instruction {
     pub const CALL: u8 = 0;
     pub const SET: u8 = 1;
     pub const GET: u8 = 2;
@@ -42,18 +26,48 @@ impl Instruction {
     pub const SKIP: u8 = 6;
 }
 
-impl Display for Instruction {
+pub struct DisplayInstruction<'a> {
+    instruction: &'a Instruction,
+    indent: usize,
+}
+
+impl<'a> DisplayInstruction<'a> {
+    pub fn new(instruction: &'a Instruction, indent: usize) -> Self {
+        Self {
+            instruction,
+            indent,
+        }
+    }
+}
+
+impl<'a> Display for DisplayInstruction<'a> {
     fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
-        match self {
-            Self::Call(operand) => write!(formatter, "call {}", operand),
-            Self::Set(operand) => write!(formatter, "set {}", operand),
-            Self::Get(operand) => write!(formatter, "get {}", operand),
-            Self::Constant(operand) => write!(formatter, "constant {}", operand),
+        let indent = self.indent + 1;
+
+        match self.instruction {
+            Instruction::Call(operand) => write!(formatter, "call {}", operand),
+            Instruction::Set(operand) => write!(formatter, "set {}", operand),
+            Instruction::Get(operand) => write!(formatter, "get {}", operand),
+            Instruction::Constant(operand) => write!(formatter, "constant {}", operand),
             #[cfg(feature = "alloc")]
-            Self::If(instructions) => write!(formatter, "if"),
+            Instruction::If(instructions) => {
+                writeln!(formatter, "if")?;
+                write!(
+                    formatter,
+                    "{}",
+                    DisplayInstructionList::new(instructions, indent)
+                )
+            }
             #[cfg(feature = "alloc")]
-            Self::Closure(arity, instructions) => writeln!(formatter, "closure {}", arity),
-            Self::Skip(count) => write!(formatter, "skip {}", count),
+            Instruction::Closure(arity, instructions) => {
+                writeln!(formatter, "closure {}", arity)?;
+                write!(
+                    formatter,
+                    "{}",
+                    DisplayInstructionList::new(instructions, indent)
+                )
+            }
+            Instruction::Skip(count) => write!(formatter, "skip {}", count),
         }
     }
 }
@@ -75,7 +89,11 @@ impl<'a> DisplayInstructionList<'a> {
 impl<'a> Display for DisplayInstructionList<'a> {
     fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
         for (index, instruction) in self.instructions.iter().enumerate() {
-            writeln!(formatter, "{}", instruction)?;
+            writeln!(
+                formatter,
+                "{}",
+                DisplayInstruction::new(instruction, self.indent)
+            )?;
 
             if index < self.instructions.len() - 1 {
                 writeln!(formatter)?;
