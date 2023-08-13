c2a6581bede5ee093ed10cb8fe7f493f54e1b5d7
Yota Toyama <raviqqe@gmail.com>
2023-08-13 19:09:07 +1000

    Fix

--- vm/src/vm.rs
+++ vm/src/vm.rs
@@ -147,7 +147,7 @@ impl<const N: usize, T: Device> Vm<N, T> {
                             } else {
                                 *self.car_mut(self.cons) = self.cdr(self.program_counter);
                                 *self.cdr_mut(self.cons) = self.cdr(last_argument);
-                                // TODO
+                                // TODO Fix GC boundary.
                                 self.cons(self.cons.into(), NULL)?
                             };
                             *self.cdr_mut(last_argument) = frame.into();
