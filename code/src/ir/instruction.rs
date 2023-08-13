49c5d40469198470c5b6ab1c5dd41fb3d474c66d
Yota Toyama <raviqqe@gmail.com>
2023-08-13 17:20:24 +1000

    Code visualizer (#175)

--- code/src/ir/instruction.rs
+++ code/src/ir/instruction.rs
@@ -1,6 +1,7 @@
 use crate::Operand;
 #[cfg(feature = "alloc")]
 use alloc::vec::Vec;
+use core::fmt::{self, Display, Formatter};
 
 #[derive(Debug, Eq, PartialEq)]
 pub enum Instruction {
@@ -23,4 +24,84 @@ impl Instruction {
     pub const IF: u8 = 4;
     pub const CLOSURE: u8 = 5;
     pub const SKIP: u8 = 6;
+}
+
+pub(crate) struct DisplayInstruction<'a> {
+    instruction: &'a Instruction,
+    indent: usize,
+}
+
+impl<'a> DisplayInstruction<'a> {
+    pub fn new(instruction: &'a Instruction, indent: usize) -> Self {
+        Self {
+            instruction,
+            indent,
+        }
+    }
+}
+
+impl<'a> Display for DisplayInstruction<'a> {
+    fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
+        let indent = self.indent + 1;
+
+        match self.instruction {
+            Instruction::Call(operand) => write!(formatter, "call {}", operand),
+            Instruction::Set(operand) => write!(formatter, "set {}", operand),
+            Instruction::Get(operand) => write!(formatter, "get {}", operand),
+            Instruction::Constant(operand) => write!(formatter, "constant {}", operand),
+            #[cfg(feature = "alloc")]
+            Instruction::If(instructions) => {
+                write!(formatter, "if")?;
+                write!(
+                    formatter,
+                    "{}",
+                    DisplayInstructionList::new(instructions, indent)
+                )
+            }
+            #[cfg(feature = "alloc")]
+            Instruction::Closure(arity, instructions) => {
+                write!(formatter, "closure {}", arity)?;
+                write!(
+                    formatter,
+                    "{}",
+                    DisplayInstructionList::new(instructions, indent)
+                )
+            }
+            Instruction::Skip(count) => write!(formatter, "skip {}", count),
+        }
+    }
+}
+
+pub(crate) struct DisplayInstructionList<'a> {
+    instructions: &'a [Instruction],
+    indent: usize,
+}
+
+impl<'a> DisplayInstructionList<'a> {
+    pub fn new(instructions: &'a [Instruction], indent: usize) -> Self {
+        Self {
+            instructions,
+            indent,
+        }
+    }
+}
+
+impl<'a> Display for DisplayInstructionList<'a> {
+    fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
+        for instruction in self.instructions {
+            writeln!(formatter)?;
+
+            for _ in 0..self.indent {
+                write!(formatter, "  ")?
+            }
+
+            write!(
+                formatter,
+                "{}",
+                DisplayInstruction::new(instruction, self.indent)
+            )?;
+        }
+
+        Ok(())
+    }
 }
