51d08b69b3c4073c65f6916881c064c229e798f9
Yota Toyama <raviqqe@gmail.com>
2023-08-28 18:17:59 +1000

    Fix

--- prelude.scm
+++ prelude.scm
@@ -2,6 +2,8 @@
 ;
 ; Those syntax definitions are copied from https://small.r7rs.org/attachment/r7rs.pdf.
 
+;; Conditionals
+
 (define-syntax cond
   (syntax-rules (else =>)
     ((cond (else result1 result2 ...))
@@ -29,6 +31,71 @@
       (if test
         (begin result1 result2 ...)
         (cond clause1 clause2 ...)))))
+
+(define-syntax case
+  (syntax-rules (else =>)
+    ((case (key ...)
+        clauses
+        ...)
+      (let ((atom-key (key ...)))
+        (case atom-key clauses ...)))
+    ((case key
+        (else => result))
+      (result key))
+    ((case key
+        (else result1 result2 ...))
+      (begin result1 result2 ...))
+    ((case key
+        ((atoms ...) result1 result2 ...))
+      (if (memv key '(atoms ...))
+        (begin result1 result2 ...)))
+    ((case key
+        ((atoms ...) => result))
+      (if (memv key '(atoms ...))
+        (result key)))
+    ((case key
+        ((atoms ...) => result)
+        clause
+        clauses
+        ...)
+      (if (memv key '(atoms ...))
+        (result key)
+        (case key clause clauses ...)))
+    ((case key
+        ((atoms ...) result1 result2 ...)
+        clause
+        clauses
+        ...)
+      (if (memv key '(atoms ...))
+        (begin result1 result2 ...)
+        (case key clause clauses ...)))))
+
+(define-syntax and
+  (syntax-rules ()
+    ((and) #t)
+    ((and test) test)
+    ((and test1 test2 ...)
+      (if test1 (and test2 ...) #f))))
+
+(define-syntax or
+  (syntax-rules ()
+    ((or) #f)
+    ((or test) test)
+    ((or test1 test2 ...)
+      (let ((x test1))
+        (if x x (or test2 ...))))))
+
+(define-syntax when
+  (syntax-rules ()
+    ((when test result1 result2 ...)
+      (if test
+        (begin result1 result2 ...)))))
+
+(define-syntax unless
+  (syntax-rules ()
+    ((unless test result1 result2 ...)
+      (if (not test)
+        (begin result1 result2 ...)))))
 
 ; Type IDs
 
