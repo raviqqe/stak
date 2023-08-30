029e89488347e6aa1fd3f2361c43fcd592e40c7c
Yota Toyama <raviqqe@gmail.com>
2023-08-30 13:22:24 +1000

    Fix letrec

--- prelude.scm
+++ prelude.scm
@@ -35,35 +35,10 @@
 
 (define-syntax letrec
   (syntax-rules ()
-    ((letrec ((var1 init1) ...) body ...)
-      (letrec "generate temp names"
-        (var1 ...)
-        ()
-        ((var1 init1) ...)
-        body
-        ...))
-    ((letrec "generate temp names"
-        ()
-        (temp1 ...)
-        ((var1 init1) ...)
-        body
-        ...)
-      (let ((var1 #f) ...)
-        (let ((temp1 init1) ...)
-          (set! var1 temp1)
-          ...
-          body
-          ...)))
-    ((letrec "generate temp names"
-        (x y ...)
-        (temp ...)
-        ((var1 init1) ...)
-        body
-        ...)
-      (letrec "generate temp names"
-        (y ...)
-        (newtemp temp ...)
-        ((var1 init1) ...)
+    ((letrec ((x y) ...) body ...)
+      (let ((x #f) ...)
+        (set! x y)
+        ...
         body
         ...))))
 
