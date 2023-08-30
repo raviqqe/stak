7a36e0fa8d83b8801696c40aafc52801a27f5133
Yota Toyama <raviqqe@gmail.com>
2023-08-30 13:16:52 +0930

    Refactor sytnax rules (#224)

--- prelude.scm
+++ prelude.scm
@@ -1,43 +1,37 @@
 ; Syntax
 ;
-; Those syntax definitions are mostly copied from https://small.r7rs.org/attachment/r7rs.pdf.
+; Those syntax definitions are mostly ported from https://small.r7rs.org/attachment/r7rs.pdf.
 
 ;; Binding
 
 (define-syntax let
   (syntax-rules ()
-    ((let ((name val) ...) body1 body2 ...)
-      ((lambda (name ...) body1 body2 ...)
-        val
-        ...))
-    ((let tag ((name val) ...) body1 body2 ...)
-      ((letrec ((tag (lambda (name ...)
-                body1
-                body2
-                ...)))
-          tag)
-        val
+    ((_ ((name value) ...) body1 body2 ...)
+      ((lambda (name ...) body1 body2 ...) value ...))
+    ((_ tag ((name value) ...) body1 body2 ...)
+      ((letrec ((tag (lambda (name ...) body1 body2 ...))) tag)
+        value
         ...))))
 
 (define-syntax let*
   (syntax-rules ()
-    ((let* () body1 body2 ...)
+    ((_ () body1 body2 ...)
       (let () body1 body2 ...))
-    ((let* ((name1 val1) (name2 val2) ...)
+    ((_ ((name1 value1) (name2 value2) ...)
         body1
         body2
         ...)
-      (let ((name1 val1))
-        (let* ((name2 val2) ...)
+      (let ((name1 value1))
+        (let* ((name2 value2) ...)
           body1
           body2
           ...)))))
 
 (define-syntax letrec
   (syntax-rules ()
-    ((_ ((x y) ...) body ...)
-      (let ((x #f) ...)
-        (set! x y)
+    ((_ ((name value) ...) body ...)
+      (let ((name #f) ...)
+        (set! name value)
         ...
         body
         ...))))
@@ -46,25 +40,25 @@
 
 (define-syntax cond
   (syntax-rules (else =>)
-    ((cond (else result1 result2 ...))
+    ((_ (else result1 result2 ...))
       (begin result1 result2 ...))
-    ((cond (test => result))
+    ((_ (test => result))
       (let ((temp test))
         (if temp (result temp))))
-    ((cond (test => result) clause1 clause2 ...)
+    ((_ (test => result) clause1 clause2 ...)
       (let ((temp test))
         (if temp
           (result temp)
           (cond clause1 clause2 ...))))
-    ((cond (test)) test)
-    ((cond (test) clause1 clause2 ...)
+    ((_ (test)) test)
+    ((_ (test) clause1 clause2 ...)
       (let ((temp test))
         (if temp
           temp
           (cond clause1 clause2 ...))))
-    ((cond (test result1 result2 ...))
+    ((_ (test result1 result2 ...))
       (if test (begin result1 result2 ...)))
-    ((cond (test result1 result2 ...)
+    ((_ (test result1 result2 ...)
         clause1
         clause2
         ...)
@@ -74,68 +68,67 @@
 
 (define-syntax case
   (syntax-rules (else =>)
-    ((case (key ...)
-        clauses
+    ((_ (key ...)
+        clause
         ...)
       (let ((atom-key (key ...)))
-        (case atom-key clauses ...)))
-    ((case key
+        (case atom-key clause ...)))
+    ((_ key
         (else => result))
       (result key))
-    ((case key
+    ((_ key
         (else result1 result2 ...))
       (begin result1 result2 ...))
-    ((case key
+    ((_ key
         ((atoms ...) result1 result2 ...))
       (if (memv key '(atoms ...))
         (begin result1 result2 ...)))
-    ((case key
+    ((_ key
         ((atoms ...) => result))
       (if (memv key '(atoms ...))
         (result key)))
-    ((case key
+    ((_ key
         ((atoms ...) => result)
-        clause
-        clauses
+        clause1
+        clause2
         ...)
       (if (memv key '(atoms ...))
         (result key)
-        (case key clause clauses ...)))
-    ((case key
+        (case key clause1 clause2 ...)))
+    ((_ key
         ((atoms ...) result1 result2 ...)
-        clause
-        clauses
+        clause1
+        clause2
         ...)
       (if (memv key '(atoms ...))
         (begin result1 result2 ...)
-        (case key clause clauses ...)))))
+        (case key clause1 clause2 ...)))))
 
 (define-syntax and
   (syntax-rules ()
-    ((and) #t)
-    ((and test) test)
-    ((and test1 test2 ...)
+    ((_) #t)
+    ((_ test) test)
+    ((_ test1 test2 ...)
       (if test1 (and test2 ...) #f))))
 
 (define-syntax or
   (syntax-rules ()
-    ((or) #f)
-    ((or test) test)
-    ((or test1 test2 ...)
+    ((_) #f)
+    ((_ test) test)
+    ((_ test1 test2 ...)
       (let ((x test1))
         (if x x (or test2 ...))))))
 
 (define-syntax when
   (syntax-rules ()
-    ((when test result1 result2 ...)
+    ((_ test result1 result2 ...)
       (if test
         (begin result1 result2 ...)))))
 
 (define-syntax unless
   (syntax-rules ()
-    ((unless test result1 result2 ...)
-      (if (not test)
-        (begin result1 result2 ...)))))
+    ((_ test result1 result2 ...)
+      (when (not test) result1 result2 ...))))
 
 ; Type IDs
 
