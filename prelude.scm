1c249fe5c7466e1b180c094cc07d23da97e289f1
Yota Toyama <raviqqe@gmail.com>
2023-08-30 13:40:30 +1000

    Fix

--- prelude.scm
+++ prelude.scm
@@ -6,18 +6,18 @@
 
 (define-syntax let
   (syntax-rules ()
-    ((let ((name value) ...) body1 body2 ...)
+    ((_ ((name value) ...) body1 body2 ...)
       ((lambda (name ...) body1 body2 ...) value ...))
-    ((let tag ((name value) ...) body1 body2 ...)
+    ((_ tag ((name value) ...) body1 body2 ...)
       ((letrec ((tag (lambda (name ...) body1 body2 ...))) tag)
         value
         ...))))
 
 (define-syntax let*
   (syntax-rules ()
-    ((let* () body1 body2 ...)
+    ((_ () body1 body2 ...)
       (let () body1 body2 ...))
-    ((let* ((name1 value1) (name2 value2) ...)
+    ((_ ((name1 value1) (name2 value2) ...)
         body1
         body2
         ...)
@@ -40,25 +40,25 @@
 
 (define-syntax cond
   (syntax-rules (else =>)
-    ((cond (else result1 result2 ...))
+    ((_ (else result1 result2 ...))
       (begin result1 result2 ...))
-    ((cond (test => result))
+    ((_ (test => result))
       (let ((temp test))
         (if temp (result temp))))
-    ((cond (test => result) clause1 clause2 ...)
+    ((_ (test => result) clause1 clause2 ...)
       (let ((temp test))
         (if temp
           (result temp)
           (cond clause1 clause2 ...))))
-    ((cond (test)) test)
-    ((cond (test) clause1 clause2 ...)
+    ((_ (test)) test)
+    ((_ (test) clause1 clause2 ...)
       (let ((temp test))
         (if temp
           temp
           (cond clause1 clause2 ...))))
-    ((cond (test result1 result2 ...))
+    ((_ (test result1 result2 ...))
       (if test (begin result1 result2 ...)))
-    ((cond (test result1 result2 ...)
+    ((_ (test result1 result2 ...)
         clause1
         clause2
         ...)
@@ -68,26 +68,26 @@
 
 (define-syntax case
   (syntax-rules (else =>)
-    ((case (key ...)
+    ((_ (key ...)
         clause
         ...)
       (let ((atom-key (key ...)))
         (case atom-key clause ...)))
-    ((case key
+    ((_ key
         (else => result))
       (result key))
-    ((case key
+    ((_ key
         (else result1 result2 ...))
       (begin result1 result2 ...))
-    ((case key
+    ((_ key
         ((atoms ...) result1 result2 ...))
       (if (memv key '(atoms ...))
         (begin result1 result2 ...)))
-    ((case key
+    ((_ key
         ((atoms ...) => result))
       (if (memv key '(atoms ...))
         (result key)))
-    ((case key
+    ((_ key
         ((atoms ...) => result)
         clause1
         clause2
@@ -95,7 +95,7 @@
       (if (memv key '(atoms ...))
         (result key)
         (case key clause1 clause2 ...)))
-    ((case key
+    ((_ key
         ((atoms ...) result1 result2 ...)
         clause1
         clause2
@@ -106,28 +106,28 @@
 
 (define-syntax and
   (syntax-rules ()
-    ((and) #t)
-    ((and test) test)
-    ((and test1 test2 ...)
+    ((_) #t)
+    ((_ test) test)
+    ((_ test1 test2 ...)
       (if test1 (and test2 ...) #f))))
 
 (define-syntax or
   (syntax-rules ()
-    ((or) #f)
-    ((or test) test)
-    ((or test1 test2 ...)
+    ((_) #f)
+    ((_ test) test)
+    ((_ test1 test2 ...)
       (let ((x test1))
         (if x x (or test2 ...))))))
 
 (define-syntax when
   (syntax-rules ()
-    ((when test result1 result2 ...)
+    ((_ test result1 result2 ...)
       (if test
         (begin result1 result2 ...)))))
 
 (define-syntax unless
   (syntax-rules ()
-    ((unless test result1 result2 ...)
+    ((_ test result1 result2 ...)
       (when (not test) result1 result2 ...))))
 
 ; Type IDs
