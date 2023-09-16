bfef5b9e28603421f193b0d534f132a58b2d44cd
Yota Toyama <raviqqe@gmail.com>
2023-09-16 15:28:27 +1000

    Refactor

--- tools/integration_test.sh
+++ tools/integration_test.sh
@@ -4,13 +4,15 @@ set -ex
 
 . $(dirname $0)/utility.sh
 
+cd $(dirname $0)/..
+
 brew install gambit-scheme gauche
 bundler install
 
 cargo build $(feature_flags)
 
 export PATH=$PWD/target/debug:$PWD/tools:$PATH
-export ROOT=$PWD
+export STAK_ROOT=$PWD
 
 (
   cd $(dirname $0)/..
