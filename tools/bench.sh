c68276ad0a1d631ef2790d092ef5ecc19d92c0ea
Yota Toyama <raviqqe@gmail.com>
2023-09-19 23:07:33 +1000

    Refactor

--- tools/bench.sh
+++ tools/bench.sh
@@ -7,19 +7,17 @@ brew install gambit-scheme gauche
 cargo install hyperfine
 cargo build --release
 
-(
-  cd $(dirname $0)/..
+cd $(dirname $0)/..
 
-  for file in $(find bench -type f -name '*.scm' | sort); do
-    base=${file%.scm}
+for file in $(find bench -type f -name '*.scm' | sort); do
+  base=${file%.scm}
 
-    cat prelude.scm $file | ./compile.scm >$base.out
+  cat prelude.scm $file | ./compile.scm >$base.out
 
-    scripts="target/release/stak $base.out,gsi $file,python3 $base.py,gosh $file"
+  scripts="target/release/stak $base.out,gsi $file,python3 $base.py,gosh $file"
 
-    hyperfine \
-      --sort command \
-      -L script "target/release/stak $base.out,gsi $file,python3 $base.py,gosh $file" \
-      "{script}"
-  done
-)
+  hyperfine \
+    --sort command \
+    -L script "target/release/stak $base.out,gsi $file,python3 $base.py,gosh $file" \
+    "{script}"
+done
