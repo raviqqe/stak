e1559b1fe34dc39500d9af60f70afa495e2020ff
Yota Toyama <raviqqe@gmail.com>
2023-09-21 17:35:41 +1000

    Benchmark Chibi Scheme

--- tools/bench.sh
+++ tools/bench.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-brew install gambit-scheme gauche
+brew install chibi-scheme gambit-scheme gauche
 
 cargo install hyperfine
 cargo build --release
@@ -14,7 +14,7 @@ for file in $(find bench -type f -name '*.scm' | sort); do
 
   cat prelude.scm $file | ./compile.scm >$base.out
 
-  scripts="target/release/stak $base.out,gsi $file,python3 $base.py,gosh $file"
+  scripts="chibi-scheme $file,target/release/stak $base.out,gsi $file,python3 $base.py,gosh $file"
 
   hyperfine \
     --sort command \
