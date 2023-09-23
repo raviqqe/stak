a43c061e4c3f779ff10f56876755f42d001e6504
Yota Toyama <raviqqe@gmail.com>
2023-09-23 13:11:17 +1000

    Fix

--- prelude.scm
+++ prelude.scm
@@ -15,6 +15,11 @@
     ((_ name value)
       ($$define name value))))
 
+(define-syntax define-syntax
+  (syntax-rules ()
+    ((_ name value)
+      ($$define-syntax name value))))
+
 (define-syntax lambda
   (syntax-rules (define define-syntax)
     ((_ arguments (define content ...) body1 body2 ...)
