a0b487e72923a0782e7e7bd734e544e712459940
Yota Toyama <raviqqe@gmail.com>
2023-09-26 17:29:59 +1000

    Fix

--- compile.scm
+++ compile.scm
@@ -451,7 +451,7 @@
               (name (denote-parameter use-context template))
               (denotation (resolve-denotation definition-context template)))
             (when (denotation? denotation)
-              ; TODO Test if this is really hygine.
+              ; TODO Test if this is really hygiene.
               ; It looks like this destructive update of contexts is fine because
               ; we always generate new names. But I'm not sure...
               ; For example, how about this?
