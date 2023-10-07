2e7a362c3c8d0ff067562291e4a173dfd2a3e185
Yota Toyama <raviqqe@gmail.com>
2023-10-07 23:02:10 +1100

    Use record type for ribs (#368)

--- compile.scm
+++ compile.scm
@@ -12,28 +12,17 @@
 
 (cond-expand
   ((or chibi gauche guile)
-    (define (rib tag car cdr)
-      (cons (cons (cons '$$rib tag) car) cdr))
+    (define-record-type rib*
+      (make-rib tag car cdr)
+      rib?
+      (tag rib-tag)
+      (car rib-car rib-set-car!)
+      (cdr rib-cdr rib-set-cdr!))
+
+    (define rib make-rib)
 
     (define (rib-cons car cdr)
-      (cons (cons (cons '$$rib 0) car) cdr))
-
-    (define rib-tag cdaar)
-    (define rib-car cdar)
-    (define rib-cdr cdr)
-
-    (define (rib-set-car! rib car)
-      (set-cdr! (car rib) car))
-
-    (define (rib-set-cdr! rib cdr)
-      (set-cdr! rib cdr))
-
-    (define (rib? value)
-      (and
-        (pair? value)
-        (pair? (car value))
-        (pair? (caar value))
-        (eqv? (caaar value) '$$rib))))
+      (rib 0 car cdr)))
 
   (else))
 
