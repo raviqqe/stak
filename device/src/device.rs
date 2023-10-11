f0fc8072f76ba956fc58a8ff4b5c371abd37ed8e
Yota Toyama <raviqqe@gmail.com>
2023-10-11 19:25:51 +1100

    Fix

--- device/src/device.rs
+++ device/src/device.rs
@@ -3,8 +3,5 @@ pub trait Device {
 
     fn read(&mut self) -> Result<u8, Self::Error>;
     fn write(&mut self, byte: u8) -> Result<(), Self::Error>;
-
-    fn write_error(&mut self, byte: u8) -> Result<(), Self::Error> {
-        self.write(byte)
-    }
+    fn write_error(&mut self, byte: u8) -> Result<(), Self::Error>;
 }
