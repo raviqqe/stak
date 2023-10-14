501acf31f4caf5240b3330619eed3661c34d3ae9
Yota Toyama <raviqqe@gmail.com>
2023-10-14 13:53:23 +1100

    Fix

--- prelude.scm
+++ prelude.scm
@@ -1418,6 +1418,43 @@
                       (lambda ()
                         (apply values args)))))))))))))
 
+(define-syntax guard-aux
+  (syntax-rules (else =>)
+    ((guard-aux reraise (else result1 result2 ...))
+      (begin result1 result2 ...))
+    ((guard-aux reraise (test => result))
+      (let ((temp test))
+        (if temp
+          (result temp)
+          reraise)))
+    ((guard-aux reraise (test => result)
+        clause1
+        clause2
+        ...)
+      (let ((temp test))
+        (if temp
+          (result temp)
+          (guard-aux reraise clause1 clause2 ...))))
+    ((guard-aux reraise (test))
+      (or test reraise))
+    ((guard-aux reraise (test) clause1 clause2 ...)
+      (let ((temp test))
+        (if temp
+          temp
+          (guard-aux reraise clause1 clause2 ...))))
+    ((guard-aux reraise (test result1 result2 ...))
+      (if test
+        (begin result1 result2 ...)
+        reraise))
+    ((guard-aux reraise
+        (test result1 result2 ...)
+        clause1
+        clause2
+        ...)
+      (if test
+        (begin result1 result2 ...)
+        (guard-aux reraise clause1 clause2 ...)))))
+
 ;; Unwind
 
 (define unwind #f)
