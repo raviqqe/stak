8c5eb4a772e585d5eb33e63e88930cd66b080a9a
Yota Toyama <raviqqe@gmail.com>
2023-10-15 13:09:58 +1100

    Filter bench

--- tools/bench.sh
+++ tools/bench.sh
@@ -9,7 +9,13 @@ cargo build --release
 
 cd $(dirname $0)/..
 
-for file in $(find bench -type f -name '*.scm' | sort); do
+filter=.
+
+if [ $# -gt 0 ]; then
+  filter="$@"
+fi
+
+for file in $(find bench -type f -name '*.scm' | sort | grep $filter); do
   base=${file%.scm}
 
   cat prelude.scm $file | gosh compile.scm >$base.out
