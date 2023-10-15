76c142e8140e58aad3931ce2ed6a48b468120543
Yota Toyama <raviqqe@gmail.com>
2023-10-15 20:40:57 +1100

    Add `no_inline` feature in `vm` crate (#427)

--- vm/src/vm.rs
+++ vm/src/vm.rs
@@ -75,6 +75,7 @@ impl<'a, T: Device> Vm<'a, T> {
         }
     }
 
+    #[cfg_attr(feature = "no_inline", inline(never))]
     pub fn run(&mut self) -> Result<(), Error> {
         while self.program_counter != NULL {
             let instruction = self.cdr(self.program_counter).assume_cons();
@@ -211,6 +212,7 @@ impl<'a, T: Device> Vm<'a, T> {
         }
     }
 
+    #[cfg_attr(feature = "no_inline", inline(never))]
     fn advance_program_counter(&mut self) {
         self.program_counter = self.cdr(self.program_counter).assume_cons();
 
@@ -305,6 +307,7 @@ impl<'a, T: Device> Vm<'a, T> {
         *self.car_mut(self.stack) = value;
     }
 
+    #[cfg_attr(feature = "no_inline", inline(never))]
     fn allocate(&mut self, car: Value, cdr: Value) -> Result<Cons, Error> {
         let mut cons = self.allocate_raw(car, cdr)?;
 
@@ -325,6 +328,7 @@ impl<'a, T: Device> Vm<'a, T> {
         Ok(cons)
     }
 
+    #[cfg_attr(feature = "no_inline", inline(never))]
     fn allocate_raw(&mut self, car: Value, cdr: Value) -> Result<Cons, Error> {
         if self.is_out_of_memory() {
             return Err(Error::OutOfMemory);
@@ -401,6 +405,7 @@ impl<'a, T: Device> Vm<'a, T> {
 
     // Primitive operations
 
+    #[cfg_attr(feature = "no_inline", inline(never))]
     fn operate_primitive(&mut self, primitive: u8) -> Result<(), Error> {
         trace!("primitive", primitive);
 
@@ -555,6 +560,7 @@ impl<'a, T: Device> Vm<'a, T> {
 
     // Garbage collection
 
+    #[cfg_attr(feature = "no_inline", inline(never))]
     fn collect_garbages(&mut self, cons: Option<&mut Cons>) -> Result<(), Error> {
         self.allocation_index = 0;
         self.space = !self.space;
@@ -605,6 +611,7 @@ impl<'a, T: Device> Vm<'a, T> {
 
     // Initialization
 
+    #[cfg_attr(feature = "no_inline", inline(never))]
     pub fn initialize(&mut self, input: impl IntoIterator<Item = u8>) -> Result<(), Error> {
         let mut input = input.into_iter();
 
@@ -627,6 +634,7 @@ impl<'a, T: Device> Vm<'a, T> {
         Ok(())
     }
 
+    #[cfg_attr(feature = "no_inline", inline(never))]
     fn decode_symbols(&mut self, input: &mut impl Iterator<Item = u8>) -> Result<(), Error> {
         for _ in 0..Self::decode_integer(input).ok_or(Error::MissingInteger)? {
             let symbol = self.create_symbol(NULL, 0)?;
@@ -695,6 +703,7 @@ impl<'a, T: Device> Vm<'a, T> {
         self.push(symbol.into())
     }
 
+    #[cfg_attr(feature = "no_inline", inline(never))]
     fn decode_instructions(&mut self, input: &mut impl Iterator<Item = u8>) -> Result<(), Error> {
         while let Some((instruction, r#return, integer)) = self.decode_instruction(input)? {
             trace!("instruction", instruction);
