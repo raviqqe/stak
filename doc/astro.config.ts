6cf8e80b5b6ce74765d6f499b89dd653a8968264
Yota Toyama <raviqqe@gmail.com>
2023-10-16 13:00:44 +1100

    Fix

--- doc/astro.config.ts
+++ doc/astro.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
+import starlight from "@astrojs/starlight";
 
 export default defineConfig({
   base: "/stak",
@@ -8,6 +9,28 @@ export default defineConfig({
     service: { entrypoint: "astro/assets/services/sharp" },
     remotePatterns: [{ protocol: "https" }],
   },
-  integrations: [prefetch({ selector: "a", intentSelector: "a" }), sitemap()],
+  integrations: [
+    prefetch({ selector: "a", intentSelector: "a" }),
+    sitemap(),
+    starlight({
+      title: "My Docs",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
   site: "https://raviqqe.github.io/stak",
 });
