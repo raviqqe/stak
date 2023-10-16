c8734677c1a7b9ccbf91d12bdcc02fbd7f0632ec
Yota Toyama <raviqqe@gmail.com>
2023-10-16 14:48:49 +1100

    Refactor

--- doc/astro.config.ts
+++ doc/astro.config.ts
@@ -2,12 +2,6 @@ import { defineConfig } from "astro/config";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import starlight from "@astrojs/starlight";
-import { readFile } from "node:fs/promises";
-import { join, parse, relative } from "node:path";
-import { glob } from "glob";
-import { sortBy } from "lodash";
-
-const exampleDirectory = "src/content/docs/examples";
 
 export default defineConfig({
   base: "/stak",
@@ -23,35 +17,6 @@ export default defineConfig({
       social: {
         github: "https://github.com/raviqqe/stak",
       },
-      sidebar: [
-        {
-          label: "Home",
-          link: "/",
-        },
-        {
-          label: "Examples",
-          items: sortBy(
-            await Promise.all(
-              (await glob(join(exampleDirectory, "/**/*.md"))).map(
-                async (path) => {
-                  const parsed = parse(relative(exampleDirectory, path));
-
-                  return {
-                    label:
-                      (await readFile(path, "utf-8"))
-                        .split("\n")
-                        .find((line) => line.startsWith("title: "))
-                        ?.replace("title: ", "")
-                        .trim() ?? "",
-                    link: join("examples", parsed.dir, parsed.name),
-                  };
-                },
-              ),
-            ),
-            "label",
-          ),
-        },
-      ],
     }),
   ],
   site: "https://raviqqe.github.io/stak",
