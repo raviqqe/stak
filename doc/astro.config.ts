68be4919d32e35658047531512a0d89593c4f135
Yota Toyama <raviqqe@gmail.com>
2023-10-16 12:38:12 +1100

    Fix

--- doc/astro.config.ts
+++ doc/astro.config.ts
@@ -1,3 +1,13 @@
 import { defineConfig } from "astro/config";
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
 
-export default defineConfig({});
+export default defineConfig({
+  base: "/stak",
+  image: {
+    service: { entrypoint: "astro/assets/services/sharp" },
+    remotePatterns: [{ protocol: "https" }],
+  },
+  integrations: [prefetch({ selector: "a", intentSelector: "a" }), sitemap()],
+  site: "https://raviqqe.github.io/stak",
+});
