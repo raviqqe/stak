983455383af6d3e4400e809cee64c00298e42b9a
Yota Toyama <raviqqe@gmail.com>
2023-10-16 23:08:38 +1100

    Add icon (#442)
    - Add icon
    - Fix

--- doc/astro.config.ts
+++ doc/astro.config.ts
@@ -51,6 +51,10 @@ export default defineConfig({
     sitemap(),
     starlight({
       title: "Stak",
+      favicon: "/icon.svg",
+      logo: {
+        src: "./public/icon.svg",
+      },
       social: {
         github: "https://github.com/raviqqe/stak",
       },
