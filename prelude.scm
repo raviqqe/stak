11942a013252938bc5b1fb55626c1be5ec04149a
Yota Toyama <raviqqe@gmail.com>
2023-11-23 12:53:37 +0900

    Define modulo

--- prelude.scm
+++ prelude.scm
@@ -528,6 +528,15 @@
 (define quotient (inverse-arithmetic-operator $$/ 1))
 (define / quotient)
 
+(define (modulo x y)
+  (let ((q (quotient x y)))
+    (let ((r (- x (* y q))))
+      (if (eqv? r 0)
+        0
+        (if (eqv? (< x 0) (< y 0))
+          r
+          (+ r y))))))
+
 (define (comparison-operator f)
   (lambda xs
     (if (null? xs)
