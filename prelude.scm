514d0f87ca919c20ebe4286b9d45661fb18b2fef
Yota Toyama <raviqqe@gmail.com>
2023-11-25 20:14:03 +0900

    Fix

--- prelude.scm
+++ prelude.scm
@@ -864,12 +864,15 @@
 (define symbol->string rib-cdr)
 
 (define (string->symbol x)
-  (let ((pair (member x symbol-table (lambda (x y) (equal? x (symbol->string y))))))
-    (if pair
-      (car pair)
-      (let ((x (rib #f (string-append x) symbol-type)))
-        (set! symbol-table (cons x symbol-table))
-        x))))
+  ; TODO Remove this hack.
+  (if (equal? x "$$rib")
+    '$$rib
+    (let ((pair (member x symbol-table (lambda (x y) (equal? x (symbol->string y))))))
+      (if pair
+        (car pair)
+        (let ((x (rib #f (string-append x) symbol-type)))
+          (set! symbol-table (cons x symbol-table))
+          x)))))
 
 ;; Vector
 
