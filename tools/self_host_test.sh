6b697561b2bab2ef54336cf67b3f95cfd6eb68c9
Yota Toyama <raviqqe@gmail.com>
2023-11-25 20:42:19 +0900

    Fix

--- tools/self_host_test.sh
+++ tools/self_host_test.sh
@@ -2,18 +2,33 @@
 
 set -e
 
+directory=$(dirname $0)/..
+
+alias stak=$directory/target/release/stak
+alias decode=$directory/target/release/stak-decode
+
 run() {
-  tools/compile.sh ./compile.scm >compile.out
-  cargo run --release --bin stak compile.out <$1 >main.out
-  cargo run --release --bin stak main.out
+  gosh ./compile.scm <$1 >$2
+  decode $2 >$3
+}
+
+run_self() {
+  stak compile.out <$1 >$2
+  decode $2 >$3
 }
 
 cd $(dirname $0)/..
 
+mkdir -p tmp
 brew install gauche
+tools/compile.sh ./compile.scm >compile.out
 
 set -x
 
 for file in test/self_host/*.scm; do
-  run $file
+  run $file tmp/other.out tmp/other.txt
+  run_self $file tmp/self.out tmp/self.txt
+
+  diff tmp/other.txt tmp/self.txt
+  diff tmp/other.out tmp/self.out
 done
