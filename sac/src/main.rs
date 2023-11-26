414de2abcda878ee28bcd69d944a789853f299ae
Yota Toyama <raviqqe@gmail.com>
2023-11-26 23:00:13 +0900

    Refactor (#508)

--- sac/src/main.rs
+++ sac/src/main.rs
@@ -1,6 +1,6 @@
 use device::StdioDevice;
 use std::{env, error::Error, process::exit};
-use vm::{Number, Vm};
+use vm::Vm;
 
 const DEFAULT_HEAP_SIZE: usize = 1 << 21;
 
@@ -17,7 +17,7 @@ fn run() -> Result<(), Box<dyn Error>> {
         .map(|string| string.parse())
         .transpose()?
         .unwrap_or(DEFAULT_HEAP_SIZE);
-    let mut heap = vec![Number::new(0).into(); size];
+    let mut heap = vec![Default::default(); size];
     let mut vm = Vm::<StdioDevice>::new(&mut heap, Default::default());
 
     vm.initialize(include_bytes!(env!("STAK_BYTECODE_FILE")).iter().copied())?;
