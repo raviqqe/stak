3b1c41b3e61c298498af4034ff9a30b46f8afa8e
Yota Toyama <raviqqe@gmail.com>
2023-12-03 17:46:02 +0900

    Fix

--- vm/src/error.rs
+++ vm/src/error.rs
@@ -24,18 +24,14 @@ impl<E: Display> Display for Error<E> {
             Self::ArgumentCount => write!(formatter, "invalid argument count"),
             Self::ConsExpected => write!(formatter, "cons expected"),
             Self::EndOfInput => write!(formatter, "unexpected end of input"),
-            Self::Halt => write!(formatter, "halt"),
             Self::IllegalInstruction => write!(formatter, "illegal instruction"),
-            Self::IllegalPrimitive => write!(formatter, "illegal primitive"),
             Self::MissingInteger => write!(formatter, "missing integer"),
             Self::MissingOperand => write!(formatter, "missing operand"),
             Self::NumberExpected => write!(formatter, "number expected"),
             Self::OutOfMemory => write!(formatter, "out of memory"),
+            Self::Primitive(error) => write!(formatter, "{}", { error }),
             Self::ProcedureExpected => write!(formatter, "procedure expected"),
-            Self::ReadInput => write!(formatter, "failed to read input"),
             Self::StackUnderflow => write!(formatter, "stack underflow"),
-            Self::WriteError => write!(formatter, "failed to write error"),
-            Self::WriteOutput => write!(formatter, "failed to write output"),
         }
     }
 }
