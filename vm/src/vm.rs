d46c9fa2bf1349c0481e9dfe5fef8edf63556a2a
Yota Toyama <raviqqe@gmail.com>
2023-12-31 16:44:03 +0900

    Refactor

--- vm/src/vm.rs
+++ vm/src/vm.rs
@@ -617,7 +617,7 @@ impl<'a, T: PrimitiveSet> Vm<'a, T> {
             }
         }
 
-        let rib = self.allocate(self.boolean(false).into(), Number::default().into())?;
+        let rib = self.allocate(self.null().into(), Number::default().into())?;
 
         self.initialize_symbol(rib.into())?;
         self.initialize_symbol(self.null().into())?;
