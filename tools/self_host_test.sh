565d1e53448d92a323a15d7806af112cb26f1d48
Yota Toyama <raviqqe@gmail.com>
2024-01-02 16:49:04 +0800

    Use custom profile for integration tests (#595)

--- tools/self_host_test.sh
+++ tools/self_host_test.sh
@@ -27,11 +27,11 @@ artifact_path() {
 
 cd $(dirname $0)/..
 
-export PATH=$(dirname $0)/../target/debug:$PATH
+export PATH=$(dirname $0)/../target/integration_test:$PATH
 
 mkdir -p tmp
 brew install gauche
-cargo build
+cargo build --profile integration_test
 
 for stage in $(seq $(expr $stage_count - 1)); do
   cat prelude.scm compile.scm | run_stage$stage >stage$(expr $stage + 1).out
