40948f1966c1c51253ddeb601add927ab53e4787
Yota Toyama <raviqqe@gmail.com>
2024-01-04 16:35:59 +0900

    Fix

--- primitive/src/lib.rs
+++ primitive/src/lib.rs
@@ -1,3 +1,10 @@
+//! Primitive sets.
+//!
+//! We equip virtual machines of [`vm::Vm`] with primitive sets that provide small primitive functionalities,
+//! such as I/O, arithmetic, and so on.
+//!
+//! This crate provides primitive sets necessary to run basic programs in Scheme.
+
 #![no_std]
 
 #[cfg(feature = "std")]
