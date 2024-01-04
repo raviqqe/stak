371bcbb169a066975f2a7130c70111014d316b9a
Yota Toyama <raviqqe@gmail.com>
2024-01-04 16:19:53 +0900

    Fix

--- cmd/compile/src/main.rs
+++ cmd/compile/src/main.rs
@@ -1,1 +1,9 @@
+//! A command to compile a source file in Scheme into bytecodes for a virtual machine.
+//!
+//! # Usage
+//!
+//! ```sh
+//! stak-decode < foo.scm > foo.bc
+//! ```
+
 sac::main!(std::env!("STAK_BYTECODE_FILE"));
