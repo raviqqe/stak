ffb8184ca9b8ef1750420345708e2082d90530e3
Yota Toyama <raviqqe@gmail.com>
2024-01-05 16:51:27 +0900

    Fix

--- device/src/lib.rs
+++ device/src/lib.rs
@@ -7,12 +7,14 @@ extern crate std;
 
 mod device;
 mod fixed_buffer;
+#[cfg(feature = "std")]
 mod read_write;
 #[cfg(feature = "std")]
 mod stdio;
 
 pub use device::Device;
 pub use fixed_buffer::FixedBufferDevice;
+#[cfg(feature = "std")]
 pub use read_write::ReadWriteDevice;
 #[cfg(feature = "std")]
 pub use stdio::StdioDevice;
