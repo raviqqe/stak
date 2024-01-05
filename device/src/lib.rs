128683354357a2fc17bd9310c84e95a887f4fcca
Yota Toyama <raviqqe@gmail.com>
2024-01-05 16:50:49 +0800

    `ReadWriteDevice` (#614)

--- device/src/lib.rs
+++ device/src/lib.rs
@@ -2,15 +2,21 @@
 
 #![no_std]
 
+#[cfg(test)]
+extern crate alloc;
 #[cfg(feature = "std")]
 extern crate std;
 
 mod device;
 mod fixed_buffer;
 #[cfg(feature = "std")]
+mod read_write;
+#[cfg(feature = "std")]
 mod stdio;
 
 pub use device::Device;
 pub use fixed_buffer::FixedBufferDevice;
+#[cfg(feature = "std")]
+pub use read_write::ReadWriteDevice;
 #[cfg(feature = "std")]
 pub use stdio::StdioDevice;
