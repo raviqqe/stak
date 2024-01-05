468c4ed57175a7d866d8eb49fec2b12c9035e57d
Yota Toyama <raviqqe@gmail.com>
2024-01-05 18:56:00 +0900

    Fix

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -10,7 +10,21 @@ use vm::Vm;
 const HEAP_SIZE: usize = 1 << 18;
 const COMPILER_BYTECODES: &[u8] = include_bytes!(std::env!("STAK_BYTECODE_FILE"));
 
-/// Compiles a program in Scheme into bytecodes with no standard library but only built-ins.
+/// Compiles a program in R7RS Scheme into bytecodes.
+///
+/// # Examples
+///
+/// ```rust
+/// let bytecodes = stak_macro::r7rs!("(define x 42)");
+/// ```
+#[proc_macro]
+pub fn r7rs(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as LitStr);
+
+    convert_result(generate_scheme(&input.value()))
+}
+
+/// Compiles a program in Scheme into bytecodes with only built-ins but no standard library.
 ///
 /// # Examples
 ///
