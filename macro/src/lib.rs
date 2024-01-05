e694bdcf22a96356f46e6ce84bf7e0eb6d1aaa77
Yota Toyama <raviqqe@gmail.com>
2024-01-05 16:29:47 +0900

    Fix

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -1,7 +1,15 @@
+use device::FixedBufferDevice;
+use primitive::SmallPrimitiveSet;
 use proc_macro::TokenStream;
+use proc_macro2::Literal;
+use quote::quote;
+use std::error::Error;
 use syn::{parse_macro_input, LitStr};
+use vm::Vm;
 
-const bytecodes: &[u8] = include_bytes!(std::env!("STAK_BYTECODE_FILE"));
+const HEAP_SIZE: usize = 1 << 20;
+const BUFFER_SIZE: usize = 1 << 20;
+const COMPILER_BYTECODES: &[u8] = include_bytes!(std::env!("STAK_BYTECODE_FILE"));
 
 /// Compiles a program in Scheme into bytecodes.
 ///
@@ -14,5 +22,27 @@ const bytecodes: &[u8] = include_bytes!(std::env!("STAK_BYTECODE_FILE"));
 pub fn scheme(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as LitStr);
 
-    todo!()
+    convert_result(run(input))
+}
+
+fn run(string: LitStr) -> Result<TokenStream, Box<dyn Error>> {
+    let mut heap = vec![Default::default(); HEAP_SIZE];
+    let device = FixedBufferDevice::<BUFFER_SIZE, BUFFER_SIZE, 0>::new();
+    let mut vm = Vm::new(&mut heap, SmallPrimitiveSet::new(device))?;
+
+    vm.initialize(COMPILER_BYTECODES.iter().copied())?;
+
+    vm.run()?;
+
+    let bytecodes = Literal::byte_string(vm.primitive_set().device().output());
+
+    Ok(quote! { #bytecodes }.into())
+}
+
+fn convert_result(result: Result<TokenStream, Box<dyn Error>>) -> TokenStream {
+    result.unwrap_or_else(|error| {
+        let message = error.to_string();
+
+        quote! { compile_error!(#message) }.into()
+    })
 }
