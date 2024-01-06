98114a9d468e9672f04f2fee0895ccde6cf7a132
Yota Toyama <raviqqe@gmail.com>
2024-01-06 14:24:24 +0900

    Fix

--- cmd/stak/src/main.rs
+++ cmd/stak/src/main.rs
@@ -7,11 +7,16 @@
 //! stak foo.scm
 //! ```
 
-use stak_device::StdioDevice;
+use stak_device::{ReadWriteDevice, StdioDevice};
 use stak_macro::include_r7rs;
 use stak_primitive::SmallPrimitiveSet;
 use stak_vm::Vm;
-use std::{env, error::Error};
+use std::{
+    env,
+    error::Error,
+    fs::{read_to_string, File},
+    io::{empty, Read},
+};
 
 const DEFAULT_HEAP_SIZE: usize = 1 << 21;
 const COMPILER_PROGRAM: &[u8] = include_r7rs!("compile.scm");
@@ -22,10 +27,36 @@ fn main() -> Result<(), Box<dyn Error>> {
         .map(|string| string.parse())
         .transpose()?
         .unwrap_or(DEFAULT_HEAP_SIZE);
+
+    let mut source = Default::default();
+    let mut program = vec![];
+
+    read_source(source)?;
+    compile(&source, &mut program, size)?;
+
     let mut heap = vec![Default::default(); size];
     let mut vm = Vm::new(&mut heap, SmallPrimitiveSet::new(StdioDevice::new()))?;
 
     vm.initialize(COMPILER_PROGRAM.iter().copied())?;
 
+    Ok(vm.run()?)
+}
+
+fn read_source(source: &mut String) -> Result<(), io::Error> {
+    for argument in env::args().skip(1) {
+        File::open(argument)?.read_to_string(source)?;
+    }
+
+    Ok(())
+}
+
+fn compile(source: &str, target: &mut Vec<u8>, heap_size: usize) -> Result<(), Box<dyn Error>> {
+    let mut heap = vec![Default::default(); heap_size];
+    let mut vm = Vm::new(
+        &mut heap,
+        SmallPrimitiveSet::new(ReadWriteDevice::new(source.as_bytes(), target, empty())),
+    )?;
+
+    vm.initialize(COMPILER_PROGRAM.iter().copied())?;
     Ok(vm.run()?)
 }
