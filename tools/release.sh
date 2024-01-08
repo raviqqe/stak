ac1ee4e0a76a59bc4862cd20d577bb91a5125180
Yota Toyama <raviqqe@gmail.com>
2024-01-08 23:16:49 +0900

    Fix

--- tools/release.sh
+++ tools/release.sh
@@ -2,9 +2,27 @@
 
 set -e
 
+dry_run=false
+
+while getopts d option; do
+  case $option in
+  d)
+    dry_run=true
+    ;;
+  esac
+done
+
+shift $(expr $OPTIND - 1)
+
 for path in $(git ls-files '**/Cargo.toml'); do
   (
     cd $(dirname $path)
+
+    if $dry_run; then
+      cargo_options=--dry-run
+    else
+
+    fi
 
     if git diff origin/main -- Cargo.toml | grep '^\+version = '; then
       cargo publish "$@"
