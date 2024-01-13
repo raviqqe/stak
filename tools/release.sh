42ebf67989f3d89d4ec9477a3f92c7ed4fde5ddf
Yota Toyama <raviqqe@gmail.com>
2024-01-13 23:24:20 +0900

    Fix

--- tools/release.sh
+++ tools/release.sh
@@ -2,7 +2,11 @@
 
 set -e
 
+if [ $# -ne 0 ]; then
+  exit 1
+fi
+
 cargo install cargo-workspaces
 
 cat prelude.scm compile.scm | stak compile.scm >compiler/src/compile.bc
-cargo workspaces publish -y --from-git "$@"
+cargo workspaces publish -y --from-git
