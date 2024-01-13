759c163da647fd2ed0c847f94730fbe95d53074b
Yota Toyama <raviqqe@gmail.com>
2024-01-13 14:01:21 +0800

    Fix dependency versions (#664)

--- tools/release.sh
+++ tools/release.sh
@@ -2,33 +2,7 @@
 
 set -e
 
-dry_run=false
-
-while getopts d option; do
-  case $option in
-  d)
-    dry_run=true
-    ;;
-  esac
-done
-
-shift $(expr $OPTIND - 1)
+cargo install cargo-release
 
 cat prelude.scm compile.scm | stak compile.scm >compiler/src/compile.bc
-
-for path in $(git ls-files '**/Cargo.toml'); do
-  (
-    cd $(dirname $path)
-
-    if $dry_run; then
-      reference=origin/main
-      cargo_options=--dry-run
-    else
-      reference=HEAD^
-    fi
-
-    if git diff $reference -- Cargo.toml | grep '^-version = '; then
-      cargo publish $cargo_options
-    fi
-  )
-done
+cargo release publish "$@"
