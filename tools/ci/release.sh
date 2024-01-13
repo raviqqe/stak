83a02070634d116399f0d0fc244206d6212ac8b2
Yota Toyama <raviqqe@gmail.com>
2024-01-13 22:46:47 +0800

    Use cargo-workspaces (#671)

---
+++ tools/ci/release.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -ex
+
+if [ $# -ne 0 ]; then
+  exit 1
+fi
+
+cargo install cargo-workspaces
+
+bytecode_file=compiler/src/compile.bc
+
+cat prelude.scm compile.scm | stak compile.scm >$bytecode_file
+
+git config user.email action@github.com
+git config user.name 'GitHub Action'
+git add -f $bytecode_file
+git commit -m "Update bytecode file"
+
+cargo workspaces publish -y --from-git
--- tools/release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-cargo install cargo-release
-
-cat prelude.scm compile.scm | stak compile.scm >compiler/src/compile.bc
-cargo release publish --no-confirm "$@"
