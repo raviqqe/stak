632c4cee0ebb91f77d8afa3e6c35f6e36a8080da
Yota Toyama <raviqqe@gmail.com>
2024-01-20 17:29:32 +0900

    Add documentation to types (#701)

--- vm/src/type.rs
+++ vm/src/type.rs
@@ -1,14 +1,23 @@
 /// A type in Scheme.
 #[derive(Clone, Copy, Debug, Default, Eq, PartialEq)]
 pub enum Type {
+    /// A pair.
     #[default]
     Pair,
+    /// A null.
     Null,
+    /// A boolean.
     Boolean,
+    /// A procedure.
     Procedure,
+    /// A symbol.
     Symbol,
+    /// A string.
     String,
+    /// A character.
     Character,
+    /// A vector.
     Vector,
+    /// A byte vector.
     ByteVector,
 }
