5fd6379d77d90170ac79031f9664770f7aaf2e17
Yota Toyama <raviqqe@gmail.com>
2024-02-10 15:34:38 +0900

    Test stage0 (#753)

--- tools/self_host_test.sh
+++ tools/self_host_test.sh
@@ -9,16 +9,12 @@ log() {
   "$@"
 }
 
-run_stage1() {
-  stak-compile
-}
-
-run_stage2() {
-  stak-interpret stage2.bc
-}
-
-run_stage3() {
-  stak-interpret stage3.bc
+run_stage() {
+  if [ $1 -eq 0 ]; then
+    log stak compile.scm
+  else
+    log $target/stak-interpret stage$1.bc
+  fi
 }
 
 artifact_path() {
@@ -27,27 +23,26 @@ artifact_path() {
 
 cd $(dirname $0)/..
 
-export PATH=$(dirname $0)/../target/release_test:$PATH
+target=target/release_test
 
 mkdir -p tmp
-brew install gauche
 cargo build --profile release_test
 
-for stage in $(seq $(expr $stage_count - 1)); do
-  cat prelude.scm compile.scm | run_stage$stage >stage$(expr $stage + 1).bc
+for stage in $(seq 0 $(expr $stage_count - 1)); do
+  cat prelude.scm compile.scm | run_stage $stage >stage$(expr $stage + 1).bc
 done
 
 for file in bench/*/main.scm compile.scm; do
   echo '>>>' $file
 
-  for stage in $(seq $stage_count); do
+  for stage in $(seq 0 $stage_count); do
     bytecode_file=$(artifact_path $stage bc)
 
-    cat prelude.scm $file | log run_stage$stage >$bytecode_file
-    stak-decode <$bytecode_file >${bytecode_file%.*}.md
+    cat prelude.scm $file | run_stage $stage >$bytecode_file
+    $target/stak-decode <$bytecode_file >${bytecode_file%.*}.md
   done
 
-  for stage in $(seq $(expr $stage_count - 1)); do
+  for stage in $(seq 0 $(expr $stage_count - 1)); do
     for extension in md bc; do
       log diff $(artifact_path $stage $extension) $(artifact_path $(expr $stage + 1) $extension)
     done
