efa62ea6e61809aea6fa4cddf867e3e2236442a1
Yota Toyama <raviqqe@gmail.com>
2024-03-05 20:56:15 +1100

    Remove empty symbols (#829)
    ```
    > hyperfine '~/worktree/ef6115a43fa7bb85/target/release/stak ~/foo.scm' 'target/release/stak ~/foo.scm'
    Benchmark 1: ~/worktree/ef6115a43fa7bb85/target/release/stak ~/foo.scm
      Time (mean ± σ):      2.130 s ±  0.006 s    [User: 2.115 s, System: 0.004 s]
      Range (min … max):    2.125 s …  2.145 s    10 runs
    
    Benchmark 2: target/release/stak ~/foo.scm
      Time (mean ± σ):      1.985 s ±  0.011 s    [User: 1.969 s, System: 0.004 s]
      Range (min … max):    1.978 s …  2.016 s    10 runs
    
      Warning: Statistical outliers were detected. Consider re-running this benchmark on a quiet system without any interferences from other programs. It might help to use the '--warmup' or '--prepare' options.
    
    Summary
      target/release/stak ~/foo.scm ran
        1.07 ± 0.01 times faster than ~/worktree/ef6115a43fa7bb85/target/release/stak ~/foo.scm
    ```

--- compile.scm
+++ compile.scm
@@ -12,13 +12,7 @@
   (stak
     (define cons-rib cons)
     (define target-pair? pair?)
-    (define target-procedure? procedure?)
-
-    ; Internal symbols must be discoverable with their string representations.
-    (set-car! '$$false "$$false")
-    (set-car! '$$true "$$true")
-    (set-car! '$$null "$$null")
-    (set-car! '$$rib "$$rib"))
+    (define target-procedure? procedure?))
 
   (else
     (define-record-type *rib*
@@ -45,12 +39,21 @@
 
 ; Constants
 
+; We need to generate those symbols from strings to match them with the ones
+; from a `read` procedure.
+(define rib-symbol (string->symbol "$$rib"))
+
 (define default-constants
-  '((#f . $$false)
-    (#t . $$true)
-    (() . $$null)
-    ; It is fine to have a key duplicate with `false`'s because it is never hit.
-    (#f . $$rib)))
+  (map
+    (lambda (pair)
+      (cons
+        (car pair)
+        (string->symbol (cdr pair))))
+    '((#f . "$$false")
+      (#t . "$$true")
+      (() . "$$null")
+      ; It is fine to have a key duplicate with `false`'s because it is never hit.
+      (#f . "$$rib"))))
 
 (define default-symbols (map cdr default-constants))
 
@@ -856,11 +859,10 @@
       (($$cons $$-)
         2)
 
-      (($$rib)
-        4)
-
       (else
-        (error "unknown primitive" name)))
+        (if (eq? name rib-symbol)
+          4
+          (error "unknown primitive" name))))
     name
     continuation))
 
@@ -1063,7 +1065,7 @@
           (code-rib
             constant-instruction
             0
-            (compile-primitive-call '$$rib (continue)))))))
+            (compile-primitive-call rib-symbol (continue)))))))
 
   (let ((symbol (constant-context-constant context constant)))
     (if symbol
@@ -1370,7 +1372,7 @@
           constant-instruction
           0
           (compile-primitive-call
-            '$$rib
+            rib-symbol
             (code-rib set-instruction (car primitive) continuation)))))))
 
 (define (build-primitives primitives continuation)
