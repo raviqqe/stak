fb49324f7be827bf86a74c118563ed5317fd1d7b
Yota Toyama <raviqqe@gmail.com>
2024-03-14 20:09:17 +0900

    Fix

--- aa-tree.scm
+++ aa-tree.scm
@@ -6,13 +6,32 @@
   (node aa-tree-node aa-tree-set-node!)
   (size aa-tree-size aa-tree-set-size!))
 
+(define-record-type aa-tree-node
+  (make-aa-tree-node value left right)
+  aa-tree-node?
+  (value aa-tree-node-value aa-tree-node-set-value!)
+  (left aa-tree-left-left aa-tree-left-set-left!)
+  (right aa-tree-right-right aa-tree-right-set-right!))
+
 (define (empty)
   (make-aa-tree #f 0))
 
+(define (left node)
+  ; TODO
+  node)
+
 (define (skew tree)
   (let ((node (aa-tree-node tree)))
-    (and
-      node
-      foo)))
+    (cond
+      ((not node)
+        #f)
+
+      ((not (left node))
+        node)
+
+      ()
+
+      (else
+        node))))
 
 (write (empty))
