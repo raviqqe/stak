cb7408c6cf2544df60807e0a671638abed1a1d02
Yota Toyama <raviqqe@gmail.com>
2024-03-14 20:00:48 +0900

    Fix

---
+++ aa-tree.scm
@@ -0,0 +1,7 @@
+(import (scheme base) (scheme write))
+
+(define-record-type aa-tree
+  (make-aa-tree node size)
+  aa-tree?
+  (node aa-tree-node aa-tree-set-node!)
+  (size aa-tree-size aa-tree-set-size!))
