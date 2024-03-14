01f7a54dc3d3508f6f5aa18682c20c6a44312eac
Yota Toyama <raviqqe@gmail.com>
2024-03-14 20:13:44 +0900

    Fix

--- aa-tree.scm
+++ aa-tree.scm
@@ -1,18 +1,20 @@
 (import (scheme base) (scheme write))
 
 (define-record-type aa-tree
-  (make-aa-tree node size)
+  (make-aa-tree root size)
   aa-tree?
-  (node aa-tree-node aa-tree-set-node!)
+  (root aa-tree-root aa-tree-set-root!)
   (size aa-tree-size aa-tree-set-size!))
 
 (define-record-type aa-tree-node
-  (make-aa-tree-node value left right)
+  (make-aa-tree-node value level left right)
   aa-tree-node?
   (value aa-tree-node-value aa-tree-node-set-value!)
+  (level aa-tree-node-level aa-tree-node-set-level!)
   (left aa-tree-node-left aa-tree-node-set-left!)
   (right aa-tree-node-right aa-tree-node-set-right!))
 
+(define level aa-tree-node-level)
 (define left aa-tree-node-left)
 (define right aa-tree-node-right)
 
@@ -28,7 +30,8 @@
       ((not (left node))
         node)
 
-      ()
+      ((eq? (level node) (level (left node)))
+        node)
 
       (else
         node))))
