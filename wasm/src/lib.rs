d701f8ce0eeae22c40768014ad2bcda001983fec
Yota Toyama <raviqqe@gmail.com>
2024-03-25 23:57:39 +0900

    Fix

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,11 +1,20 @@
+use stak_device::ReadWriteDevice;
+use stak_primitive::SmallPrimitiveSet;
+use stak_vm::Vm;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
+pub fn run(bytecodes: &[u8], input: &[u8], heap_size: usize) -> Result<(), JsError> {
+    let mut heap = vec![Default::default(); heap_size];
+    let mut output = vec![];
+    let mut error = vec![];
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm!");
+    let mut vm = Vm::new(
+        &mut heap,
+        SmallPrimitiveSet::new(ReadWriteDevice::new(input, &mut output, &mut error)),
+    )?;
+
+    vm.initialize(bytecodes.iter().copied())?;
+
+    Ok(vm.run()?)
 }
