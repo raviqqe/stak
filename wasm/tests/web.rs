5b18e9a0181ea4295e8eb1242ded1fac2c656e03
Yota Toyama <raviqqe@gmail.com>
2024-03-26 00:20:03 +0900

    Fix

--- wasm/tests/web.rs
+++ wasm/tests/web.rs
@@ -1,35 +1,32 @@
 #![cfg(target_arch = "wasm32")]
 
 use stak_configuration::DEFAULT_HEAP_SIZE;
+use stak_macro::compile_r7rs;
 use stak_wasm::{compile, run};
 use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
 
 wasm_bindgen_test_configure!(run_in_browser run_in_worker);
 
+const SOURCE: &str = r#"
+    (import (scheme base))
+
+    (display "Hello, World!")
+"#;
+
 #[wasm_bindgen_test]
 fn compile_source() {
-    compile(
-        r#"
-            (import (scheme base))
-
-            (display "Hello, World!")
-        "#,
-    )
-    .ok()
-    .unwrap();
+    compile(SOURCE).ok().unwrap();
 }
 
 #[wasm_bindgen_test]
 fn run_bytecodes() {
-    let bytecodes = compile(
+    const PROGRAM: &[u8] = compile_r7rs!(
         r#"
             (import (scheme base))
 
             (display "Hello, World!")
-        "#,
-    )
-    .ok()
-    .unwrap();
+        "#
+    );
 
-    run(&bytecodes, &[], DEFAULT_HEAP_SIZE).ok().unwrap();
+    run(PROGRAM, &[], DEFAULT_HEAP_SIZE).ok().unwrap();
 }
