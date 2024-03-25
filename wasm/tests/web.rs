b1be56a7ee7c2675d9a63cd1d7f2198c089fbf62
Yota Toyama <raviqqe@gmail.com>
2024-03-26 01:09:55 +0900

    Fix WASM build

--- wasm/tests/web.rs
+++ wasm/tests/web.rs
@@ -8,25 +8,21 @@ use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
 wasm_bindgen_test_configure!(run_in_browser run_in_worker);
 
 const SOURCE: &str = r#"
-    (import (scheme base))
+    (import (scheme write))
 
     (display "Hello, World!")
 "#;
 
-// TODO Enable this test.
 #[wasm_bindgen_test]
-#[ignore]
 fn compile_source() {
     compile(SOURCE).ok().unwrap();
 }
 
-// TODO Enable this test.
 #[wasm_bindgen_test]
-#[ignore]
 fn run_bytecodes() {
     const PROGRAM: &[u8] = compile_r7rs!(
         r#"
-            (import (scheme base))
+            (import (scheme write))
 
             (display "Hello, World!")
         "#
