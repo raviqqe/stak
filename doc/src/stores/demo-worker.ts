16202e24275b5a2ded63400614385a2e0553fdfe
Yota Toyama <raviqqe@gmail.com>
2024-03-26 23:45:34 +0900

    Fix

--- doc/src/stores/demo-worker.ts
+++ doc/src/stores/demo-worker.ts
@@ -1,6 +1,6 @@
 import { compile, interpret } from "@raviqqe/stak";
 
-addEventListener("message", (event) => {
+addEventListener("message", (event: MessageEvent<string>) => {
   postMessage(
     new TextDecoder().decode(
       interpret(compile(event.data), new Uint8Array(0), Math.pow(2, 20)),
