e27df8d2ee189882f10e299b4e96a6f921ee6c28
Yota Toyama <raviqqe@gmail.com>
2024-03-27 19:32:46 +0900

    Fix

---
+++ doc/src/stores/compiler-worker.ts
@@ -0,0 +1,7 @@
+import init, { compile } from "@raviqqe/stak";
+
+await init();
+
+addEventListener("message", (event: MessageEvent<string>) => {
+  postMessage(compile(event.data));
+});
---
+++ doc/src/stores/interpreter-worker.ts
@@ -0,0 +1,11 @@
+import init, { interpret } from "@raviqqe/stak";
+
+await init();
+
+addEventListener("message", (event: MessageEvent<Uint8Array>) => {
+  postMessage(
+    new TextDecoder().decode(
+      interpret(event.data, new Uint8Array(0), Math.pow(2, 20)),
+    ),
+  );
+});
