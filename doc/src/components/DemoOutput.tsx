469237c3dfd4d4a5a07344b052ac14126507cd82
Yota Toyama <raviqqe@gmail.com>
2024-03-27 19:08:16 +0900

    Fix

--- doc/src/components/DemoOutput.tsx
+++ doc/src/components/DemoOutput.tsx
@@ -1,15 +1,15 @@
-import { useStore } from "@nanostores/preact";
-import type { JSX } from "preact/jsx-runtime";
+import { useStore } from "@nanostores/solid";
 import { $output } from "../stores/demo-store";
 import styles from "./DemoOutput.module.css";
+import type { JSX } from "solid-js/jsx-runtime";
 
 export const DemoOutput = (): JSX.Element => {
   const output = useStore($output);
 
   return (
-    <div className={styles.container}>
-      <pre className={styles.output}>
-        <code>{output}</code>
+    <div class={styles.container}>
+      <pre class={styles.output}>
+        <code>{output()}</code>
       </pre>
     </div>
   );
