4039b78cb4790d511780fe3eefef75c182fa348a
Yota Toyama <raviqqe@gmail.com>
2024-03-27 23:03:18 +0900

    Fix

--- doc/src/stores/demo-store.ts
+++ doc/src/stores/demo-store.ts
@@ -15,16 +15,16 @@ const $compilerOutput = atom<Uint8Array | null>(new Uint8Array());
 export const $compiling = computed($compilerOutput, (output) => !output);
 
 const $interpreterInput = atom<Uint8Array | null>(null);
-export const $interpreterOutput = atom("");
-export const $interpreting = computed($interpreterOutput, (output) => !output);
+export const $interpreterOutput = atom<string | null>("");
+export const $interpreting = computed(
+  $interpreterOutput,
+  (output) => output === null,
+);
 
 export const initializeCompilerWorker = (): Worker => {
   const worker = new CompilerWorker();
 
-  $compilerInput.subscribe((source) => {
-    worker.postMessage(source);
-    $compilerOutput.set(null);
-  });
+  $compilerInput.subscribe((source) => worker.postMessage(source));
   worker.addEventListener("message", (event: MessageEvent<Uint8Array>) =>
     $compilerOutput.set(event.data),
   );
@@ -38,7 +38,6 @@ export const initializeInterpreterWorker = (): Worker => {
   $interpreterInput.subscribe((bytecodes) => {
     if (bytecodes) {
       worker.postMessage(bytecodes);
-      $interpreterOutput.set("");
     }
   });
   worker.addEventListener("message", (event: MessageEvent<string>) =>
@@ -48,7 +47,12 @@ export const initializeInterpreterWorker = (): Worker => {
   return worker;
 };
 
-export const compile = (): void => $compilerInput.set($source.get());
+export const compile = (): void => {
+  $compilerInput.set($source.get());
+  $compilerOutput.set(null);
+};
 
-export const interpret = (): void =>
+export const interpret = (): void => {
   $interpreterInput.set($compilerOutput.get());
+  $interpreterOutput.set(null);
+};
