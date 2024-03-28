d103ce931d4763b3169f14189ff7eefc25ffe34e
Yota Toyama <raviqqe@gmail.com>
2024-03-29 00:11:21 +0900

    Fix

--- doc/src/components/DemoIo.tsx
+++ doc/src/components/DemoIo.tsx
@@ -1,23 +1,18 @@
 import { useStore } from "@nanostores/preact";
 import type { JSX } from "preact";
-import type { CSSProperties } from "preact/compat";
 import { inputStore, outputStore, outputUrlStore } from "../stores/demo-store";
 import styles from "./DemoIo.module.css";
 import { Label } from "./Label";
 import { Link } from "./Link";
 import { TextArea } from "./TextArea";
 
-interface Props {
-  style?: CSSProperties;
-}
-
-export const DemoIo = ({ style }: Props): JSX.Element => {
+export const DemoIo = (): JSX.Element => {
   const input = useStore(inputStore);
   const output = useStore(outputStore);
   const outputUrl = useStore(outputUrlStore);
 
   return (
-    <div class={styles.container} style={style}>
+    <div class={styles.container}>
       <Label for="input">stdin</Label>
       <TextArea
         id="input"
