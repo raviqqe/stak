f4ca131dc4ecb8a7623d850b394d85d673422de1
Yota Toyama <raviqqe@gmail.com>
2024-03-28 17:15:35 +0900

    Fix

--- doc/src/components/Button.tsx
+++ doc/src/components/Button.tsx
@@ -1,14 +1,18 @@
 import type { ComponentChildren, JSX } from "preact";
 import styles from "./Button.module.css";
+import classNames from "classnames/bind";
+
+const classes = classNames.bind(styles);
 
 interface Props {
   children: ComponentChildren;
+  disabled?: boolean;
   onClick: () => void;
 }
 
-export const Button = ({ children, onClick }: Props): JSX.Element => (
+export const Button = ({ children, disabled, onClick }: Props): JSX.Element => (
   <button
-    class={styles.container}
+    class={classes(styles.container, { disabled })}
     onClick={(event) => {
       event.preventDefault();
       onClick();
