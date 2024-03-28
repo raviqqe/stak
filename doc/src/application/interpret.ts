4a9663a2feed5ae72363a67f6e8a3e824e9176da
Yota Toyama <raviqqe@gmail.com>
2024-03-28 17:30:36 +0900

    Fix

--- doc/src/application/interpret.ts
+++ doc/src/application/interpret.ts
@@ -1,5 +1,5 @@
 import { runWorker } from "../application/run-worker.js";
 import Worker from "./interpret/worker.js?worker";
 
-export const interpret = async (source: Uint8Array): Promise<Uint8Array> =>
-  runWorker(() => new Worker(), source);
+export const interpret = async (program: Uint8Array): Promise<Uint8Array> =>
+  runWorker(() => new Worker(), program);
