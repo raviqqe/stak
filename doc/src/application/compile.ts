e9a133f2adc4511cd730e63e8e8753d5ab1aa9f4
Yota Toyama <raviqqe@gmail.com>
2024-03-29 20:52:45 +0900

    Fix

--- doc/src/application/compile.ts
+++ doc/src/application/compile.ts
@@ -1,6 +1,5 @@
 import { runWorker } from "../application/run-worker.js";
 import Worker from "./compile/worker.js?worker";
-import type { Result } from "./result.js";
 
-export const compile = async (source: string): Promise<Result<Uint8Array>> =>
+export const compile = async (source: string): Promise<Uint8Array> =>
   runWorker(() => new Worker(), source);
--- doc/src/application/interpret.ts
+++ doc/src/application/interpret.ts
@@ -1,9 +1,7 @@
 import { runWorker } from "../application/run-worker.js";
 import Worker from "./interpret/worker.js?worker";
-import type { Result } from "./result.js";
 
 export const interpret = async (
   program: Uint8Array,
   input: Uint8Array,
-): Promise<Result<Uint8Array>> =>
-  runWorker(() => new Worker(), { input, program });
+): Promise<Uint8Array> => runWorker(() => new Worker(), { input, program });
