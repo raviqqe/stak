f1345f3029f24b7515e779bd161d49d46a780a48
Yota Toyama <raviqqe@gmail.com>
2024-03-29 23:45:01 +0900

    Revert "Fix"
    This reverts commit e76e43b1be4c9604769d65eda33be36271cf4829.

--- doc/src/components/DemoIo.tsx
+++ doc/src/components/DemoIo.tsx
@@ -1,4 +1,11 @@
-import { createMemo, type Accessor, type JSX } from "solid-js";
+import { useStore } from "@nanostores/solid";
+import { type JSX } from "solid-js";
+import {
+  inputStore,
+  interpreterErrorStore,
+  outputStore,
+  outputUrlStore,
+} from "../stores/demo-store";
 import styles from "./DemoIo.module.css";
 import { ErrorMessage } from "./ErrorMessage";
 import { Label } from "./Label";
@@ -6,43 +13,30 @@ import { Link } from "./Link";
 import { TextArea } from "./TextArea";
 
 interface Props {
-  input: Accessor<string>;
-  output: Accessor<Uint8Array | null>;
-  outputUrl: Accessor<string | null>;
-  interpreterError: Accessor<string>;
-  onInputChange: (input: string) => void;
   style?: JSX.CSSProperties;
 }
 
-export const DemoIo = ({
-  input,
-  interpreterError,
-  output,
-  outputUrl,
-  onInputChange,
-  style,
-}: Props): JSX.Element => {
-  const decoder = new TextDecoder();
-  const textOutput = createMemo(() => {
-    const value = output();
-    return value ? decoder.decode(value) : "";
-  });
+export const DemoIo = ({ style }: Props): JSX.Element => {
+  const input = useStore(inputStore);
+  const output = useStore(outputStore);
+  const outputUrl = useStore(outputUrlStore);
+  const error = useStore(interpreterErrorStore);
 
   return (
     <div class={styles.container} style={style}>
       <Label for="input">stdin</Label>
       <TextArea
         id="input"
-        onChange={onInputChange}
+        onChange={(input) => inputStore.set(input)}
         style={{ flex: 1 }}
         value={input()}
       />
       <Label for="output">stdout</Label>
       <pre class={styles.output} id="output">
-        {textOutput()}
+        {output()}
       </pre>
       {outputUrl() && <Link href={outputUrl() ?? ""}>Download</Link>}
-      <ErrorMessage>{interpreterError()}</ErrorMessage>
+      <ErrorMessage>{error()}</ErrorMessage>
     </div>
   );
 };
