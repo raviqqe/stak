d8e2d9c85e1991751f5ac4937794ced7daf71dd9
Yota Toyama <raviqqe@gmail.com>
2024-04-01 19:23:10 +0900

    Fix

--- minifier/src/lib.rs
+++ minifier/src/lib.rs
@@ -1,7 +1,11 @@
-/// let bytecodes = stak_macro::compile_r7rs!("(define x 42)");
-
-pub fn minify() {
+/// Minifies given source codes.
+pub fn minify(read: impl Read, writer: impl Write) -> Result<(), Box<dyn Error>> {
     const PROGRAM: &[u8] = stak_macro::include_r7rs!("minify.scm");
 
-    Vm::new().run(PROGRAM);
+    let mut vm = Vm::new();
+
+    vm.initialize(PROGRAM.iter().copied())?;
+    vm.run(PROGRAM)?;
+
+    Ok(())
 }
