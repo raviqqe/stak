ad0368de6cbe25ea073f5a105da907e343a6fc42
Yota Toyama <raviqqe@gmail.com>
2024-04-01 19:32:15 +0900

    Fix

--- minifier/src/lib.rs
+++ minifier/src/lib.rs
@@ -1,11 +1,20 @@
+use stak_configuration::DEFAULT_HEAP_SIZE;
+use stak_device::ReadWriteDevice;
+use stak_primitive::SmallPrimitiveSet;
+use stak_vm::Vm;
+use std::error::Error;
+use std::io::{empty, Read, Write};
+
 /// Minifies given source codes.
-pub fn minify(read: impl Read, writer: impl Write) -> Result<(), Box<dyn Error>> {
+pub fn minify(reader: impl Read, writer: impl Write) -> Result<(), Box<dyn Error>> {
     const PROGRAM: &[u8] = stak_macro::include_r7rs!("minify.scm");
 
-    let mut vm = Vm::new();
+    let mut heap = [Default::default(); DEFAULT_HEAP_SIZE];
+    let device = ReadWriteDevice::new(reader, writer, empty());
+    let mut vm = Vm::new(&mut heap, SmallPrimitiveSet::new(device)).unwrap();
 
     vm.initialize(PROGRAM.iter().copied())?;
-    vm.run(PROGRAM)?;
+    vm.run()?;
 
     Ok(())
 }
