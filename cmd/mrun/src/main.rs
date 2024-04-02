386528bc679af0414aff87797cad929071a15c43
Yota Toyama <raviqqe@gmail.com>
2024-04-03 00:18:33 +0900

    Fix

--- cmd/mrun/src/main.rs
+++ cmd/mrun/src/main.rs
@@ -46,6 +46,7 @@ unsafe extern "C" fn main(argc: isize, argv: *const *const u8) -> isize {
         let source = libc::malloc(size + 1);
         libc::fread(source, size, 1, file);
         libc::fclose(file);
+
         ReadBuffer::new(slice::from_raw_parts(source as _, size))
     };
 
