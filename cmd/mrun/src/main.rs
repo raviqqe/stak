1e3941e5272156552c94a737301eb32b6045866f
Yota Toyama <raviqqe@gmail.com>
2024-04-02 20:10:06 +0900

    Fix

--- cmd/mrun/src/main.rs
+++ cmd/mrun/src/main.rs
@@ -9,54 +9,67 @@
 #![no_std]
 #![no_main]
 
+use core::slice;
+
 use stak_configuration::DEFAULT_HEAP_SIZE;
 use stak_device::{ReadWriteDevice, StdioDevice};
 use stak_macro::include_r7rs;
 use stak_minifier_macro::include_minified;
-use stak_primitive::{SmallError, SmallPrimitiveSet};
+use stak_primitive::SmallPrimitiveSet;
 use stak_vm::{Value, Vm};
 
-const PRELUDE_SOURCE: &str = include_minified!("prelude.scm");
-const COMPILER_PROGRAM: &[u8] = include_r7rs!("compile.scm");
+const PRELUDE_SOURCE: &str = "";
+const COMPILER_PROGRAM: &[u8] = &[];
 
 extern crate alloc;
 extern crate libc;
 
+const DEFAULT_BUFFER_SIZE: usize = 2usize.pow(20);
+
 #[no_mangle]
 extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
-    run().unwrap();
+    run();
 
     0
 }
 
-#[panic_handler]
-fn handle_panic(_info: &core::panic::PanicInfo) -> ! {
-    libc::exit(1);
+// #[panic_handler]
+// fn handle_panic(_info: &core::panic::PanicInfo) -> ! {
+//     libc::exit(1);
+// }
+
+unsafe fn run() {
+    let mut heap = libc::malloc(DEFAULT_HEAP_SIZE);
+    let mut target = libc::malloc(DEFAULT_BUFFER_SIZE);
+
+    let file = libc::fopen("main.scm" as _ as _, "rb" as _ as _);
+    libc::fseek(file, 0, libc::SEEK_END);
+    let size = libc::ftell(file) as usize;
+    libc::rewind(file);
+
+    let source = libc::malloc(size + 1);
+    libc::fread(source, size, 1, file);
+    libc::fclose(file);
+
+    compile(
+        &source,
+        slice::from_raw_parts_mut(target, DEFAULT_BUFFER_SIZE),
+        &mut heap,
+    );
+
+    let mut vm = Vm::new(&mut heap, SmallPrimitiveSet::new(StdioDevice::new())).unwrap();
+
+    vm.initialize(target).unwrap();
+    vm.run().unwrap()
 }
 
-fn run() -> Result<(), Box<dyn Error>> {
-    let mut heap = vec![Default::default(); DEFAULT_HEAP_SIZE];
-
-    let mut source = PRELUDE_SOURCE.into();
-    let mut target = vec![];
-
-    read_source(&arguments.files, &mut source)?;
-    compile(&source, &mut target, &mut heap)?;
-
-    let mut vm = Vm::new(&mut heap, SmallPrimitiveSet::new(StdioDevice::new()))?;
-
-    vm.initialize(target)?;
-
-    Ok(vm.run()?)
-}
-
-fn compile(source: &str, target: &mut Vec<u8>, heap: &mut [Value]) -> Result<(), SmallError> {
+fn compile(source: &[u8], target: &mut [u8], heap: &mut [Value]) {
     let mut vm = Vm::new(
         heap,
-        SmallPrimitiveSet::new(ReadWriteDevice::new(source.as_bytes(), target, empty())),
-    )?;
+        SmallPrimitiveSet::new(ReadWriteDevice::new(source, target, (&mut []) as &mut [u8])),
+    )
+    .unwrap();
 
-    vm.initialize(COMPILER_PROGRAM.iter().copied())?;
-
-    Ok(vm.run()?)
+    vm.initialize(COMPILER_PROGRAM.iter().copied()).unwrap();
+    vm.run().unwrap()
 }
