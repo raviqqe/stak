b965c93d03636271c75df53026f844d34dd8d0dc
Yota Toyama <raviqqe@gmail.com>
2024-04-02 19:28:52 +0900

    Fix

--- cmd/mrun/src/main.rs
+++ cmd/mrun/src/main.rs
@@ -6,7 +6,9 @@
 //! stak foo.scm
 //! ```
 
-use clap::Parser;
+#![no_std]
+#![no_main]
+
 use main_error::MainError;
 use stak_configuration::DEFAULT_HEAP_SIZE;
 use stak_device::{ReadWriteDevice, StdioDevice};
@@ -31,18 +33,24 @@ enum Library {
     R7rs,
 }
 
-#[derive(clap::Parser)]
-#[command(about, version)]
-struct Arguments {
-    #[arg(required(true))]
-    files: Vec<PathBuf>,
-    #[arg(short = 's', long, default_value_t = DEFAULT_HEAP_SIZE)]
-    heap_size: usize,
-    #[arg(short, long, default_value = "r7rs")]
-    library: Library,
+extern crate libc;
+
+#[no_mangle]
+pub extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
+    // Since we are passing a C string the final null character is mandatory.
+    const HELLO: &'static str = "Hello, world!\n\0";
+    unsafe {
+        libc::printf(HELLO.as_ptr() as *const _);
+    }
+    0
 }
 
-fn main() -> Result<(), MainError> {
+#[panic_handler]
+fn my_panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
     let arguments = Arguments::parse();
 
     let mut heap = vec![Default::default(); arguments.heap_size];
