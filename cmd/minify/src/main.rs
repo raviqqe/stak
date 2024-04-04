8c121bdf773ecfb23549e158783d887a56162af8
Yota Toyama <raviqqe@gmail.com>
2024-04-04 19:57:30 +0900

    Move minify.scm

---
+++ cmd/minify/src/main.rs
@@ -0,0 +1,10 @@
+//! A command to compile source codes in Scheme into bytecodes for a virtual
+//! machine.
+//!
+//! # Usage
+//!
+//! ```sh
+//! stak-compile < foo.scm > foo.bc
+//! ```
+
+stak_sac::main!("compile.scm");
--- minifier/src/minify.scm
+++ minifier/src/minify.scm
@@ -1,17 +1,1 @@
-(import (scheme base) (scheme read) (scheme write))
-
-(define (read-all)
-  (let ((x (read)))
-    (if (eof-object? x)
-      '()
-      (cons x (read-all)))))
-
-(define (write-all xs)
-  (for-each
-    (lambda (x)
-      (begin
-        (write x)
-        (newline)))
-    xs))
-
-(write-all (read-all))
+../../minify.scm
