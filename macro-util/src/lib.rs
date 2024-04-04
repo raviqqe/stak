0c592f62c27fb8af13db22027b50ea5337485070
Yota Toyama <raviqqe@gmail.com>
2024-04-04 21:46:16 +0900

    Fix

---
+++ macro-util/src/lib.rs
@@ -0,0 +1,22 @@
+//! Macro utilities.
+
+use proc_macro::TokenStream;
+use quote::quote;
+use std::{env, error::Error, fs::read_to_string, path::Path};
+use syn::LitStr;
+
+pub fn read_file(path: LitStr) -> Result<String, Box<dyn Error>> {
+    Ok(read_to_string(
+        Path::new(&env::var("CARGO_MANIFEST_DIR")?)
+            .join("src")
+            .join(path.value()),
+    )?)
+}
+
+pub fn convert_result(result: Result<TokenStream, Box<dyn Error>>) -> TokenStream {
+    result.unwrap_or_else(|error| {
+        let message = error.to_string();
+
+        quote! { compile_error!(#message) }.into()
+    })
+}
