1d82c2a2fcb69ce8aa0a964a5216ef926fd7bf6a
Yota Toyama <raviqqe@gmail.com>
2024-04-06 00:33:17 +0900

    Refactor

--- cmd/mrun/build.rs
+++ cmd/mrun/build.rs
@@ -12,50 +12,57 @@ const COMPILER_SOURCE_FILE: &str = "src/compile.scm";
 const MINIFIER_SOURCE_FILE: &str = "src/minify.scm";
 
 fn main() -> Result<(), Box<dyn Error>> {
-    for (script_file, source_files, target_file, environment_variable) in [
-        (
-            MINIFIER_SOURCE_FILE,
-            &[PRELUDE_SOURCE_FILE] as &[&str],
-            "prelude.scm",
-            "STAK_PRELUDE_FILE",
-        ),
-        (
-            COMPILER_SOURCE_FILE,
-            &[PRELUDE_SOURCE_FILE, COMPILER_SOURCE_FILE],
-            "compile.bc",
-            "STAK_COMPILER_FILE",
-        ),
-    ] {
-        let target_file = Path::new(&env::var("OUT_DIR")?).join(target_file);
+    run(
+        MINIFIER_SOURCE_FILE,
+        &[PRELUDE_SOURCE_FILE] as &[&str],
+        "prelude.scm",
+        "STAK_PRELUDE_FILE",
+    )?;
+    run(
+        COMPILER_SOURCE_FILE,
+        &[PRELUDE_SOURCE_FILE, COMPILER_SOURCE_FILE],
+        "compile.bc",
+        "STAK_COMPILER_FILE",
+    )?;
 
-        println!(
-            "cargo:rustc-env={environment_variable}={}",
-            target_file.display()
-        );
+    Ok(())
+}
 
-        let mut command = Command::new(option_env!("STAK_HOST_INTERPRETER").unwrap_or("stak"))
-            .arg(script_file)
-            .stdin(Stdio::piped())
-            .stdout(Stdio::piped())
-            .spawn()?;
-        let stdin = command.stdin.as_mut().expect("stdin");
+fn run(
+    script_file: &str,
+    source_files: &[&str],
+    target_file: &str,
+    environment_variable: &str,
+) -> Result<(), Box<dyn Error>> {
+    let target_file = Path::new(&env::var("OUT_DIR")?).join(target_file);
 
-        for file in source_files {
-            println!("cargo:rerun-if-changed={file}");
-            stdin.write_all(&fs::read(file)?)?;
-        }
+    println!(
+        "cargo:rustc-env={environment_variable}={}",
+        target_file.display()
+    );
 
-        command.wait()?;
+    let mut command = Command::new(option_env!("STAK_HOST_INTERPRETER").unwrap_or("stak"))
+        .arg(script_file)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()?;
+    let stdin = command.stdin.as_mut().expect("stdin");
 
-        io::copy(
-            &mut command.stdout.expect("stdout"),
-            &mut File::options()
-                .create(true)
-                .write(true)
-                .truncate(true)
-                .open(target_file)?,
-        )?;
+    for file in source_files {
+        println!("cargo:rerun-if-changed={file}");
+        stdin.write_all(&fs::read(file)?)?;
     }
+
+    command.wait()?;
+
+    io::copy(
+        &mut command.stdout.expect("stdout"),
+        &mut File::options()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(target_file)?,
+    )?;
 
     Ok(())
 }
