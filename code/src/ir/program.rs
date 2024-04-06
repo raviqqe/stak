de6a9fab93d82d3146c9d2e691695899810656fc
Yota Toyama <raviqqe@gmail.com>
2024-04-06 13:37:34 +0900

    Fix

--- code/src/ir/program.rs
+++ code/src/ir/program.rs
@@ -58,8 +58,8 @@ impl Display for Program {
 mod tests {
     use super::*;
     use crate::Operand;
-    use insta::assert_snapshot;
     use alloc::{format, vec};
+    use insta::assert_snapshot;
 
     #[test]
     fn display_symbols() {
