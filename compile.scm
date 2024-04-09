42d815140b82afec2d4bb379581a92047d2b90b1
Yota Toyama <raviqqe@gmail.com>
2024-04-09 19:09:31 +0900

    Update compile.scm

--- compile.scm
+++ compile.scm
@@ -518,7 +518,7 @@
       ((and
           (symbol? predicate)
           (assoc (symbol->string predicate) primitive-functions)
-          (= (length expression) 3))
+          (= (relaxed-length expression) 3))
         =>
         (lambda (pair)
           (cons (cdr pair) (cdr expression))))
