1600546f6579639c6e07ec5cb0b01183d05d6551
Yota Toyama <raviqqe@gmail.com>
2024-04-10 23:45:54 +0900

    Revert "Fix"
    This reverts commit 9fe689213f7e2d5f3cd1a940331b00ea0b78ea6f.

--- device/src/libc.rs
+++ device/src/libc.rs
@@ -1,7 +1,6 @@
 mod buffer;
 mod device;
 mod error;
-mod multi_reader;
 mod read;
 mod stdio;
 mod write;
@@ -9,7 +8,6 @@ mod write;
 pub use buffer::{Buffer, BufferMut};
 pub use device::ReadWriteDevice;
 pub use error::Error;
-pub use multi_reader::MultiReader;
 pub use read::Read;
 pub use stdio::{Stderr, Stdin, Stdout};
 pub use write::Write;
