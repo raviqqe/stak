8061b2bd2a240ce7123657897455edb9df637b5b
Yota Toyama <raviqqe@gmail.com>
2024-04-10 21:01:08 +0900

    Fix

--- cmd/minimal/interpret/src/main.rs
+++ cmd/minimal/interpret/src/main.rs
@@ -6,30 +6,56 @@
 //! mstak-interpret foo.bc
 //! ```
 
-use clap::Parser;
-use main_error::MainError;
-use stak_configuration::DEFAULT_HEAP_SIZE;
-use stak_device::StdioDevice;
-use stak_primitive::SmallPrimitiveSet;
-use stak_vm::Vm;
-use std::{fs::read, path::PathBuf};
+#![no_std]
+#![cfg_attr(not(test), no_main)]
 
-#[derive(clap::Parser)]
-#[command(about, version)]
-struct Arguments {
-    #[arg(required(true))]
-    file: PathBuf,
-    #[arg(short = 's', long, default_value_t = DEFAULT_HEAP_SIZE)]
-    heap_size: usize,
+use core::{mem::size_of, slice};
+use stak_device::libc::{ReadWriteDevice, Stderr, Stdin, Stdout};
+use stak_primitive::SmallPrimitiveSet;
+use stak_vm::{Value, Vm};
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    unsafe { libc::exit(1) }
 }
 
-fn main() -> Result<(), MainError> {
-    let arguments = Arguments::parse();
+#[cfg_attr(not(test), no_mangle)]
+unsafe extern "C" fn main(argc: isize, argv: *const *const i8) -> isize {
+    let arguments = slice::from_raw_parts(argv, argc as _);
 
-    let mut heap = vec![Default::default(); arguments.heap_size];
-    let mut vm = Vm::new(&mut heap, SmallPrimitiveSet::new(StdioDevice::new()))?;
+    if arguments.len() != 2 {
+        return 1;
+    }
 
-    vm.initialize(read(&arguments.file)?)?;
+    let mut heap = [Default::default(); 1 << 18];
 
-    Ok(vm.run()?)
+    let mut vm = Vm::new(
+        &mut heap,
+        SmallPrimitiveSet::new(ReadWriteDevice::new(
+            Stdin::new(),
+            Stdout::new(),
+            Stderr::new(),
+        )),
+    )
+    .unwrap();
+
+    vm.initialize(read_file(arguments[1]).iter().copied())
+        .unwrap();
+    vm.run().unwrap();
+
+    0
+}
+
+unsafe fn read_file(path: *const i8) -> &'static [u8] {
+    let file = libc::fopen(path, c"rb" as *const _ as _);
+    libc::fseek(file, 0, libc::SEEK_END);
+    let size = libc::ftell(file) as usize;
+    libc::rewind(file);
+
+    let source = libc::malloc(size + 1);
+    libc::fread(source, size, 1, file);
+    libc::fclose(file);
+
+    slice::from_raw_parts(source as _, size)
 }
