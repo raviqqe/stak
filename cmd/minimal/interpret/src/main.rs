8f51d23b0d24471477ed4d768945c61197fd49b5
Yota Toyama <raviqqe@gmail.com>
2024-04-10 20:46:24 +0900

    Fix

---
+++ cmd/minimal/interpret/src/main.rs
@@ -0,0 +1,35 @@
+//! A command to interpret a bytecode file.
+//!
+//! # Usage
+//!
+//! ```sh
+//! stak-interpret foo.bc
+//! ```
+
+use clap::Parser;
+use main_error::MainError;
+use stak_configuration::DEFAULT_HEAP_SIZE;
+use stak_device::StdioDevice;
+use stak_primitive::SmallPrimitiveSet;
+use stak_vm::Vm;
+use std::{fs::read, path::PathBuf};
+
+#[derive(clap::Parser)]
+#[command(about, version)]
+struct Arguments {
+    #[arg(required(true))]
+    file: PathBuf,
+    #[arg(short = 's', long, default_value_t = DEFAULT_HEAP_SIZE)]
+    heap_size: usize,
+}
+
+fn main() -> Result<(), MainError> {
+    let arguments = Arguments::parse();
+
+    let mut heap = vec![Default::default(); arguments.heap_size];
+    let mut vm = Vm::new(&mut heap, SmallPrimitiveSet::new(StdioDevice::new()))?;
+
+    vm.initialize(read(&arguments.file)?)?;
+
+    Ok(vm.run()?)
+}
