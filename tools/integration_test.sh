d8728c2515feedc229a1b8d6a9c70cdae2010ffc
Yota Toyama <raviqqe@gmail.com>
2024-04-10 20:36:45 +0900

    Fix

--- tools/integration_test.sh
+++ tools/integration_test.sh
@@ -23,7 +23,7 @@ cd $(dirname $0)/..
 bundler install
 
 cargo build --profile release_test --features $features
-(cd cmd/minimal/run && cargo build --release)
+(cd cmd/minimal && cargo build --release)
 
 export PATH=$PWD/tools/scheme/$interpreter:$PATH
 
