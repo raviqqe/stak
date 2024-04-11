80eef19a5f116b8dc3acef84f0e64a3012be8a76
Yota Toyama <raviqqe@gmail.com>
2024-04-11 18:48:23 +0900

    Fix

--- cmd/minimal/util/src/heap.rs
+++ cmd/minimal/util/src/heap.rs
@@ -1,4 +1,4 @@
-use core::{mem::size_of, slice};
+use core::{mem::align_of, ptr::write, slice};
 
 /// A memory block on a heap.
 pub struct Heap<T> {
@@ -8,11 +8,17 @@ pub struct Heap<T> {
 
 impl<T> Heap<T> {
     /// Creates a heap.
-    pub fn new(len: usize) -> Self {
-        Self {
-            ptr: unsafe { libc::malloc(len * size_of::<T>()) } as _,
+    pub fn new(len: usize, default: impl Fn() -> T) -> Self {
+        let mut this = Self {
+            ptr: unsafe { libc::malloc(len * align_of::<T>()) } as _,
             len,
+        };
+
+        for x in this.as_slice_mut() {
+            unsafe { write(x, default()) };
         }
+
+        this
     }
 
     /// Returns a slice.
@@ -23,5 +29,21 @@ impl<T> Heap<T> {
     /// Returns a mutable slice.
     pub fn as_slice_mut(&mut self) -> &mut [T] {
         unsafe { slice::from_raw_parts_mut(self.ptr as _, self.len) }
+    }
+}
+
+impl<T> Drop for Heap<T> {
+    fn drop(&mut self) {
+        unsafe { libc::free(self.ptr as _) }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn file_size() {
+        Heap::<usize>::new(42, || 42);
     }
 }
