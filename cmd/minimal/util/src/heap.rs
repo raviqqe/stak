af5125dcde37ca68834768b46edecd3c4b417b64
Yota Toyama <raviqqe@gmail.com>
2024-04-11 18:42:42 +0900

    Fix

--- cmd/minimal/util/src/heap.rs
+++ cmd/minimal/util/src/heap.rs
@@ -1,12 +1,13 @@
 use core::{mem::size_of, slice};
 
+/// A memory block on a heap.
 pub struct Heap<T> {
     ptr: *mut T,
     len: usize,
 }
 
 impl<T> Heap<T> {
-    /// Allocates a memory block on heap.
+    /// Creates a heap.
     pub fn new(len: usize) -> Self {
         Self {
             ptr: unsafe { libc::malloc(len * size_of::<T>()) } as _,
@@ -14,10 +15,12 @@ impl<T> Heap<T> {
         }
     }
 
+    /// Returns a slice.
     pub fn as_slice(&mut self) -> &[T] {
         unsafe { slice::from_raw_parts(self.ptr as _, self.len) }
     }
 
+    /// Returns a mutable slice.
     pub fn as_slice_mut(&mut self) -> &mut [T] {
         unsafe { slice::from_raw_parts_mut(self.ptr as _, self.len) }
     }
