c4426e3eae6394249da4df14d5b3f93792c36a17
Yota Toyama <raviqqe@gmail.com>
2024-04-11 19:16:10 +0900

    Heap object (#1030)

--- cmd/minimal/util/src/lib.rs
+++ cmd/minimal/util/src/lib.rs
@@ -2,18 +2,14 @@
 
 #![no_std]
 
+mod heap;
 mod mmap;
 mod validate;
 
-use core::{ffi::CStr, mem::size_of, slice};
+use core::ffi::CStr;
+pub use heap::Heap;
 pub use mmap::Mmap;
 pub use validate::validate;
-
-/// Allocates a memory block on heap.
-// TODO Return a heap object.
-pub fn allocate_heap<T>(size: usize) -> &'static mut [T] {
-    unsafe { slice::from_raw_parts_mut(libc::malloc(size * size_of::<T>()) as _, size) }
-}
 
 /// Reads a file size at a path.
 pub fn read_file_size(path: &CStr) -> usize {
