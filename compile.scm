ae64100b381c2f491a210e4eeb76ebcc9e5e4150
Yota Toyama <raviqqe@gmail.com>
2024-04-15 18:38:10 +0900

    Refactor

--- compile.scm
+++ compile.scm
@@ -571,20 +571,11 @@
 (define (match-ellipsis-pattern context pattern expression)
   (map-values
     make-ellipsis-match
-    (fold-right
-      (lambda (all expression)
-        (map
-          (lambda (pair)
-            (let ((name (car pair)))
-              (cons
-                name
-                (cons
-                  (cdr pair)
-                  (cdr (assq name all))))))
-          (match-pattern context pattern expression)))
-      (map
-        (lambda (name) (cons name '()))
-        (find-pattern-variables context (rule-context-literals context) pattern))
+    (apply
+      map
+      list
+      (find-pattern-variables context (rule-context-literals context) pattern)
+      (lambda (expression) (match-pattern context pattern expression))
       expression)))
 
 (define (match-pattern context pattern expression)
