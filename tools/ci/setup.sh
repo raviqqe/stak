27c22c774d804ccc1cfee3d535f32fe3e4230677
Yota Toyama <raviqqe@gmail.com>
2024-04-15 22:49:24 +0900

    Fix

--- tools/ci/setup.sh
+++ tools/ci/setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ $(uname) = Linux ]; then
-  sudo apt install libfakeroot
+  sudo mkdir -p /usr/lib/x86_64-linux-gnu/libfakeroot
 fi
 
 brew install chibi-scheme gambit-scheme gauche guile
