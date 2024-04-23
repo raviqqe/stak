f43d04fedfa7f3daf82f2e63adea376429b51db0
Yota Toyama <raviqqe@gmail.com>
2024-04-23 18:22:18 +0900

    fix

--- tools/lint.sh
+++ tools/lint.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-cargo clippy "$@" -- -D warnings -D clippy::use_self
-cargo clippy --all-features "$@" -- -D warnings -D clippy::use_self
+cargo clippy "$@"
+cargo clippy --all-features "$@"
