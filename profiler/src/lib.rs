61b54f34ef3addee9b664d034cc34b483b3b9f86
Yota Toyama <raviqqe@gmail.com>
2024-04-26 09:33:35 +0900

    Fix

--- profiler/src/lib.rs
+++ profiler/src/lib.rs
@@ -1,14 +1,36 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct Record {}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub enum RecordType {
+    Call,
+    Return,
+    ReturnCall,
+}
+
+pub fn parse_records(source: &str) -> impl Iterator<Item = Record> {
+    source.lines().map(|line| line.split("\t"))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use indoc::indoc;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn parse_record() {
+        assert_eq!(
+            parse_records(
+                &indoc!(
+                    "
+                    call\tfoo;bar;baz\t0
+                    return\tfoo;bar;baz\t42
+                    "
+                )
+                .trim()
+            )
+            .collect::<Vec<_>>(),
+            vec![]
+        );
     }
 }
