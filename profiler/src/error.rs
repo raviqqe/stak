c2a11746de8e6ccf177a6334dae04e1d8de147c3
Yota Toyama <raviqqe@gmail.com>
2024-04-26 19:05:16 +0900

    Fix

---
+++ profiler/src/error.rs
@@ -0,0 +1,20 @@
+/// An error.
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub enum Error {
+    /// A missing record type.
+    MissingRecordType,
+    /// A missing stack.
+    MissingStack,
+    /// A missing time.
+    MissingTime,
+    /// Integer parse failure.
+    ParseInt(ParseIntError),
+    /// An unknown record type.
+    UnknownRecordType,
+}
+
+impl From<ParseIntError> for Error {
+    fn from(error: ParseIntError) -> Self {
+        Self::ParseInt(error)
+    }
+}
