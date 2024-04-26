31e9c7ed09f1d8a502ad1b54bd2c61333b6bdd53
Yota Toyama <raviqqe@gmail.com>
2024-04-26 19:47:10 +0900

    Fix

--- profiler/src/lib.rs
+++ profiler/src/lib.rs
@@ -11,3 +11,31 @@ pub use flamegraph::burn_flamegraph;
 pub use parse::parse_records;
 pub use record::Record;
 pub use record_type::RecordType;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn parse_record() {
+        let mut buffer = vec![];
+
+        burn_flamegraph(
+            parse_records(
+                &indoc!(
+                    "
+                    call\tfoo;bar;baz\t0
+                    return\tfoo;bar;baz\t42
+                    "
+                )
+                .trim(),
+            ),
+            &mut buffer,
+        )
+        .unwrap();
+
+        assert_eq!(String::from_utf8(buffer).unwrap(), "baz;bar;foo 42\n");
+    }
+}
