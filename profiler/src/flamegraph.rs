17c0bb54c5bba5108d4e859a88ca84d5dbc7b533
Yota Toyama <raviqqe@gmail.com>
2024-04-26 19:27:54 +0900

    Fix

--- profiler/src/flamegraph.rs
+++ profiler/src/flamegraph.rs
@@ -3,7 +3,11 @@ use std::io::{self, Write};
 
 pub fn burn_flamegraph<'a>(
     records: impl Iterator<Item = Result<Record<'a>, Error>>,
-    writer: impl Write,
+    mut writer: impl Write,
 ) -> Result<(), io::Error> {
-    todo!()
+    for record in records {
+        write!(writer, "foo")?;
+    }
+
+    Ok(())
 }
