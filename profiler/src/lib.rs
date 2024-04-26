32ba0514345fbac5eda7a766e789b8f21263b9f4
Yota Toyama <raviqqe@gmail.com>
2024-04-26 09:43:11 +0900

    Fix

--- profiler/src/lib.rs
+++ profiler/src/lib.rs
@@ -2,6 +2,8 @@ use core::num::ParseIntError;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Record {
+    r#type: RecordType,
+    stack: Vec<String>,
     time: u128,
 }
 
@@ -30,11 +32,14 @@ pub fn parse_records(source: &str) -> impl Iterator<Item = Result<Record, Error>
     source.lines().map(|line| -> Result<Record, Error> {
         let mut iterator = line.split("\t");
 
-        let r#type = iterator.next().ok_or(Error::MissingRecordType)?;
-        let stack = iterator.next().ok_or(Error::MissingStack)?;
-        let time = iterator.next().ok_or(Error::MissingTime)?;
-
         Ok(Record {
+            r#type: iterator.next().ok_or(Error::MissingRecordType)?.parse()?,
+            stack: iterator
+                .next()
+                .ok_or(Error::MissingStack)?
+                .split(";")
+                .map(ToOwned::to_owned)
+                .collect(),
             time: iterator.next().ok_or(Error::MissingTime)?.parse()?,
         })
     })
