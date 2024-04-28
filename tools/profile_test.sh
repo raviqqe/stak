0eecd07b316639f488d096b3913cfd05459669e7
Yota Toyama <raviqqe@gmail.com>
2024-04-28 14:06:42 +0900

    Fix

--- tools/profile_test.sh
+++ tools/profile_test.sh
@@ -4,8 +4,10 @@ set -ex
 
 cd $(dirname $0)/..
 
-mkdir -p tmp
+export PATH=$PWD/target/release:$PATH
+cargo build --release
 
+mkdir -p tmp
 cat <<EOF >tmp/main.scm
 (import (scheme base) (scheme write))
 
@@ -14,3 +16,4 @@ cat <<EOF >tmp/main.scm
 EOF
 
 cat prelude.scm tmp/main.scm | stak compile.scm >tmp/main.bc
+stak-profile run --profile tmp/profile.txt tmp/main.bc
