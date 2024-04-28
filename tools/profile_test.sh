df424451551d18e6be9c2c10912a395ad2fd6a10
Yota Toyama <raviqqe@gmail.com>
2024-04-28 14:07:29 +0900

    Fix

--- tools/profile_test.sh
+++ tools/profile_test.sh
@@ -17,3 +17,4 @@ EOF
 
 cat prelude.scm tmp/main.scm | stak compile.scm >tmp/main.bc
 stak-profile run --profile tmp/profile.txt tmp/main.bc
+stak-profile analyze duration tmp/main.bc >tmp/durations.txt
