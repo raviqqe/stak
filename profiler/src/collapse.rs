8cec1b32a2628aa163d5271214688d7540959f6b
Yota Toyama <raviqqe@gmail.com>
2024-04-28 13:03:10 +0900

    Fix

--- profiler/src/collapse.rs
+++ profiler/src/collapse.rs
@@ -1,4 +1,4 @@
-use crate::{DurationRecord, Error, ProcedureOperation, ProcedureRecord, Stack};
+use crate::{DurationRecord, Error};
 use std::io::Write;
 
 /// Collapses stacks.
@@ -6,132 +6,13 @@ pub fn collapse_stacks(
     records: impl IntoIterator<Item = Result<DurationRecord, Error>>,
     mut writer: impl Write,
 ) -> Result<(), Error> {
-    let mut first = true;
-    let mut stack = vec![];
-
     for record in records {
-        let record = record?;
+        let mut record = record?;
 
-        if first {
-            stack.push(ProcedureRecord::new(
-                ProcedureOperation::Call,
-                Stack::new(vec![None]),
-                record.time(),
-            ));
-            first = false;
-        }
+        record.stack_mut().collapse;
 
-        match record.operation() {
-            ProcedureOperation::Call => {
-                stack.push(record);
-            }
-            ProcedureOperation::Return => calculate_duration(&mut stack, &record, &mut writer)?,
-            ProcedureOperation::ReturnCall => {
-                calculate_duration(&mut stack, &record, &mut writer)?;
-                stack.push(record);
-            }
-        }
+        write!(writer, "{}", record)?;
     }
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use indoc::indoc;
-    use pretty_assertions::assert_eq;
-
-    #[test]
-    fn analyze_call() {
-        let mut buffer = vec![];
-
-        calculate_durations(
-            [
-                Ok(ProcedureRecord::new(
-                    ProcedureOperation::Call,
-                    Stack::new(vec![
-                        Some("baz".into()),
-                        Some("bar".into()),
-                        Some("foo".into()),
-                    ]),
-                    0,
-                )),
-                Ok(ProcedureRecord::new(
-                    ProcedureOperation::Return,
-                    Stack::new(vec![
-                        Some("baz".into()),
-                        Some("bar".into()),
-                        Some("foo".into()),
-                    ]),
-                    42,
-                )),
-            ],
-            &mut buffer,
-        )
-        .unwrap();
-
-        assert_eq!(String::from_utf8(buffer).unwrap(), "baz;bar;foo\t42\n");
-    }
-
-    #[test]
-    fn analyze_nested_calls() {
-        let mut buffer = vec![];
-
-        calculate_durations(
-            [
-                Ok(ProcedureRecord::new(
-                    ProcedureOperation::Call,
-                    Stack::new(vec![Some("baz".into())]),
-                    0,
-                )),
-                Ok(ProcedureRecord::new(
-                    ProcedureOperation::Call,
-                    Stack::new(vec![Some("baz".into()), Some("bar".into())]),
-                    1,
-                )),
-                Ok(ProcedureRecord::new(
-                    ProcedureOperation::Call,
-                    Stack::new(vec![
-                        Some("baz".into()),
-                        Some("bar".into()),
-                        Some("foo".into()),
-                    ]),
-                    2,
-                )),
-                Ok(ProcedureRecord::new(
-                    ProcedureOperation::Return,
-                    Stack::new(vec![
-                        Some("baz".into()),
-                        Some("bar".into()),
-                        Some("foo".into()),
-                    ]),
-                    42,
-                )),
-                Ok(ProcedureRecord::new(
-                    ProcedureOperation::Return,
-                    Stack::new(vec![Some("baz".into()), Some("bar".into())]),
-                    84,
-                )),
-                Ok(ProcedureRecord::new(
-                    ProcedureOperation::Return,
-                    Stack::new(vec![Some("baz".into())]),
-                    126,
-                )),
-            ],
-            &mut buffer,
-        )
-        .unwrap();
-
-        assert_eq!(
-            String::from_utf8(buffer).unwrap(),
-            indoc!(
-                "
-                baz;bar;foo\t40
-                baz;bar\t83
-                baz\t126
-                "
-            )
-        );
-    }
 }
