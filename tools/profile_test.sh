7ba7b6397e57e132a368a068314af83b5005681a
Yota Toyama <raviqqe@gmail.com>
2024-04-29 17:13:42 +0900

    Try profiling a compiler

--- tools/profile_test.sh
+++ tools/profile_test.sh
@@ -17,8 +17,8 @@ cat <<EOF >tmp/main.scm
 (foo 42)
 EOF
 
-cat prelude.scm tmp/main.scm | stak compile.scm >tmp/main.bc
-stak-profile run --profile tmp/profile.txt tmp/main.bc
+cat prelude.scm compile.scm | stak compile.scm >tmp/main.bc
+stak-profile run --profile tmp/profile.txt tmp/main.bc <prelude.scm <compile.scm
 stak-profile analyze duration <tmp/profile.txt |
   stak-profile analyze stack-collapse |
   stak-profile analyze stack-reverse |
