2266d6203ed5495c2715fe561c07fecfb2ac756d
Yota Toyama <raviqqe@gmail.com>
2024-05-11 21:44:35 +0900

    Simple library symbols (#1166)

--- compile.scm
+++ compile.scm
@@ -285,25 +285,20 @@
 
 ;; Procedures
 
-(define library-symbol-prefix "$%")
+(define library-symbol-separator #\%)
 
 (define (resolve-library-symbol name)
-  (let ((string (symbol->string name)))
-    (if (and
-         (> (string-length string) 4)
-         (equal? (substring string 0 2) library-symbol-prefix))
-      (let ((position (memv-position #\$ (cdr (string->list string)))))
-        (if position
-          (string->symbol (string-copy string (+ position 2)))
-          name))
+  (let* ((string (symbol->string name))
+         (position (memv-position library-symbol-separator (string->list string))))
+    (if position
+      (string->symbol (string-copy string (+ position 1)))
       name)))
 
 (define (build-library-symbol id name)
   (string->uninterned-symbol
     (string-append
-      library-symbol-prefix
       (id->string id)
-      "$"
+      (list->string (list library-symbol-separator))
       (symbol->string name))))
 
 (define (rename-library-symbol context id name)
