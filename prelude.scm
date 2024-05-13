b2012ad862dc5d5e31134e08f3adf6231fd98cc9
Yota Toyama <raviqqe@gmail.com>
2024-05-13 19:02:18 +0900

    Fix internal `define-values`

--- prelude.scm
+++ prelude.scm
@@ -228,14 +228,8 @@
         ((_ "value" arguments ((name value) ...) body1 body2 ...)
           (lambda arguments (letrec* ((name value) ...) body1 body2 ...)))
 
-        ((_ arguments (define-values content ...) body1 body2 ...)
-          (lambda "values" arguments () (define-values content ...) body1 body2 ...))
-
-        ((_ "values" arguments ((names value) ...) (define-values new-names new-value) body1 body2 ...)
-          (lambda "values" arguments ((names value) ... (new-names new-value)) body1 body2 ...))
-
-        ((_ "values" arguments ((names value) ...) body1 body2 ...)
-          (lambda arguments (let-values ((names value) ...) body1 body2 ...)))
+        ((_ arguments (define-values names value) body1 body2 ...)
+          (lambda arguments (let-values ((names value)) body1 body2 ...)))
 
         ((_ arguments (define-syntax name value) body1 body2 ...)
           (lambda "syntax" arguments ((name value)) body1 body2 ...))
