ac9739962e92b4e7f86ccf2d9ce3754ba318f9e1
Yota Toyama <raviqqe@gmail.com>
2024-06-05 20:14:59 +0900

    Fix

--- stak-file/src/void.rs
+++ stak-file/src/void.rs
@@ -1,15 +1,17 @@
+use crate::Error;
+
 pub struct VoidFileSystem {}
 
 impl FileSystem<Error> for VoidFileSystem {
     fn open(&self, path: &str) -> Result<Box<dyn Read>, Error> {
-        Err(Error::new(ErrorKind::NotFound, "File not found"))
+        Err(Error::Open)
     }
 
     fn read(&self, file: &mut dyn Read, buffer: &mut [u8]) -> Result<usize, Error> {
-        Err(Error::new(ErrorKind::NotFound, "File not found"))
+        Err(Error::Read)
     }
 
     fn write(&self, byte: u8) -> Result<void, Error> {
-        Err(Error::new(ErrorKind::NotFound, "File not found"))
+        Err(Error::Write)
     }
 }
