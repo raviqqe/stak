eb81b7420e7250bf2f359d0e45248ea18a2c8e0d
Yota Toyama <raviqqe@gmail.com>
2024-06-05 20:08:35 +0900

    Fix

--- stak-file/src/file_system.rs
+++ stak-file/src/file_system.rs
@@ -1,1 +1,5 @@
-pub trait FileSystem {}
+pub trait FileSystem<E> {
+    fn open(&self, path: &[u8]) -> Result<usize, E>;
+    fn read(&self, descriptor: usize) -> Result<u8, E>;
+    fn write(&self, descriptor: usize, byte: u8) -> Result<(), E>;
+}
