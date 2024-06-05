0db9e44b921c34c60620336112b6d04d6c9af405
Yota Toyama <raviqqe@gmail.com>
2024-06-05 20:28:46 +0900

    Fix

--- file/src/file_system.rs
+++ file/src/file_system.rs
@@ -1,10 +1,10 @@
-use crate::Error;
+use crate::{Error, FileDescriptor};
 use core::fmt::Debug;
 
 pub trait FileSystem {
     type Error: Debug;
 
-    fn open(&self, path: &[u8]) -> Result<usize, Error>;
-    fn read(&self, descriptor: usize) -> Result<u8, Error>;
-    fn write(&self, descriptor: usize, byte: u8) -> Result<(), Error>;
+    fn open(&self, path: &[u8]) -> Result<FileDescriptor, Error>;
+    fn read(&self, descriptor: FileDescriptor) -> Result<u8, Error>;
+    fn write(&self, descriptor: FileDescriptor, byte: u8) -> Result<(), Error>;
 }
