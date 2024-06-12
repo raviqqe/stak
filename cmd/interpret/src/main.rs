fc71826a4b339b313d5ef3ab3ef1bd02b5216433
Yota Toyama <raviqqe@gmail.com>
2024-06-12 21:07:15 +0900

    fix

--- cmd/interpret/src/main.rs
+++ cmd/interpret/src/main.rs
@@ -10,6 +10,7 @@ use clap::Parser;
 use main_error::MainError;
 use stak_configuration::DEFAULT_HEAP_SIZE;
 use stak_device::StdioDevice;
+use stak_file::LibcFileSystem;
 use stak_primitive::SmallPrimitiveSet;
 use stak_vm::Vm;
 use std::{fs::read, path::PathBuf};
@@ -27,7 +28,10 @@ fn main() -> Result<(), MainError> {
     let arguments = Arguments::parse();
 
     let mut heap = vec![Default::default(); arguments.heap_size];
-    let mut vm = Vm::new(&mut heap, SmallPrimitiveSet::new(StdioDevice::new()))?;
+    let mut vm = Vm::new(
+        &mut heap,
+        SmallPrimitiveSet::new(StdioDevice::new(), LibcFileSystem::new()),
+    )?;
 
     vm.initialize(read(&arguments.file)?)?;
 
