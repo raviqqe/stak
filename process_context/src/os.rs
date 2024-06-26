6401600cadebcca6a8338e757b0fcfad9ebe8193
Yota Toyama <raviqqe@gmail.com>
2024-06-26 19:59:57 +0900

    Fix

--- process_context/src/os.rs
+++ process_context/src/os.rs
@@ -1,3 +1,13 @@
+use crate::ProcessContext;
+
 pub struct OsProcessContext {}
 
-impl ProcessContext for OsProcessContext {}
+impl ProcessContext for OsProcessContext {
+    fn command_line(&self) -> impl IntoIterator<Item = &str> {
+        []
+    }
+
+    fn environment_variables(&self) -> impl IntoIterator<Item = (&str, &str)> {
+        []
+    }
+}
--- process_context/src/process_context.rs
+++ process_context/src/process_context.rs
@@ -1,8 +1,8 @@
 /// A process context.
-pub trait ProcessContext<'a> {
+pub trait ProcessContext {
     /// Returns a command name and its arguments.
-    fn command_line() -> impl IntoIterator<Item = &'a str>;
+    fn command_line(&self) -> impl IntoIterator<Item = &str>;
 
     /// Returns environment variables.
-    fn environment_variables() -> impl IntoIterator<Item = (&'a str, &'a str)>;
+    fn environment_variables(&self) -> impl IntoIterator<Item = (&str, &str)>;
 }
