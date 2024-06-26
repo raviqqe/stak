96d215e083cacb80652afa1cdcf928c5ba68fe32
Yota Toyama <raviqqe@gmail.com>
2024-06-26 20:11:02 +0900

    Fix

--- process_context/src/lib.rs
+++ process_context/src/lib.rs
@@ -9,7 +9,9 @@ extern crate std;
 #[cfg(feature = "std")]
 mod os;
 mod process_context;
+mod void;
 
 #[cfg(feature = "std")]
 pub use os::OsProcessContext;
 pub use process_context::ProcessContext;
+pub use void::VoidProcessContext;
---
+++ process_context/src/void.rs
@@ -0,0 +1,20 @@
+use crate::ProcessContext;
+
+#[derive(Debug, Default)]
+pub struct VoidProcessContext {}
+
+impl VoidProcessContext {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl ProcessContext for VoidProcessContext {
+    fn command_line(&self) -> impl IntoIterator<Item = &str> {
+        []
+    }
+
+    fn environment_variables(&self) -> impl IntoIterator<Item = (&str, &str)> {
+        []
+    }
+}
