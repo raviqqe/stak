4dbc2598cb1ddf93222e1013eda55d3a79769542
Yota Toyama <raviqqe@gmail.com>
2024-06-26 19:54:15 +0900

    Fix

--- process_context/src/process_context.rs
+++ process_context/src/process_context.rs
@@ -1,8 +1,8 @@
 /// A process context.
 pub trait ProcessContext<'a> {
     /// Returns a command name and its arguments.
-    fn command_line() -> &'a [&'a str];
+    fn command_line() -> impl IntoIterator<Item = &'a str>;
 
     /// Returns environment variables.
-    fn environment_variables() -> &'a [(&'a str, &'a str)];
+    fn environment_variables() -> impl IntoIterator<Item = (&'a str, &'a str)>;
 }
