85577f7ccde49b090818a5e76ed6b2ca284c8688
Yota Toyama <raviqqe@gmail.com>
2024-06-26 20:07:08 +0900

    Fix

--- process_context/src/lib.rs
+++ process_context/src/lib.rs
@@ -1,6 +1,9 @@
 #![no_std]
 
 #[cfg(feature = "std")]
+extern crate alloc;
+
+#[cfg(feature = "std")]
 extern crate std;
 
 #[cfg(feature = "std")]
--- process_context/src/os.rs
+++ process_context/src/os.rs
@@ -1,13 +1,30 @@
 use crate::ProcessContext;
+use alloc::{string::String, vec::Vec};
+use once_cell::unsync::Lazy;
+use std::env::{args, vars};
 
-pub struct OsProcessContext {}
+pub struct OsProcessContext {
+    arguments: Lazy<Vec<String>>,
+    environment_variables: Lazy<Vec<(String, String)>>,
+}
+
+impl OsProcessContext {
+    pub fn new() -> Self {
+        Self {
+            arguments: Lazy::new(|| args().collect()),
+            environment_variables: Lazy::new(|| vars().collect()),
+        }
+    }
+}
 
 impl ProcessContext for OsProcessContext {
     fn command_line(&self) -> impl IntoIterator<Item = &str> {
-        []
+        (*self.arguments).iter().map(AsRef::as_ref)
     }
 
     fn environment_variables(&self) -> impl IntoIterator<Item = (&str, &str)> {
-        []
+        (*self.environment_variables)
+            .iter()
+            .map(|(key, value)| (key.as_ref(), value.as_ref()))
     }
 }
