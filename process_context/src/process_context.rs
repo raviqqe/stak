00120bb19f0d5d3513c9127e3dc1f204fbdd95ca
Yota Toyama <raviqqe@gmail.com>
2024-06-26 19:49:36 +0900

    Fix

--- process_context/src/process_context.rs
+++ process_context/src/process_context.rs
@@ -1,4 +1,8 @@
+/// A process context.
 pub trait ProcessContext<'a> {
+    /// Returns a command name and its arguments.
     fn command_line() -> &'a [&'a str];
+
+    /// Returns environment variables.
     fn environment_variables() -> &'a [(&'a str, &'a str)];
 }
