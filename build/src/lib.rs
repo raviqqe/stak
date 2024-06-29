5050a716d5dff3e92244feac24f65726b92fe237
Yota Toyama <raviqqe@gmail.com>
2024-06-29 15:14:24 +0900

    Fix

--- build/src/lib.rs
+++ build/src/lib.rs
@@ -1,11 +1,9 @@
 //! Build scripts for Stak Scheme.
 
 use proc_macro::TokenStream;
-use proc_macro2::Literal;
 use quote::quote;
-use stak_compiler::CompileError;
-use stak_macro_util::{convert_result, read_source_file};
-use std::error::Error;
+use stak_macro_util::convert_result;
+use std::{env, error::Error, path::Path};
 use syn::{parse_macro_input, LitStr};
 
 /// Includes a program in R7RS Scheme as bytecodes.
@@ -13,13 +11,16 @@ use syn::{parse_macro_input, LitStr};
 pub fn include(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as LitStr);
 
-    let foo = env::var("CARGO_MANIFEST_DIR")?;
-
-    quote!(include_bytes!(concat!(env!("OUT_DIR"), #foo)))
+    convert_result(include_result(&input.value())).into()
 }
 
-fn include_result(path: &str) -> Result<TokenStream, Box<dyn Error>> {
-    let manifest_dir = env::var("CARGO_MANIFEST_DIR")?;
+fn include_result(path: &str) -> Result<proc_macro2::TokenStream, Box<dyn Error>> {
+    let path = format!(
+        "{}",
+        Path::new(path)
+            .strip_prefix(Path::new(&env::var("CARGO_MANIFEST_DIR")?).join("src"))?
+            .display()
+    );
 
-    quote!(include_bytes!(concat!(env!("OUT_DIR"), #foo)))
+    Ok(quote!(include_bytes!(concat!(env!("OUT_DIR"), #path))))
 }
