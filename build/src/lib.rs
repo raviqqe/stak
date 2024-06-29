ddf44737e1056adee0419c706042d157d22b474e
Yota Toyama <raviqqe@gmail.com>
2024-06-29 15:06:00 +0900

    Fix

--- build/src/lib.rs
+++ build/src/lib.rs
@@ -1,4 +1,4 @@
-//! Macros to bundle and use Scheme programs.
+//! Build scripts for Stak Scheme.
 
 use proc_macro::TokenStream;
 use proc_macro2::Literal;
@@ -8,83 +8,18 @@ use stak_macro_util::{convert_result, read_source_file};
 use std::error::Error;
 use syn::{parse_macro_input, LitStr};
 
-/// Compiles a program in R7RS Scheme into bytecodes.
-///
-/// # Examples
-///
-/// ```rust
-/// let bytecodes = stak_macro::compile_r7rs!("(define x 42)");
-/// ```
-#[proc_macro]
-pub fn compile_r7rs(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as LitStr);
-
-    convert_result(generate_r7rs(&input.value())).into()
-}
-
 /// Includes a program in R7RS Scheme as bytecodes.
-///
-/// # Examples
-///
-/// ```rust
-/// let bytecodes = stak_macro::include_r7rs!("foo.scm");
-/// ```
 #[proc_macro]
-pub fn include_r7rs(input: TokenStream) -> TokenStream {
+pub fn include(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as LitStr);
 
-    convert_result((|| generate_r7rs(&read_source_file(input)?))()).into()
+    let foo = env::var("CARGO_MANIFEST_DIR")?;
+
+    quote!(include_bytes!(concat!(env!("OUT_DIR"), #foo)))
 }
 
-fn generate_r7rs(source: &str) -> Result<proc_macro2::TokenStream, Box<dyn Error>> {
-    generate_scheme(source, |source, target| {
-        stak_compiler::compile_r7rs(source, target)
-    })
-}
+fn include_result(path: &str) -> Result<TokenStream, Box<dyn Error>> {
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR")?;
 
-/// Compiles a program in Scheme into bytecodes with only built-ins.
-///
-/// # Examples
-///
-/// ```rust
-/// let bytecodes = stak_macro::compile_bare!("($$define x 42)");
-/// ```
-#[proc_macro]
-pub fn compile_bare(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as LitStr);
-
-    convert_result(generate_bare(&input.value())).into()
-}
-
-/// Includes a program in Scheme as bytecodes with only built-ins.
-///
-/// # Examples
-///
-/// ```rust
-/// let bytecodes = stak_macro::include_bare!("foo.scm");
-/// ```
-#[proc_macro]
-pub fn include_bare(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as LitStr);
-
-    convert_result((|| generate_bare(&read_source_file(input)?))()).into()
-}
-
-fn generate_bare(source: &str) -> Result<proc_macro2::TokenStream, Box<dyn Error>> {
-    generate_scheme(source, |source, target| {
-        stak_compiler::compile_bare(source, target)
-    })
-}
-
-fn generate_scheme(
-    source: &str,
-    compile: fn(&[u8], &mut Vec<u8>) -> Result<(), CompileError>,
-) -> Result<proc_macro2::TokenStream, Box<dyn Error>> {
-    let mut target = vec![];
-
-    compile(source.as_bytes(), &mut target)?;
-
-    let target = Literal::byte_string(&target);
-
-    Ok(quote! { #target })
+    quote!(include_bytes!(concat!(env!("OUT_DIR"), #foo)))
 }
