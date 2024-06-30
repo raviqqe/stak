b6ff0df1e686a5a52fc81b6b86af9ddfbab891f2
Yota Toyama <raviqqe@gmail.com>
2024-06-30 17:17:24 +0900

    Fix

--- vm/src/primitive_set.rs
+++ vm/src/primitive_set.rs
@@ -3,7 +3,8 @@ use crate::{memory::Memory, Error};
 /// A primitive set.
 ///
 /// [`PrimitiveSet`](PrimitiveSet)s provide primitive functionalities, such as
-/// arithmetic and I/O, to [`Vm`](Vm)s. Each primitive has its own identifier.
+/// arithmetic and I/O, to [`Vm`](super::Vm)s. Each primitive has its own
+/// identifier.
 pub trait PrimitiveSet: Sized {
     type Error: From<Error>;
 
