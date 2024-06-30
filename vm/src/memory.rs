14d00abffb735a90e26736f8ad480935562a8d66
Yota Toyama <raviqqe@gmail.com>
2024-06-30 17:03:11 +0900

    Fix

--- vm/src/memory.rs
+++ vm/src/memory.rs
@@ -93,7 +93,7 @@ impl<'a> Memory<'a> {
         list
     }
 
-    /// Returns a cons.
+    /// Allocates a cons.
     pub fn cons(&mut self, car: Value, cdr: Cons) -> Result<Cons, Error> {
         self.allocate(car.set_tag(Type::Pair as Tag), cdr.into())
     }
