f88d0fcca9391763b38bced50847d72af6e2b7fe
Yota Toyama <raviqqe@gmail.com>
2024-06-30 23:27:53 +0900

    Fix

--- macro-util/src/lib.rs
+++ macro-util/src/lib.rs
@@ -5,6 +5,7 @@ use quote::quote;
 use std::{env, error::Error, fs::read_to_string, path::Path};
 use syn::LitStr;
 
+/// Reads a source file.
 pub fn read_source_file(path: LitStr) -> Result<String, Box<dyn Error>> {
     Ok(read_to_string(
         Path::new(&env::var("CARGO_MANIFEST_DIR")?)
@@ -13,6 +14,7 @@ pub fn read_source_file(path: LitStr) -> Result<String, Box<dyn Error>> {
     )?)
 }
 
+/// Converts a macro result into a token stream.
 pub fn convert_result(result: Result<TokenStream, Box<dyn Error>>) -> TokenStream {
     result.unwrap_or_else(|error| {
         let message = error.to_string();
