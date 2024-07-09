95213599c83e9a99a2304b33b3e080da8840a27f
Yota Toyama <raviqqe@gmail.com>
2024-07-09 23:22:55 +1000

    Parallel integration tests (#1317)
    - **Run tests in parallel**
    - **Fix**
    - **Fix**

---
+++ tools/cucumber.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+set -e
+
+while getopts t: option; do
+  case $option in
+  t)
+    tags=$OPTARG
+    ;;
+  esac
+done
+
+shift $(expr $OPTIND - 1)
+
+if [ $# -ne 1 ]; then
+  exit 1
+fi
+
+file=$1
+
+cd $(dirname $0)/..
+
+directory=tmp/$(basename ${file%.*})
+
+mkdir -p $directory
+cd $directory
+
+run() {
+  unset $(env | cut -d = -f 1 | gre -v -e PATH)
+
+  bundler exec cucumber \
+    --publish-quiet \
+    --strict-undefined \
+    --require ../../features \
+    ${tags:+--tags "$tags"} \
+    ../../$file
+}
+
+output=$(run 2>&1)
+status=$?
+
+echo "$output"
+
+exit $status
