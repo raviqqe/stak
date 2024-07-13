809056baf8f8223f37dba62c7aaf3c6ba25835f0
Yota Toyama <raviqqe@gmail.com>
2024-07-13 17:24:32 +0900

    Fix

--- cmd/run/src/main.scm
+++ cmd/run/src/main.scm
@@ -9,6 +9,15 @@
   (scheme process-context))
 
 (define (main)
+  (define script
+    (car
+      (member
+        #f
+        (command-line)
+        (lambda (x y)
+          (let ((length (string-length y)))
+            (equal? (substring y (- length 4) length) ".scm"))))))
+
   (do ()
     ((eof-object? (peek-char))
       #f)
