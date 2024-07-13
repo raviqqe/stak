ea45e0d2b178f9b7964303d3badd30e5d98b90e5
Yota Toyama <raviqqe@gmail.com>
2024-07-13 17:15:15 +0900

    Fix

--- cmd/run/src/main.scm
+++ cmd/run/src/main.scm
@@ -10,20 +10,8 @@
   (do ()
     ((eof-object? (peek-char))
       #f)
-    (set! y (+ y (vector-ref xs i))))
-  (do ()
-    (let ((char (peek-char)))
-      (cond
-        ((char-whitespace? char)
-          (read-char)
-          (loop))
-
-        ((or (eof-object? char) (eqv? char (integer->char 4)))
-          #f)
-
-        (else
-          (write (eval (read) (interaction-environment)))
-          (newline)
-          (main))))))
+    (if (char-whitespace? (peek-char))
+      (read-char)
+      (eval (read) (interaction-environment)))))
 
 (main)
