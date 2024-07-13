359297c50af5ad8ce03c164d13ff35f8bf2bdfe5
Yota Toyama <raviqqe@gmail.com>
2024-07-13 17:10:48 +0900

    Fix

---
+++ cmd/run/src/main.scm
@@ -0,0 +1,27 @@
+(import
+  (scheme base)
+  (scheme char)
+  (scheme read)
+  (scheme write)
+  (scheme eval)
+  (scheme repl))
+
+(define (main)
+  (display "> " (current-error-port))
+
+  (do ()
+    (let ((char (peek-char)))
+      (cond
+        ((char-whitespace? char)
+          (read-char)
+          (loop))
+
+        ((or (eof-object? char) (eqv? char (integer->char 4)))
+          #f)
+
+        (else
+          (write (eval (read) (interaction-environment)))
+          (newline)
+          (main))))))
+
+(main)
