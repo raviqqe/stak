f4c3d951594ad97f3d833311cdf9321f380f2184
Yota Toyama <raviqqe@gmail.com>
2024-07-14 16:00:37 +0900

    Fix

--- sac/src/lib.rs
+++ sac/src/lib.rs
@@ -84,11 +84,13 @@ macro_rules! libc_main {
         #![no_std]
         #![cfg_attr(not(test), no_main)]
 
+        use mstak_util::Mmap;
         use $crate::__private::{
             clap::{self, Parser},
+            core::{ffi::CStr, mem::size_of, slice},
             main_error::MainError,
-            stak_device::StdioDevice,
-            stak_file::OsFileSystem,
+            stak_device::libc::{ReadWriteDevice, Stderr, Stdin, Stdout},
+            stak_file::LibcFileSystem,
             stak_macro::include_r7rs,
             stak_primitive::SmallPrimitiveSet,
             stak_process_context::VoidProcessContext,
@@ -96,31 +98,33 @@ macro_rules! libc_main {
             std::{env, error::Error},
         };
 
-        #[derive(clap::Parser)]
-        #[command(about, version)]
-        struct Arguments {
-            #[arg()]
-            arguments: Vec<String>,
-            #[arg(short = 's', long, default_value_t = $heap_size)]
-            heap_size: usize,
-        }
+        #[cfg_attr(not(test), no_mangle)]
+        unsafe extern "C" fn main(argc: isize, argv: *const *const i8) -> isize {
+            let Some(&file) = &slice::from_raw_parts(argv, argc as _).get(1) else {
+                return 1;
+            };
 
-        fn main() -> Result<(), MainError> {
-            let arguments = Arguments::parse();
+            let heap = slice::from_raw_parts_mut(
+                libc::malloc(size_of::<Value>() * HEAP_SIZE) as *mut Value,
+                HEAP_SIZE,
+            );
 
-            let mut heap = vec![Default::default(); arguments.heap_size];
             let mut vm = Vm::new(
-                &mut heap,
+                heap,
                 SmallPrimitiveSet::new(
-                    StdioDevice::new(),
+                    ReadWriteDevice::new(Stdin::new(), Stdout::new(), Stderr::new()),
                     LibcFileSystem::new(),
                     VoidProcessContext::new(),
                 ),
-            )?;
+            )
+            .unwrap();
 
-            vm.initialize(include_r7rs!($path).iter().copied())?;
+            let mmap = Mmap::new(CStr::from_ptr(file));
 
-            Ok(vm.run()?)
+            vm.initialize(mmap.as_slice().iter().copied()).unwrap();
+            vm.run().unwrap();
+
+            0
         }
     };
 }
