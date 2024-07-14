fb5dac9da3342bbfcde3be565dfc600bd6e66c92
Yota Toyama <raviqqe@gmail.com>
2024-07-14 15:39:27 +0900

    Fix

--- sac/src/lib.rs
+++ sac/src/lib.rs
@@ -66,3 +66,56 @@ macro_rules! main {
         }
     };
 }
+
+/// Defines a `main` function that executes a bytecode file at a given path.
+///
+/// The given bytecode file is bundled into a resulting binary.
+#[macro_export]
+macro_rules! libc_main {
+    ($path:expr) => {
+        $crate::main!(
+            $path,
+            $crate::__private::stak_configuration::DEFAULT_HEAP_SIZE
+        );
+    };
+    ($path:expr, $heap_size:expr) => {
+        use $crate::__private::{
+            clap::{self, Parser},
+            main_error::MainError,
+            stak_device::StdioDevice,
+            stak_file::OsFileSystem,
+            stak_macro::include_r7rs,
+            stak_primitive::SmallPrimitiveSet,
+            stak_process_context::OsProcessContext,
+            stak_vm::Vm,
+            std::{env, error::Error},
+        };
+
+        #[derive(clap::Parser)]
+        #[command(about, version)]
+        struct Arguments {
+            #[arg()]
+            arguments: Vec<String>,
+            #[arg(short = 's', long, default_value_t = $heap_size)]
+            heap_size: usize,
+        }
+
+        fn main() -> Result<(), MainError> {
+            let arguments = Arguments::parse();
+
+            let mut heap = vec![Default::default(); arguments.heap_size];
+            let mut vm = Vm::new(
+                &mut heap,
+                SmallPrimitiveSet::new(
+                    StdioDevice::new(),
+                    OsFileSystem::new(),
+                    OsProcessContext::new(),
+                ),
+            )?;
+
+            vm.initialize(include_r7rs!($path).iter().copied())?;
+
+            Ok(vm.run()?)
+        }
+    };
+}
