17c6bfdc8c1570f76701292d6faa55124ef0727a
Yota Toyama <raviqqe@gmail.com>
2024-07-15 18:10:37 +1000

    Libc process context (#1335)
    - **Fix**
    - **Fix**

--- process_context/src/lib.rs
+++ process_context/src/lib.rs
@@ -8,11 +8,15 @@ extern crate alloc;
 #[cfg(feature = "std")]
 extern crate std;
 
+#[cfg(feature = "libc")]
+mod libc;
 #[cfg(feature = "std")]
 mod os;
 mod process_context;
 mod void;
 
+#[cfg(feature = "libc")]
+pub use libc::LibcProcessContext;
 #[cfg(feature = "std")]
 pub use os::OsProcessContext;
 pub use process_context::ProcessContext;
