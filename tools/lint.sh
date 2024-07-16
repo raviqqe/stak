19898934df99fc2011d5fb5100310d626211e368
Yota Toyama <raviqqe@gmail.com>
2024-07-16 23:30:31 +0900

    Fix

--- tools/lint.sh
+++ tools/lint.sh
@@ -4,6 +4,6 @@ set -e
 
 cd $(dirname $0)/..
 
-parallel --trim lr --verbose "cd {1} && cargo clippy $@ {2}" ::: \
+parallel --verbose "cd {1} && cargo clippy $@ {2}" ::: \
   $(tools/workspaces.sh) ::: \
   -- --all-features
