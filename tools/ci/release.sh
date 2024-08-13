e96010c36a52a3508d7fe01d286f98d860518275
Yota Toyama <raviqqe@gmail.com>
2024-08-13 19:10:17 +0900

    Fix

--- tools/ci/release.sh
+++ tools/ci/release.sh
@@ -2,19 +2,37 @@
 
 set -ex
 
+bytecode_file=compiler/src/compile.bc
+
+update_bytecode() {
+  cat prelude.scm compile.scm | stak compile.scm >$bytecode_file
+
+  git config user.email action@github.com
+  git config user.name 'GitHub Action'
+  git add -f $bytecode_file
+}
+
+update_cargo_toml() {
+  for main_file in $(git ls-files '*/main.rs'); do
+    cat <<EOF >>$(dirname $main_file)/Cargo.toml
+[profile.release.build-override]
+opt-level = 3
+debug-assertions = false
+overflow-checks = false
+EOF
+  done
+
+  git add .
+}
+
 if [ $# -ne 0 ]; then
   exit 1
 fi
 
 cargo install cargo-workspaces
 
-bytecode_file=compiler/src/compile.bc
-
-cat prelude.scm compile.scm | stak compile.scm >$bytecode_file
-
-git config user.email action@github.com
-git config user.name 'GitHub Action'
-git add -f $bytecode_file
+update_cargo_toml
+update_bytecode
 git commit -m release
 
 for directory in . cmd/minimal; do
