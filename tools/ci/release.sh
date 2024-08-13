dc1e924e95b91e99753d7636aefb79543e1a7b0b
Yota Toyama <raviqqe@gmail.com>
2024-08-13 23:30:10 +0900

    Fix `Cargo.toml` paths for release jobs

--- tools/ci/release.sh
+++ tools/ci/release.sh
@@ -12,7 +12,7 @@ update_bytecode() {
 
 update_cargo_toml() {
   for main_file in $(git ls-files '*/src/main.rs'); do
-    cat <<EOF >>$(dirname $main_file)/Cargo.toml
+    cat <<EOF >>$(dirname $main_file)/../Cargo.toml
 [profile.release.build-override]
 opt-level = 3
 debug-assertions = false
