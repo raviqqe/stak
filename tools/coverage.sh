102b5a61cbc8db8e14fe697e6997c4641173b3be
Yota Toyama <raviqqe@gmail.com>
2024-08-18 14:29:28 +0900

    Check coverage with release profile

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -5,4 +5,4 @@ set -e
 rustup component add llvm-tools-preview
 
 cargo install cargo-llvm-cov
-cargo llvm-cov --workspace --profile release_test --lcov --output-path lcov.info
+cargo llvm-cov --workspace --profile release --lcov --output-path lcov.info
