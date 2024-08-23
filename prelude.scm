dc5f9493317f815e0fe00e53b1e5648ae5a49c9f
Yota Toyama <raviqqe@gmail.com>
2024-08-23 19:56:01 +0900

    Fix

--- prelude.scm
+++ prelude.scm
@@ -2438,14 +2438,7 @@
 
   (import
     (scheme base)
-    (scheme char)
     (scheme cxr)
-    (scheme file)
-    (scheme inexact)
-    (scheme lazy)
-    (scheme process-context)
-    (scheme read)
-    (scheme write)
     (stak base))
 
   (begin
