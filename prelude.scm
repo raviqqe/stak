daac9ab430649a4037de30fb98a1bb9e6b837610
Yota Toyama <raviqqe@gmail.com>
2024-08-24 13:19:52 +0900

    Fix

--- prelude.scm
+++ prelude.scm
@@ -3121,8 +3121,8 @@
               (if (symbol? (cdr pair))
                 (resolve-denotation macro-context (cdr pair))
                 (make-transformer macro-context (cdr pair)))))
-          ; TODO Use macros from this `(scheme eval)` library's context rather
-          ; than the top level's.
+          ; TODO Use macros from this `(scheme eval)` library's environment rather
+          ; than the top level one?
           ($$macros))
 
         (lambda (expression environment)
