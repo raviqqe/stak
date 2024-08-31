243f69448fdf25f56cfce10393f26f3915d3901d
Yota Toyama <raviqqe@gmail.com>
2024-08-31 22:17:35 +0900

    Fix

--- vm/src/mod_index.rs
+++ vm/src/mod_index.rs
@@ -3,12 +3,20 @@ pub trait ModIndex<T> {
     fn at_mut(&mut self, index: usize) -> &mut T;
 }
 
+fn calculate_slice_index<T>(slice: &[T], index: usize) -> usize {
+    if slice.is_empty() {
+        0
+    } else {
+        index % slice.len()
+    }
+}
+
 impl<T> ModIndex<T> for [T] {
     fn at(&self, index: usize) -> &T {
-        unsafe { &*self.as_ptr().add(index % self.len()) }
+        unsafe { &*self.as_ptr().add(calculate_slice_index(self, index)) }
     }
 
     fn at_mut(&mut self, index: usize) -> &mut T {
-        unsafe { &mut *self.as_mut_ptr().add(index % self.len()) }
+        unsafe { &mut *self.as_mut_ptr().add(calculate_slice_index(self, index)) }
     }
 }
