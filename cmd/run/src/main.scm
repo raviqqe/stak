16bfa8132c26ca9dd858785e9b0f793d3c01ad59
Yota Toyama <raviqqe@gmail.com>
2024-09-14 00:43:13 +0900

    Fix run main

--- cmd/run/src/main.scm
+++ cmd/run/src/main.scm
@@ -1,14 +1,12 @@
 ; We import all libraries here because the compiler strips any unimported libraries.
 (import
-  ; TODO (shake (scheme base))
-  (scheme base)
+  (shake (scheme base))
   (shake (scheme char))
   (only (scheme cxr))
   (only (scheme eval) eval)
   (shake (scheme file))
   (only (scheme inexact))
-  ; TODO (only (scheme lazy))
-  (scheme lazy)
+  (only (scheme lazy))
   (only (scheme process-context) command-line)
   (scheme read)
   (scheme repl)
