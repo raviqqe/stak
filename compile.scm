21b044bf02e8d5eb1bf4f5b260309d341be8e157
Yota Toyama <raviqqe@gmail.com>
2024-09-16 13:12:22 +0900

    Use `min` procedure in a compiler (#1560)

--- compile.scm
+++ compile.scm
@@ -344,10 +344,7 @@
   (if (or
        (not id)
        (let ((name (symbol->string name)))
-         ; TODO (equal? (substring name 0 (min 2 (string-length name))) "$$")
-         (and
-           (> (string-length name) 1)
-           (equal? (substring name 0 2) "$$"))))
+         (equal? (substring name 0 (min 2 (string-length name))) "$$")))
     name
     (let* ((maps (library-context-name-maps context))
            (pair (or (assq id maps) (cons id '())))
