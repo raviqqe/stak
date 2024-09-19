1959ae55eff7a24bced7cea0d4a0dcd1c1172985
Yota Toyama <raviqqe@gmail.com>
2024-09-19 11:26:10 +0900

    Move REPL source file to root (#1567)

---
+++ repl.scm
@@ -0,0 +1,32 @@
+(import
+  (shake (scheme base))
+  (shake (scheme char))
+  (only (scheme cxr))
+  (shake (scheme eval))
+  (only (scheme file))
+  (only (scheme inexact))
+  (only (scheme lazy))
+  (only (scheme process-context))
+  (shake (scheme read))
+  (shake (scheme repl))
+  (shake (scheme write)))
+
+(define (main)
+  (display "> " (current-error-port))
+
+  (let loop ()
+    (let ((char (peek-char)))
+      (cond
+        ((char-whitespace? char)
+          (read-char)
+          (loop))
+
+        ((or (eof-object? char) (eqv? char (integer->char 4)))
+          #f)
+
+        (else
+          (write (eval (read) (interaction-environment)))
+          (newline)
+          (main))))))
+
+(main)
