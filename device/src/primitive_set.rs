5e34d21464645840fc3b5531b5dd1dd303b231d6
Yota Toyama <raviqqe@gmail.com>
2024-09-26 19:48:00 +0900

    Fix

--- device/src/primitive_set.rs
+++ device/src/primitive_set.rs
@@ -1,15 +1,10 @@
-mod error;
-mod primitive;
-
-pub use self::error::Error;
-use self::primitive::Primitive;
-use core::ops::{Add, Div, Mul, Rem, Sub};
-use heapless::Vec;
-use stak_device::Device;
+use crate::Device;
 use stak_vm::{Cons, Memory, Number, NumberRepresentation, PrimitiveSet, Tag, Type, Value};
 
+enum Error {}
+
 /// A primitive set for devices.
-pub struct DevicePrimitiveSet<D: Device> {
+pub struct DevicePrimitiveSet<const READ: u8, const WRITE: u8, const WRITE_ERROR: u8, D: Device> {
     device: D,
 }
 
@@ -190,63 +185,14 @@ impl<D: Device> DevicePrimitiveSet<D> {
     }
 }
 
-impl<D: Device, F: FileSystem, P: ProcessContext> PrimitiveSet for SmallPrimitiveSet<D, F, P> {
+impl<const READ: u8, const WRITE: u8, const WRITE_ERROR: u8, D: Device> PrimitiveSet
+    for DevicePrimitiveSet<READ, WRITE, WRITE_ERROR, D>
+{
     type Error = Error;
 
     fn operate(&mut self, memory: &mut Memory, primitive: u8) -> Result<(), Self::Error> {
         match primitive {
-            Primitive::RIB => {
-                let [r#type, car, cdr, tag] = Self::pop_arguments(memory);
-
-                Self::rib(
-                    memory,
-                    r#type.assume_number().to_i64() as Tag,
-                    car,
-                    cdr.set_tag(tag.assume_number().to_i64() as Tag),
-                )?;
-            }
-            // Optimize a cons.
-            Primitive::CONS => {
-                let [car, cdr] = Self::pop_arguments(memory);
-
-                Self::rib(memory, Type::Pair as Tag, car, cdr)?;
-            }
-            Primitive::CLOSE => {
-                let closure = memory.pop();
-
-                Self::rib(
-                    memory,
-                    Type::Procedure as Tag,
-                    memory.stack().into(),
-                    memory.cdr_value(closure),
-                )?;
-            }
-            Primitive::IS_RIB => Self::operate_top(memory, |memory, value| {
-                memory.boolean(value.is_cons()).into()
-            })?,
-            Primitive::CAR => Self::operate_top(memory, Memory::car_value)?,
-            Primitive::CDR => Self::operate_top(memory, Memory::cdr_value)?,
-            Primitive::TYPE => Self::tag(memory, Memory::car_value)?,
-            Primitive::TAG => Self::tag(memory, Memory::cdr_value)?,
-            Primitive::SET_CAR => Self::set_field(memory, Memory::set_car_value)?,
-            Primitive::SET_CDR => Self::set_field(memory, Memory::set_cdr_value)?,
-            Primitive::EQUAL => {
-                let [x, y] = Self::pop_arguments(memory);
-                memory.push(memory.boolean(x == y).into())?;
-            }
-            Primitive::LESS_THAN => Self::operate_comparison(memory, |x, y| x < y)?,
-            Primitive::ADD => Self::operate_binary(memory, Add::add)?,
-            Primitive::SUBTRACT => Self::operate_binary(memory, Sub::sub)?,
-            Primitive::MULTIPLY => Self::operate_binary(memory, Mul::mul)?,
-            Primitive::DIVIDE => Self::operate_binary(memory, Div::div)?,
-            Primitive::REMAINDER => Self::operate_binary(memory, Rem::rem)?,
-            Primitive::EXPONENTIATION => {
-                Self::operate_unary(memory, |x| Number::from_f64(libm::exp(x.to_f64())))?
-            }
-            Primitive::LOGARITHM => {
-                Self::operate_unary(memory, |x| Number::from_f64(libm::log(x.to_f64())))?
-            }
-            Primitive::READ => {
+            READ => {
                 let byte = self.device.read().map_err(|_| Error::ReadInput)?;
 
                 memory.push(if let Some(byte) = byte {
@@ -255,87 +201,8 @@ impl<D: Device, F: FileSystem, P: ProcessContext> PrimitiveSet for SmallPrimitiv
                     memory.boolean(false).into()
                 })?;
             }
-            Primitive::WRITE => self.write(memory, Device::write, Error::WriteOutput)?,
-            Primitive::WRITE_ERROR => self.write(memory, Device::write_error, Error::WriteError)?,
-            Primitive::HALT => return Err(Error::Halt),
-            // Optimize type checks.
-            Primitive::NULL => Self::check_type(memory, Type::Null)?,
-            Primitive::PAIR => Self::check_type(memory, Type::Pair)?,
-            Primitive::OPEN_FILE => Self::operate_option(memory, |memory| {
-                let [list, output] = Self::pop_arguments(memory);
-                let path = Self::decode_path(memory, list)?;
-                let output = output != memory.boolean(false).into();
-
-                self.file_system
-                    .open(&path, output)
-                    .ok()
-                    .map(|descriptor| Number::new(descriptor as _).into())
-            })?,
-            Primitive::CLOSE_FILE => Self::operate_result(memory, |memory| {
-                let [descriptor] = Self::pop_number_arguments(memory);
-
-                self.file_system.close(descriptor.to_i64() as _)
-            })?,
-            Primitive::READ_FILE => Self::operate_option(memory, |memory| {
-                let [descriptor] = Self::pop_number_arguments(memory);
-
-                self.file_system
-                    .read(descriptor.to_i64() as _)
-                    .ok()
-                    .map(|byte| Number::new(byte as _).into())
-            })?,
-            Primitive::WRITE_FILE => Self::operate_result(memory, |memory| {
-                let [descriptor, byte] = Self::pop_number_arguments(memory);
-
-                self.file_system
-                    .write(descriptor.to_i64() as _, byte.to_i64() as _)
-            })?,
-            Primitive::DELETE_FILE => Self::operate_option(memory, |memory| {
-                let [list] = Self::pop_arguments(memory);
-                let path = Self::decode_path(memory, list)?;
-
-                self.file_system
-                    .delete(&path)
-                    .ok()
-                    .map(|_| memory.boolean(true).into())
-            })?,
-            Primitive::EXISTS_FILE => Self::operate_option(memory, |memory| {
-                let [list] = Self::pop_arguments(memory);
-                let path = Self::decode_path(memory, list)?;
-
-                self.file_system
-                    .exists(&path)
-                    .ok()
-                    .map(|value| memory.boolean(value).into())
-            })?,
-            Primitive::COMMAND_LINE => {
-                memory.set_register(memory.null());
-
-                for argument in self.process_context.command_line_rev() {
-                    let string = Self::build_string(memory, argument)?;
-                    let list = memory.cons(string.into(), memory.register())?;
-                    memory.set_register(list);
-                }
-
-                memory.push(memory.register().into())?;
-            }
-            Primitive::ENVIRONMENT_VARIABLES => {
-                memory.set_register(memory.null());
-
-                for (key, value) in self.process_context.environment_variables() {
-                    let pair = memory.allocate(memory.null().into(), memory.null().into())?;
-                    let list = memory.cons(pair.into(), memory.register())?;
-                    memory.set_register(list);
-
-                    let string = Self::build_string(memory, key)?;
-                    memory.set_car_value(memory.car(memory.register()), string.into());
-
-                    let string = Self::build_string(memory, value)?;
-                    memory.set_cdr_value(memory.car(memory.register()), string.into());
-                }
-
-                memory.push(memory.register().into())?;
-            }
+            WRITE => self.write(memory, Device::write, Error::WriteOutput)?,
+            WRITE_ERROR => self.write(memory, Device::write_error, Error::WriteError)?,
             _ => return Err(Error::Illegal),
         }
 
