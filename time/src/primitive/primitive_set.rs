7981c797915d0568ae8021ba3fbbbdf0c2443176
Yota Toyama <raviqqe@gmail.com>
2024-09-27 21:27:44 +0900

    Fix

--- time/src/primitive/primitive_set.rs
+++ time/src/primitive/primitive_set.rs
@@ -20,7 +20,7 @@ impl<T: Clock> PrimitiveSet for TimePrimitiveSet<T> {
     fn operate(&mut self, memory: &mut Memory, primitive: usize) -> Result<(), Self::Error> {
         match primitive {
             Primitive::CURRENT_JIFFY => {
-                memory.push(self.clock.current_jiffy())?;
+                memory.push(self.clock.current_jiffy().into())?;
             }
             _ => return Err(Error::IllegalPrimitive),
         }
