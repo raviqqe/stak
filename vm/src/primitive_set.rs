d7540b3ef99f8f38790ba4060d3acf627111db2b
Yota Toyama <raviqqe@gmail.com>
2024-09-27 22:55:33 +0900

    Fix

--- vm/src/primitive_set.rs
+++ vm/src/primitive_set.rs
@@ -1,4 +1,5 @@
 use crate::{memory::Memory, Error};
+use core::error;
 
 /// A primitive set.
 ///
@@ -7,7 +8,7 @@ use crate::{memory::Memory, Error};
 /// identifier.
 pub trait PrimitiveSet: Sized {
     /// An error.
-    type Error: From<Error>;
+    type Error: From<Error> + error::Error;
 
     /// Runs a primitive on a virtual machine.
     fn operate(&mut self, memory: &mut Memory, primitive: usize) -> Result<(), Self::Error>;
