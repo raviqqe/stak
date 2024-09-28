558f9fdd1b27f1a8c482519b9847edebed81cdd1
Yota Toyama <raviqqe@gmail.com>
2024-09-28 14:59:42 +0900

    Fix

--- tools/bench.sh
+++ tools/bench.sh
@@ -50,7 +50,7 @@ for file in $(find bench -type f -name '*.scm' | sort | grep $filter); do
 
   cat prelude.scm $file | stak-compile >$base.bc
 
-  scripts="$baseline $base.bc,$candidate $base.bc,gsi $file,gsi $file,chibi-scheme $file,gosh $file"
+  scripts="$baseline $base.bc,$candidate $base.bc, $file,gsi $file,chibi-scheme $file,gosh $file"
 
   if [ -r $base.py ]; then
     scripts="$scripts,python3 $base.py"
