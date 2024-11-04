499987e1d5024bb3b40e8cf1da5f7b6c13c2d6e1
Yota Toyama <raviqqe@gmail.com>
2024-11-04 20:52:58 +0900

    Fix

--- tools/ci/setup.sh
+++ tools/ci/setup.sh
@@ -3,6 +3,6 @@
 set -e
 
 # TODO Install a pre-built `chibi-scheme`.
-brew install --from-source chibi-scheme
+brew install -s chibi-scheme
 brew install gauche guile parallel uutils-coreutils uutils-findutils
 cargo install stak
