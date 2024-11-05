e80ba7e83daada3d0a937de6d29af336d5781fd1
Yota Toyama <raviqqe@gmail.com>
2024-11-06 01:19:06 +0900

    Revert "Fix `chibi-scheme` installation temporarily (#1696)"
    This reverts commit e7fcf8bedc5945cf81471c674ef75910d613c103.

--- tools/ci/setup.sh
+++ tools/ci/setup.sh
@@ -2,7 +2,5 @@
 
 set -e
 
-# TODO Install a pre-built `chibi-scheme`.
-brew install -s chibi-scheme
-brew install gauche guile parallel uutils-coreutils uutils-findutils
+brew install chibi-scheme gauche guile parallel uutils-coreutils uutils-findutils
 cargo install stak
