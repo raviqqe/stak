9abd2738178c41ec3946185a1745ca0c55c93439
Yota Toyama <raviqqe@gmail.com>
2024-11-16 16:38:35 +0900

    Fix

--- cmd/decode/src/main.scm
+++ cmd/decode/src/main.scm
@@ -7,17 +7,46 @@
 (define nop-instruction 4)
 (define call-instruction 5)
 
+(define integer-base 128)
+(define number-base 64)
+(define tag-base 32)
+(define share-base 31)
+
 (define stack '())
 
-(define (decode-number byte)
-  (set! stack (cons byte stack)))
+(define (decode-integer-tail x base)
+  (let loop ((x x)
+             (y (quotient x 2))
+             (base base))
+    (if (even? x)
+      y
+      (let ((x (read-u8)))
+        (loop x (+ y (* base (quotient x 2))) (* base integer-base))))))
+
+(define (decode-number integer)
+  (cond
+    ((even? integer)
+      (quotient integer 2))
+
+    ((even? (quotient integer 2))
+      (- (quotient integer 4)))
+
+    (else
+      (let* ((x (quotient integer 4))
+             (m (* (if (even? x) 1 -1) (quotient x 4096)))
+             (e (- (modulo (quotient integer 2) 2048) 1023)))
+        (* m (expt 2 e))))))
 
 (define (decode-ribs data)
   (do ((byte (read-u8) (read-u8)))
     ((eof-object? byte))
     (cond
       ((even? byte)
-        (set! stack (cons byte stack)))
+        (set!
+          stack
+          (cons
+            (decode-number (decode-integer-tail byte number-base))
+            stack)))
 
       (else
         (write-string "- ")
