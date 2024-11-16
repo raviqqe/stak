5f6ff679db536b33fe84d279cc9b7978a526aced
Yota Toyama <raviqqe@gmail.com>
2024-11-16 12:43:56 +0900

    Fix

--- cmd/decode/src/main.scm
+++ cmd/decode/src/main.scm
@@ -1,1 +1,9 @@
-; TODO Implement a decoder.
+(import (scheme base) (scheme write))
+
+(define (decode-ribs)
+  (do ((byte (read-u8) (read-u8)))
+    ((eof-object? byte))
+    (write byte)
+    (newline)))
+
+(decode-ribs)
