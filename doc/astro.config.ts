d7922fcfc681081fa2a1afa03442bb902d631ed0
Yota Toyama <raviqqe@gmail.com>
2024-12-02 11:23:47 +0900

    Fix

--- doc/astro.config.ts
+++ doc/astro.config.ts
@@ -3,8 +3,8 @@ import { join, parse } from "node:path";
 import sitemap from "@astrojs/sitemap";
 import solid from "@astrojs/solid-js";
 import starlight from "@astrojs/starlight";
-import { capitalize, sortBy } from "es-toolkit";
 import { defineConfig } from "astro/config";
+import { capitalize, sortBy } from "es-toolkit";
 import wasm from "vite-plugin-wasm";
 
 type Item = { label: string; link: string } | { label: string; items: Item[] };
