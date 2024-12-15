7008de56df6974a28f11774a1d854762152d685d
Yota Toyama <raviqqe@gmail.com>
2024-12-15 10:23:55 +0900

    Inline allocation functions

--- vm/src/memory.rs
+++ vm/src/memory.rs
@@ -181,6 +181,7 @@ impl<'a> Memory<'a> {
     }
 
     /// Allocates a cons on heap.
+    #[inline]
     pub fn allocate(&mut self, car: Value, cdr: Value) -> Result<Cons, Error> {
         let mut cons = self.allocate_unchecked(car, cdr)?;
 
@@ -196,6 +197,7 @@ impl<'a> Memory<'a> {
         Ok(cons)
     }
 
+    #[inline]
     fn allocate_unchecked(&mut self, car: Value, cdr: Value) -> Result<Cons, Error> {
         if self.is_out_of_memory() {
             return Err(Error::OutOfMemory);
