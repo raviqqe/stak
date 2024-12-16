53c3db01b1b127f77261328bd0ffca8c205c952b
Yota Toyama <raviqqe@gmail.com>
2024-12-16 16:16:03 +0900

    Fix

--- examples/build/src/main.rs
+++ examples/build/src/main.rs
@@ -1,10 +1,31 @@
 //! A `stak-build` example.
 
+use stak_device::StdioDevice;
+use stak_file::VoidFileSystem;
 use stak_macro::include_bytecode;
+use stak_process_context::VoidProcessContext;
+use stak_r7rs::SmallPrimitiveSet;
+use stak_time::VoidClock;
+use stak_vm::Vm;
+use std::error::Error;
+
+const HEAP_SIZE: usize = 1 << 16;
 
 static BYTECODES: &[u8] = include_bytecode!("main.scm");
 
-fn main() {
-    println!("Hello, world!");
-    dbg!(BYTECODES);
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut heap = vec![Default::default(); HEAP_SIZE];
+    let mut vm = Vm::new(
+        &mut heap,
+        SmallPrimitiveSet::new(
+            StdioDevice::new(),
+            VoidFileSystem::new(),
+            VoidProcessContext::new(),
+            VoidClock::new(),
+        ),
+    )?;
+
+    vm.initialize(include_r7rs!($path).iter().copied())?;
+
+    Ok(vm.run()?)
 }
