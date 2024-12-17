4c0218a973827ab2a9f6add799eafb46a9189575
Yota Toyama <raviqqe@gmail.com>
2024-12-17 09:08:05 +0900

    Fix

--- root/src/lib.rs
+++ root/src/lib.rs
@@ -6,4 +6,28 @@ pub mod vm {
     pub use stak_vm::*;
 }
 
+pub mod device {
+    //! I/O devices.
+
+    pub use stak_device::*;
+}
+
+pub mod file {
+    //! File systems.
+
+    pub use stak_file::*;
+}
+
+pub mod process_context {
+    //! Process context.
+
+    pub use stak_process_context::*;
+}
+
+pub mod time {
+    //! Time measurement.
+
+    pub use stak_time::*;
+}
+
 pub use vm::Vm;
