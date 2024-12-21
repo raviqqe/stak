3177247139a1e0ce5c882dd2e0a1328bef75a171
Yota Toyama <raviqqe@gmail.com>
2024-12-21 21:05:57 +0900

    Fix

--- minifier_macro/tests/main.rs
+++ minifier_macro/tests/main.rs
@@ -5,14 +5,14 @@ use stak_minifier_macro::{include_minified, minify};
 
 #[test]
 fn minify_expressions() {
-    const MODULE: &str = minify!("( foo  bar )\n\n(baz)");
+    const SCRIPT: &str = minify!("( foo  bar )\n\n(baz)");
 
-    assert_eq!(MODULE, "(foo bar)\n(baz)\n");
+    assert_eq!(SCRIPT, "(foo bar)\n(baz)\n");
 }
 
 #[test]
 fn include_minified_expressions() {
-    const MODULE: &str = include_minified!("../tests/foo.scm");
+    const SCRIPT: &str = include_minified!("../tests/foo.scm");
 
-    assert_eq!(MODULE, "(foo bar)\n(baz)\n");
+    assert_eq!(SCRIPT, "(foo bar)\n(baz)\n");
 }
