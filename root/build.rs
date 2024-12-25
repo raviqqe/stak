d9de4266323aa85baf1c984dedf5063235811203
Yota Toyama <raviqqe@gmail.com>
2024-12-25 22:14:45 +0900

    Fix

--- root/build.rs
+++ root/build.rs
@@ -1,11 +1,11 @@
 //! A build script.
 
-#[cfg(any(test, feature = "cli"))]
+#[cfg(any(feature = "cli", test))]
 use stak_build::build_r7rs;
 use stak_build::BuildError;
 
 fn main() -> Result<(), BuildError> {
-    #[cfg(any(test, feature = "cli"))]
+    #[cfg(any(feature = "cli", test))]
     build_r7rs()?;
 
     Ok(())
