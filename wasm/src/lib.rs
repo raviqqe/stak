d359cb0c3e1cf4b38c53fd02057c95736577223e
Yota Toyama <raviqqe@gmail.com>
2024-12-27 19:01:30 +0900

    Fix

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -5,7 +5,7 @@ use stak_device::ReadWriteDevice;
 use stak_file::{MemoryFileSystem, VoidFileSystem};
 use stak_macro::include_module;
 use stak_module::{Module, UniversalModule};
-use stak_process_context::VoidProcessContext;
+use stak_process_context::{MemoryProcessContext, VoidProcessContext};
 use stak_r7rs::SmallPrimitiveSet;
 use stak_time::VoidClock;
 use stak_vm::Vm;
@@ -45,10 +45,12 @@ pub fn interpret(bytecodes: &[u8], input: &[u8], heap_size: usize) -> Result<Vec
 /// Runs a Scheme script with standard input and returns its standard output.
 #[wasm_bindgen]
 pub fn run(source: &str, input: &[u8], heap_size: usize) -> Result<Vec<u8>, JsError> {
+    const MAIN_FILE: &str = "main.scm";
+
     let mut heap = vec![Default::default(); heap_size];
     let mut output = vec![];
     let mut error = vec![];
-    let files = [(b"main.scm".as_slice(), source.as_bytes())];
+    let files = [(MAIN_FILE.as_bytes(), source.as_bytes())];
     let mut file_entries = [Default::default(); 8];
 
     let mut vm = Vm::new(
@@ -56,7 +58,7 @@ pub fn run(source: &str, input: &[u8], heap_size: usize) -> Result<Vec<u8>, JsEr
         SmallPrimitiveSet::new(
             ReadWriteDevice::new(input, &mut output, &mut error),
             MemoryFileSystem::new(&files, &mut file_entries),
-            VoidProcessContext::new(),
+            MemoryProcessContext::new(&["scheme", MAIN_FILE], &[]),
             VoidClock::new(),
         ),
     )?;
