ffa34b384e02e3dae35dc273dd03d4609ed5acba
Yota Toyama <raviqqe@gmail.com>
2024-12-29 20:21:56 +0900

    Fix

--- tools/ci/bencher.sh
+++ tools/ci/bencher.sh
@@ -40,5 +40,34 @@ hyperfine() {
     $hyperfine --export-json results.json "$@"
 }
 
-. tools/utility.sh
-bench
+brew install chibi-scheme gambit-scheme gauche
+cargo install hyperfine
+
+for directory in . cmd/minimal; do
+  (
+    cd $directory
+    cargo build --release $build_options
+  )
+done
+
+export PATH=$PWD/target/release:$PWD/cmd/minimal/target/release:$PATH
+
+filter=.
+
+if [ $# -gt 0 ]; then
+  filter="$@"
+fi
+
+for file in $(find bench -type f -name '*.scm' | sort | grep $filter); do
+  base=${file%.scm}
+
+  cat prelude.scm $file | stak-compile >$base.bc
+
+  scripts="stak $file,mstak $file,stak-interpret $base.bc,mstak-interpret $base.bc,gsi $file,chibi-scheme $file,gosh $file"
+
+  if [ -r $base.py ]; then
+    scripts="$scripts,python3 $base.py"
+  fi
+
+  hyperfine --sort command --input compile.scm -L script "$scripts" "{script}"
+done
