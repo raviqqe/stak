7e417a4d9a134e21a8427c264ec15dd442c8d21f
Yota Toyama <raviqqe@gmail.com>
2024-12-29 23:03:10 +0900

    Fix

--- native/src/list.rs
+++ native/src/list.rs
@@ -32,6 +32,24 @@ impl PrimitiveSet for ListPrimitiveSet {
 
     fn operate(&mut self, memory: &mut Memory, primitive: usize) -> Result<(), Self::Error> {
         match primitive {
+            ListPrimitive::ASSQ => {
+                let [x, xs] = memory.pop_many();
+                let mut xs = xs.assume_cons();
+                let mut y = memory.boolean(false);
+
+                while xs != memory.null() {
+                    let cons = memory.car(xs).assume_cons();
+
+                    if x == memory.car(cons) {
+                        y = cons;
+                        break;
+                    }
+
+                    xs = memory.cdr(xs).assume_cons();
+                }
+
+                memory.push(y.into())?;
+            }
             ListPrimitive::CONS => {
                 let [car, cdr] = memory.pop_many();
 
@@ -46,24 +64,6 @@ impl PrimitiveSet for ListPrimitiveSet {
                 while xs != memory.null() {
                     if x == memory.car(xs) {
                         y = xs;
-                        break;
-                    }
-
-                    xs = memory.cdr(xs).assume_cons();
-                }
-
-                memory.push(y.into())?;
-            }
-            ListPrimitive::ASSQ => {
-                let [x, xs] = memory.pop_many();
-                let mut xs = xs.assume_cons();
-                let mut y = memory.boolean(false);
-
-                while xs != memory.null() {
-                    let cons = memory.car(xs).assume_cons();
-
-                    if x == memory.car(cons) {
-                        y = cons;
                         break;
                     }
 
