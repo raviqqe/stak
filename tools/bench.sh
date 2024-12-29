8dc635a636de91044c8322fac80d32517503a9cf
Yota Toyama <raviqqe@gmail.com>
2024-12-29 16:48:38 +0900

    Bencher (#1910)

--- tools/bench.sh
+++ tools/bench.sh
@@ -16,37 +16,7 @@ if [ $# -ne 0 ]; then
   exit 1
 fi
 
-brew install chibi-scheme gambit-scheme gauche
-
-cargo install hyperfine stak
-
 cd $(dirname $0)/..
 
-for directory in . cmd/minimal; do
-  (
-    cd $directory
-    cargo build --release $build_options
-  )
-done
-
-export PATH=$PWD/target/release:$PWD/cmd/minimal/target/release:$PATH
-
-filter=.
-
-if [ $# -gt 0 ]; then
-  filter="$@"
-fi
-
-for file in $(find bench -type f -name '*.scm' | sort | grep $filter); do
-  base=${file%.scm}
-
-  cat prelude.scm $file | stak-compile >$base.bc
-
-  scripts="stak $file,mstak $file,stak-interpret $base.bc,mstak-interpret $base.bc,gsi $file,chibi-scheme $file,gosh $file"
-
-  if [ -r $base.py ]; then
-    scripts="$scripts,python3 $base.py"
-  fi
-
-  hyperfine --sort command --input compile.scm -L script "$scripts" "{script}"
-done
+. tools/utility.sh
+bench
---
+++ tools/ci/bencher.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+set -e
+
+while getopts b:o: option; do
+  case $option in
+  b)
+    branch=$OPTARG
+    ;;
+  o)
+    os=$OPTARG
+    ;;
+  esac
+done
+
+shift $(expr $OPTIND - 1)
+
+[ -n "$branch" ]
+[ -n "$os" ]
+[ $# -eq 0 ]
+
+cd $(dirname $0)/../..
+
+cargo install hyperfine
+hyperfine=$(which hyperfine)
+
+hyperfine() {
+  bencher run \
+    --adapter shell_hyperfine \
+    --branch $branch \
+    --err \
+    --file results.json \
+    --github-actions $GITHUB_TOKEN \
+    --project stak \
+    --testbed $os \
+    --threshold-max-sample-size 10 \
+    --threshold-measure latency \
+    --threshold-test t_test \
+    --threshold-upper-boundary 0.99 \
+    --thresholds-reset \
+    --token $BENCHER_TOKEN \
+    $hyperfine --export-json results.json "$@"
+}
+
+. tools/utility.sh
+bench
