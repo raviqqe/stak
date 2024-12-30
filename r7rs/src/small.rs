23fa4a4851d348927a633a65ae33b13deb1ba8e2
Yota Toyama <raviqqe@gmail.com>
2024-12-30 20:00:17 +0900

    Fix

--- r7rs/src/small.rs
+++ r7rs/src/small.rs
@@ -26,6 +26,7 @@ impl<D: Device, F: FileSystem, P: ProcessContext, C: Clock> SmallPrimitiveSet<D,
             file: FilePrimitiveSet::new(file_system),
             process_context: ProcessContextPrimitiveSet::new(process_context),
             time: TimePrimitiveSet::new(clock),
+            type_check: TypeCheckPrimitiveSet::new(),
         }
     }
 
