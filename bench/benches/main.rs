dbdaac894331636fa7b7d513d182bce9aa294a30
Yota Toyama <raviqqe@gmail.com>
2025-01-09 17:29:30 +0900

    Embedded benchmarks (#1953)

---
+++ bench/benches/main.rs
@@ -0,0 +1,56 @@
+#![allow(missing_docs)]
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use stak::{
+    device::StdioDevice,
+    file::VoidFileSystem,
+    include_module,
+    module::{Module, UniversalModule},
+    process_context::VoidProcessContext,
+    r7rs::{SmallError, SmallPrimitiveSet},
+    time::VoidClock,
+    vm::Vm,
+};
+
+const HEAP_SIZE: usize = 1 << 18;
+
+static EMPTY_MODULE: UniversalModule = include_module!("empty/main.scm");
+static FIBONACCI_MODULE: UniversalModule = include_module!("fibonacci/main.scm");
+static HELLO_MODULE: UniversalModule = include_module!("hello/main.scm");
+static SUM_MODULE: UniversalModule = include_module!("sum/main.scm");
+static TAK_MODULE: UniversalModule = include_module!("tak/main.scm");
+
+fn run(module: &'static UniversalModule) -> Result<(), SmallError> {
+    let mut heap = [Default::default(); HEAP_SIZE];
+    let mut vm = Vm::new(
+        &mut heap,
+        SmallPrimitiveSet::new(
+            StdioDevice::new(),
+            VoidFileSystem::new(),
+            VoidProcessContext::new(),
+            VoidClock::new(),
+        ),
+    )?;
+
+    vm.initialize(module.bytecode().iter().copied())?;
+    vm.run()
+}
+
+fn stak(criterion: &mut Criterion) {
+    for (name, module) in [
+        ("empty", &EMPTY_MODULE),
+        ("fibonacci", &FIBONACCI_MODULE),
+        ("hello", &HELLO_MODULE),
+        ("sum", &SUM_MODULE),
+        ("tak", &TAK_MODULE),
+    ] {
+        criterion.bench_function(name, |bencher| bencher.iter(|| run(black_box(module))));
+    }
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default().sample_size(10);
+    targets = stak
+}
+criterion_main!(benches);
