52d04e0a6fc960af409d8d0c06fb846b822a1e13
Yota Toyama <raviqqe@gmail.com>
2025-01-10 23:57:40 +0900

    Fix

--- compile.scm
+++ compile.scm
@@ -1517,7 +1517,7 @@
   (when (or
          (member "-h" arguments)
          (member "--help" arguments))
-    (write-string (string-append "Usage: " (car arguments) " < SOURCE_FILE > BYTECODE_FILE"))
+    (write-string (string-append "Usage: " (car arguments) " < SOURCE_FILE > BYTECODE_FILE\n"))
     (exit)))
 
 (main)
