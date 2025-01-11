956d65e21ea5e29dd7a9c8082aaf676775e029c2
Yota Toyama <raviqqe@gmail.com>
2025-01-11 18:49:14 +0900

    Fix

--- tools/ci/setup.sh
+++ tools/ci/setup.sh
@@ -3,5 +3,5 @@
 set -e
 
 brew install --overwrite pkgconf
-brew install chibi-scheme gauche guile parallel uutils-coreutils uutils-findutils
+brew install chibi-scheme gauche guile uutils-coreutils uutils-findutils
 cargo install stak
