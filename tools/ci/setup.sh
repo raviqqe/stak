81f693ad4e6d36a285dde7dd47182b84f20990f3
Yota Toyama <raviqqe@gmail.com>
2025-01-18 22:44:25 +0900

    Fix

--- tools/ci/setup.sh
+++ tools/ci/setup.sh
@@ -2,6 +2,9 @@
 
 set -e
 
-brew install --overwrite pkgconf
-brew install uutils-coreutils uutils-findutils
+if which brew; then
+  brew install --overwrite pkgconf
+  brew install uutils-coreutils uutils-findutils
+fi
+
 cargo install stak
