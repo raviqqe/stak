566f8ccf174899a7e1c5bff14371b42228458750
Yota Toyama <raviqqe@gmail.com>
2025-01-19 15:05:54 +0900

    Fix

--- doc/src/stores/interpreter-demo.ts
+++ doc/src/stores/interpreter-demo.ts
@@ -1,6 +1,5 @@
 import { atom, computed } from "nanostores";
-import { compile as compileProgram } from "../application/compile.js";
-import { interpret as interpretProgram } from "../application/interpret.js";
+import { run as runProgram } from "../application/run.js";
 
 export const source = atom(
   `
@@ -10,67 +9,10 @@ export const source = atom(
   `.trim(),
 );
 
-const bytecodes = atom<Uint8Array | null>(new Uint8Array());
-
-export const bytecodesReady = computed(
-  bytecodes,
-  (bytecodes) => bytecodes?.length,
-);
-
-export const compiling = computed(bytecodes, (output) => output === null);
-
-const output = atom<Uint8Array | null>(new Uint8Array());
-
-export const input = atom("(hello world)");
-export const textOutput = computed(output, (output) =>
-  output === null ? null : new TextDecoder().decode(output),
-);
-
-export const outputUrlStore = computed(output, (output) =>
-  output?.length ? URL.createObjectURL(new Blob([output])) : null,
-);
-
-export const interpretingStore = computed(output, (output) => output === null);
-
-export const compilerError = atom("");
-
-export const interpreterError = atom("");
-
-export const compile = async (): Promise<void> => {
-  bytecodes.set(null);
-  compilerError.set("");
-
-  let value = new Uint8Array();
-
+export const output = computed(source, async (source) => {
   try {
-    value = await compileProgram(source.get());
+    return await runProgram(source);
   } catch (error) {
-    compilerError.set((error as Error).message);
+    return error;
   }
-
-  bytecodes.set(value);
-};
-
-export const interpret = async (): Promise<void> => {
-  const bytecodeValue = bytecodes.get();
-
-  if (!bytecodeValue) {
-    return;
-  }
-
-  output.set(null);
-  interpreterError.set("");
-
-  let outputValue = new Uint8Array();
-
-  try {
-    outputValue = await interpretProgram(
-      bytecodeValue,
-      new TextEncoder().encode(input.get()),
-    );
-  } catch (error) {
-    interpreterError.set((error as Error).message);
-  }
-
-  output.set(outputValue);
-};
+});
