58ac89eb9ab5ab637fbacd280ed569e71761152b
Yota Toyama <raviqqe@gmail.com>
2025-01-19 16:53:49 +0900

    Fix

--- doc/src/application/run/worker.ts
+++ doc/src/application/run/worker.ts
@@ -1,7 +1,7 @@
 import init, { run } from "@raviqqe/stak";
 import { handleWorkerMessage } from "../handle-worker-message";
 
-const heapSize: number = Math.pow(2, 20);
+const heapSize: number = 2 ** 20;
 
 handleWorkerMessage(init, (input: string) =>
   run(input, new Uint8Array(), heapSize),
