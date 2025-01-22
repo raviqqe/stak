4aff0f65dd2494afd41566eccf5418a5719c5396
Yota Toyama <raviqqe@gmail.com>
2025-01-22 18:46:03 +0900

    Fix

--- stak-engine/src/engine.rs
+++ stak-engine/src/engine.rs
@@ -1,3 +1,4 @@
+use stak_device::StdioDevice;
 use stak_file::VoidFileSystem;
 use stak_r7rs::SmallPrimitiveSet;
 use stak_time::VoidClock;
@@ -5,7 +6,18 @@ use stak_vm::Vm;
 
 /// A scripting engine.
 pub struct Engine {
-    vm: Vm<SmallPrimitiveSet<Device, VoidFileSystem, VoidClock>>,
+    vm: Vm<SmallPrimitiveSet<StdioDevice, VoidFileSystem, VoidProcessContext, VoidClock>>,
 }
 
-impl Engine {}
+impl Engine {
+    pub fn new() -> Self {
+        Vm::new(SmallPrimitiveSet::new(
+            Default::default(),
+            Default::default(),
+            Default::default(),
+            Default::default(),
+        ))
+        .map(|vm| Engine { vm })
+        .unwrap()
+    }
+}
