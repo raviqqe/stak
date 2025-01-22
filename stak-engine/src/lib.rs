784b5a498229f5c1d956398bf5915be282483e40
Yota Toyama <raviqqe@gmail.com>
2025-01-22 18:41:42 +0900

    Fix

--- stak-engine/src/lib.rs
+++ stak-engine/src/lib.rs
@@ -1,18 +1,7 @@
+//! Stak Scheme scripting engine for Rust.
+
 #![no_std]
 
 mod engine;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use engine::*;
