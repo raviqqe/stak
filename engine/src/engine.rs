3478ebf5396c59046e406876425c53ee3c7819a6
Yota Toyama <raviqqe@gmail.com>
2025-01-23 19:34:02 +0900

    Fix

--- engine/src/engine.rs
+++ engine/src/engine.rs
@@ -1,38 +1,18 @@
-use cfg_if::cfg_if;
-use stak_file::VoidFileSystem;
 use stak_module::Module;
-use stak_process_context::VoidProcessContext;
-use stak_r7rs::SmallPrimitiveSet;
-use stak_time::VoidClock;
 use stak_vm::{Error, Value, Vm};
 
-// TODO Use `cfg-elif`.
-cfg_if! {
-    if #[cfg(feature = "std")] {
-        type Device = stak_device::StdioDevice;
-    } else {
-        type Device = stak_device::VoidDevice;
-    }
-}
+use crate::primitive_set::ScriptPrimitiveSet;
 
 /// A scripting engine.
-pub struct Engine<'a> {
-    vm: Vm<'a, SmallPrimitiveSet<Device, VoidFileSystem, VoidProcessContext, VoidClock>>,
+pub struct Engine<'a, const N: usize> {
+    vm: Vm<'a, ScriptPrimitiveSet<N>>,
 }
 
 impl<'a> Engine<'a> {
     /// Creates a scripting engine.
     pub fn new(heap: &'a mut [Value]) -> Result<Self, Error> {
         Ok(Self {
-            vm: Vm::new(
-                heap,
-                SmallPrimitiveSet::new(
-                    Default::default(),
-                    Default::default(),
-                    Default::default(),
-                    Default::default(),
-                ),
-            )?,
+            vm: Vm::new(heap, ScriptPrimitiveSet::new())?,
         })
     }
 
--- engine/src/primitive_set.rs
+++ engine/src/primitive_set.rs
@@ -1,15 +1,27 @@
 use stak_device::Device;
 use stak_file::VoidFileSystem;
+use stak_native::dynamic::DynamicPrimitiveSet;
 use stak_process_context::VoidProcessContext;
 use stak_r7rs::SmallPrimitiveSet;
 use stak_time::VoidClock;
 use stak_vm::{Error, Memory, PrimitiveSet, Type};
 
+const DYNAMIC_PRIMITIVE_COUNT: usize = 128;
+
+// TODO Use `cfg-elif`.
+cfg_if! {
+    if #[cfg(feature = "std")] {
+        type Device = stak_device::StdioDevice;
+    } else {
+        type Device = stak_device::VoidDevice;
+    }
+}
+
 /// A type check primitive set.
 #[derive(Debug, Default)]
-pub struct ScriptPrimitiveSet {
+pub struct ScriptPrimitiveSet<const N : usize> {
     small: SmallPrimitiveSet<Device, VoidFileSystem, VoidProcessContext, VoidClock>
-    dynamic: DynamicPrimitiveSet
+    dynamic: DynamicPrimitiveSet<N>
 }
 
 impl ScriptPrimitiveSet {
