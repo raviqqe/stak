38d9980a7d7024a1e0e36ac8f8d7cf050e6ca988
Yota Toyama <raviqqe@gmail.com>
2025-01-23 21:15:01 +0900

    Fix

--- engine/src/fight.scm
+++ engine/src/fight.scm
@@ -4,8 +4,8 @@
 (define make-person (primitive 1000))
 (define throw-pie (primitive 1001))
 
-(define me (make-person 4 0.5))
-(define you (make-person 2 0.2))
+(define me (make-person 4 0.2))
+(define you (make-person 2 0.6))
 
 (throw-pie me you)
 (throw-pie you you)
