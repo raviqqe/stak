490ca1268152d1ee04b40ff9253a5cbb2a1d8cd6
Yota Toyama <raviqqe@gmail.com>
2025-01-28 20:37:10 +0900

    Fix

--- prelude.scm
+++ prelude.scm
@@ -3369,8 +3369,9 @@
           (define-rust "count" 1000 name1 name2 ...))
 
         ((_ "count" index name1 name2 ...)
-          (define name1 (primitive index))
-          (define-rust "count" (+ index 1) name2 ...))
+          (begin
+            (define name1 (primitive index))
+            (define-rust "count" (+ index 1) name2 ...)))
 
         ((_ "count" index)
           #f)))))
