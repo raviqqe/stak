ea73046281494a8cdd6822132906c2c181681cd3
Yota Toyama <raviqqe@gmail.com>
2025-01-28 20:29:43 +0900

    Fix

--- prelude.scm
+++ prelude.scm
@@ -3365,10 +3365,10 @@
   (begin
     (define-syntax define-rust
       (syntax-rules ()
-        ((_ "define" name1 name2 ...)
-          (define "define" 1000 name1 1000))
+        ((_ name1 name2 ...)
+          (define "define" 1000 name1 name2 ...))
 
-        ((_ "define" name1 name2 ...)
+        ((_ "define" index name1 name2 ...)
           (define name1 (primitive 1000)))
 
         ((_ "define")
