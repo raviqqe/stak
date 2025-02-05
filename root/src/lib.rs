ea8faad053944464ad55ad08fd21d6b3abdc6550
Yota Toyama <raviqqe@gmail.com>
2025-02-05 19:40:14 +0900

    Fix

--- root/src/lib.rs
+++ root/src/lib.rs
@@ -3,135 +3,10 @@
 
 pub mod device {
     //! I/O devices.
-
-    pub use stak_device::*;
-}
-
-pub mod dynamic {
-    //! Dynamically-defined primitives.
-
-    pub use stak_dynamic::*;
-}
-
-pub mod engine {
-    //! A scripting engine.
-
-    pub use stak_engine::*;
-}
-
-pub mod file {
-    //! File systems.
-
-    pub use stak_file::*;
-}
-
-pub mod module {
-    //! Modules.
-
-    pub use stak_module::*;
-}
-
-pub mod process_context {
-    //! Process context.
-
-    pub use stak_process_context::*;
-}
-
-pub mod r7rs {
-    //! Primitives for R7RS Scheme.
-
-    pub use stak_r7rs::*;
-}
-
-pub mod sac {
-    //! Standalone complex.
-
-    pub use stak_sac::*;
-}
-
-pub mod time {
-    //! Time measurement.
-
-    pub use stak_time::*;
-}
-
-pub mod vm {
-    //! A virtual machine and its runtime values.
     //!
-    //! # Examples
+    //! ## Examples
     //!
-    //! ## Embedding Scheme scripts
-    //!
-    //! First, prepare a Scheme script named `src/hello.scm`.
-    //!
-    //! ```scheme
-    //! (import (scheme base))
-    //!
-    //! (write-string "Hello, world!\n")
-    //! ```
-    //!
-    //! Then, add a build script at `build.rs` to build the Scheme source file
-    //! into bytecodes.
-    //!
-    //! ```rust no_run
-    //! use stak_build::{build_r7rs, BuildError};
-    //!
-    //! fn main() -> Result<(), BuildError> {
-    //!     build_r7rs()
-    //! }
-    //! ```
-    //!
-    //! Finally, you can include the Scheme script into a Rust program using
-    //! [`stak::include_module`] macro and run the script.
-    //!
-    //! ```rust
-    //! use core::error::Error;
-    //! use stak::{
-    //!     device::StdioDevice,
-    //!     file::VoidFileSystem,
-    //!     include_module,
-    //!     process_context::VoidProcessContext,
-    //!     module::{Module, UniversalModule},
-    //!     r7rs::{SmallError, SmallPrimitiveSet},
-    //!     time::VoidClock,
-    //!     vm::Vm,
-    //! };
-    //!
-    //! const HEAP_SIZE: usize = 1 << 16;
-    //!
-    //! // Include a Scheme script in the bytecode format built by the build script above.
-    //! static MODULE: UniversalModule = include_module!("hello.scm");
-    //!
-    //! fn main() -> Result<(), Box<dyn Error>> {
-    //!     run(&MODULE.bytecode())?;
-    //!
-    //!     Ok(())
-    //! }
-    //!
-    //! fn run(bytecodes: &[u8]) -> Result<(), SmallError> {
-    //!     // Prepare a heap memory of a virtual machine.
-    //!     let mut heap = [Default::default(); HEAP_SIZE];
-    //!     // Create a virtual machine with its heap memory primitive procedures.
-    //!     let mut vm = Vm::new(
-    //!         &mut heap,
-    //!         SmallPrimitiveSet::new(
-    //!             // Attach standard input, output, and error of this process to a virtual machine.
-    //!             StdioDevice::new(),
-    //!             // Use void system interfaces for security because we don't need them for this example.
-    //!             VoidFileSystem::new(),
-    //!             VoidProcessContext::new(),
-    //!             VoidClock::new(),
-    //!         ),
-    //!     )?;
-    //!
-    //!     // Initialize a virtual machine with bytecodes.
-    //!     vm.initialize(bytecodes.iter().copied())?;
-    //!     // Run bytecodes on a virtual machine.
-    //!     vm.run()
-    //! }
-    //! ```
-    //!
-    //! ## Communication via standard I/O
+    //! ### Communication via standard I/O
     //!
     //! You can pass in-memory standard input and output to Scheme scripts for
     //! communicate information between Rust and Scheme.
@@ -199,6 +74,133 @@ pub mod vm {
     //!     )?;
     //!
     //!     vm.initialize(bytecodes.iter().copied())?;
+    //!     vm.run()
+    //! }
+    //! ```
+
+    pub use stak_device::*;
+}
+
+pub mod dynamic {
+    //! Dynamically-defined primitives.
+
+    pub use stak_dynamic::*;
+}
+
+pub mod engine {
+    //! A scripting engine.
+
+    pub use stak_engine::*;
+}
+
+pub mod file {
+    //! File systems.
+
+    pub use stak_file::*;
+}
+
+pub mod module {
+    //! Modules.
+
+    pub use stak_module::*;
+}
+
+pub mod process_context {
+    //! Process context.
+
+    pub use stak_process_context::*;
+}
+
+pub mod r7rs {
+    //! Primitives for R7RS Scheme.
+
+    pub use stak_r7rs::*;
+}
+
+pub mod sac {
+    //! Standalone complex.
+
+    pub use stak_sac::*;
+}
+
+pub mod time {
+    //! Time measurement.
+
+    pub use stak_time::*;
+}
+
+pub mod vm {
+    //! A virtual machine and its runtime values.
+    //!
+    //! # Examples
+    //!
+    //! ## Embedding Scheme scripts in Rust with a custom virtual machine
+    //!
+    //! First, prepare a Scheme script named `src/hello.scm`.
+    //!
+    //! ```scheme
+    //! (import (scheme base))
+    //!
+    //! (write-string "Hello, world!\n")
+    //! ```
+    //!
+    //! Then, add a build script at `build.rs` to build the Scheme source file
+    //! into bytecodes.
+    //!
+    //! ```rust no_run
+    //! use stak_build::{build_r7rs, BuildError};
+    //!
+    //! fn main() -> Result<(), BuildError> {
+    //!     build_r7rs()
+    //! }
+    //! ```
+    //!
+    //! Finally, you can include the Scheme script into a Rust program using
+    //! [`stak::include_module`] macro and run the script.
+    //!
+    //! ```rust
+    //! use core::error::Error;
+    //! use stak::{
+    //!     device::StdioDevice,
+    //!     file::VoidFileSystem,
+    //!     include_module,
+    //!     process_context::VoidProcessContext,
+    //!     module::{Module, UniversalModule},
+    //!     r7rs::{SmallError, SmallPrimitiveSet},
+    //!     time::VoidClock,
+    //!     vm::Vm,
+    //! };
+    //!
+    //! const HEAP_SIZE: usize = 1 << 16;
+    //!
+    //! // Include a Scheme script in the bytecode format built by the build script above.
+    //! static MODULE: UniversalModule = include_module!("hello.scm");
+    //!
+    //! fn main() -> Result<(), Box<dyn Error>> {
+    //!     run(&MODULE.bytecode())?;
+    //!
+    //!     Ok(())
+    //! }
+    //!
+    //! fn run(bytecodes: &[u8]) -> Result<(), SmallError> {
+    //!     // Prepare a heap memory of a virtual machine.
+    //!     let mut heap = [Default::default(); HEAP_SIZE];
+    //!     // Create a virtual machine with its heap memory primitive procedures.
+    //!     let mut vm = Vm::new(
+    //!         &mut heap,
+    //!         SmallPrimitiveSet::new(
+    //!             // Attach standard input, output, and error of this process to a virtual machine.
+    //!             StdioDevice::new(),
+    //!             // Use void system interfaces for security because we don't need them for this example.
+    //!             VoidFileSystem::new(),
+    //!             VoidProcessContext::new(),
+    //!             VoidClock::new(),
+    //!         ),
+    //!     )?;
+    //!
+    //!     // Initialize a virtual machine with bytecodes.
+    //!     vm.initialize(bytecodes.iter().copied())?;
+    //!     // Run bytecodes on a virtual machine.
     //!     vm.run()
     //! }
     //! ```
