360c92df10365b2dce6c4925d566bd5af334615b
Yota Toyama <raviqqe@gmail.com>
2025-03-02 23:26:47 -0800

    Fix

--- tools/decode_test.sh
+++ tools/decode_test.sh
@@ -12,10 +12,11 @@ cd $(dirname $0)/..
 
 for file in $(git ls-files '*.scm' | grep -v prelude); do
   echo '>>>' $file
-  snapshot_file=snapshots/$file
+  snapshot_file=snapshots/${file%.scm}.md
   mkdir -p $(dirname $snapshot_file)
   cat prelude.scm $file | cargo_run stak-compile >main.bc
   cargo_run stak-decode <main.bc >$snapshot_file
 done
 
+npx prettier --write snapshots
 git diff --exit-code
