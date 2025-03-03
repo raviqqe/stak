5c8258408c1b4db5d4ad5e683c278bb0e2152e51
Yota Toyama <raviqqe@gmail.com>
2025-03-02 23:28:39 -0800

    Fix

--- tools/decode_test.sh
+++ tools/decode_test.sh
@@ -2,20 +2,22 @@
 
 set -e
 
-cargo_run() {
-  command=$1
-  shift 1
-  cargo run --release --bin $command -- $@
-}
-
 cd $(dirname $0)/..
 
+cargo build --release
+
+export PATH=$PWD/target/release:$PATH
+
 for file in $(git ls-files '*.scm' | grep -v prelude); do
+  if [ -L $file ]; then
+    continue
+  fi
+
   echo '>>>' $file
   snapshot_file=snapshots/${file%.scm}.md
   mkdir -p $(dirname $snapshot_file)
-  cat prelude.scm $file | cargo_run stak-compile >main.bc
-  cargo_run stak-decode <main.bc >$snapshot_file
+  cat prelude.scm $file | stak-compile >main.bc
+  stak-decode <main.bc >$snapshot_file
 done
 
 npx prettier --write snapshots
