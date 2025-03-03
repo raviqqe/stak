8e39704cd277edb624160a21ace4bcfb70f083b7
Yota Toyama <raviqqe@gmail.com>
2025-03-02 23:20:49 -0800

    Fix

---
+++ tools/decode_test.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+cargo_run() {
+  command=$1
+  shift 1
+  cargo run --release --bin $command -- $@
+}
+
+cd $(dirname $0)/..
+
+for file in $(git ls-files '*.scm' | grep -v prelude); do
+  echo '>>>' $file
+  snapshot_file=snapshots/$file
+  mkdir -p $(dirname $snapshot_file)
+  cat prelude.scm $file | cargo_run stak-compile >main.bc
+  cargo_run stak-decode <main.bc >$snapshot_file
+done
+
+git diff --exit-code
