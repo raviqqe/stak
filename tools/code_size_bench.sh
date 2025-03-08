e0a5eb8bf15f64eed3d78d22159d39524efec197
Yota Toyama <raviqqe@gmail.com>
2025-03-07 23:01:05 -0800

    Fix

--- tools/code_size_bench.sh
+++ tools/code_size_bench.sh
@@ -10,7 +10,7 @@ cargo install tokei
 comby -in-place '... mod tests { ... }' '' .rs
 cargo fmt --all
 
-for cargo_file in $(find . -name Cargo.toml); do
+for cargo_file in $(find . -name Cargo.toml | sort); do
   directory=$(dirname $cargo_file)
 
   echo '>>>' $directory
