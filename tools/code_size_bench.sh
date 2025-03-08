a0358c9020b51238ac8b6bc2886f2b0836a17fdf
Yota Toyama <raviqqe@gmail.com>
2025-03-07 20:59:30 -0800

    Fix

--- tools/code_size_bench.sh
+++ tools/code_size_bench.sh
@@ -11,8 +11,11 @@ cargo install tokei
 cargo fmt --all
 
 for cargo_file in $(find . -name Cargo.toml); do
+  directory=$(dirname $cargo_file)
+
+  echo '>>>' $directory
   (
-    cd $(dirname $cargo_file)
+    cd $directory
     tokei
   )
 done
