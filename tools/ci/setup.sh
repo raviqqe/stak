d5ad00c010b71104aae285f1d1672b05d5a9df4a
Yota Toyama <raviqqe@gmail.com>
2025-03-08 13:13:35 -0800

    Set up LD_LIBRARY_PATH

--- tools/ci/setup.sh
+++ tools/ci/setup.sh
@@ -5,3 +5,5 @@ set -e
 brew install --overwrite pkgconf
 brew install lua@5.4 uutils-coreutils uutils-findutils
 cargo install stak
+
+echo LD_LIBRARY_PATH=$(brew --prefix lua@5.4)/lib:$LD_LIBRARY_PATH >>$GITHUB_ENV
