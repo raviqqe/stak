5fdc0196fe21152b8bde49148d76ebbca098e90d
Yota Toyama <raviqqe@gmail.com>
2025-03-08 20:16:08 -0800

    Fix

--- tools/ci/setup.sh
+++ tools/ci/setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-brew install lua@5.4 uutils-coreutils uutils-findutils
+brew install lua@5.4 pkgconf uutils-coreutils uutils-findutils
 cargo install stak
 
 echo LD_LIBRARY_PATH=$(brew --prefix lua@5.4)/lib:$LD_LIBRARY_PATH >>$GITHUB_ENV
