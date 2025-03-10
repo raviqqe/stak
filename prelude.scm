fa9f4f3b4faaebb1b8a55232fcc81eab168e9ab3
Yota Toyama <raviqqe@gmail.com>
2025-03-09 19:53:16 -0700

    Remove relaxed-map replacement

--- prelude.scm
+++ prelude.scm
@@ -2588,7 +2588,12 @@
             ((not (pair? xs))
               y)))
 
-        ($$relaxed-map)
+        (define (relaxed-map f xs)
+          (if (pair? xs)
+            (cons
+              (f (car xs))
+              (relaxed-map f (cdr xs)))
+            (f xs)))
 
         (define (relaxed-deep-map f xs)
           (if (pair? xs)
