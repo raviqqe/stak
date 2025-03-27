3d0b576c2929bf2fbeb311ebf046a3169c7cc584
Yota Toyama <raviqqe@gmail.com>
2025-03-26 17:01:27 -0700

    Fix

--- tools/r7rs_compatible_compiler_test.sh
+++ tools/r7rs_compatible_compiler_test.sh
@@ -16,11 +16,11 @@ for file in $(list_scheme_files); do
   echo '>>>' $file
 
   for interpreter in $interpreters stak; do
-    echo '>>>>>>' $interpreter $file
+    echo '>>>>>>' $interpreter
     cat prelude.scm $file | $interpreter compile.scm >$interpreter.bc
   done
 
   for interpreter in $interpreters; do
-    diff stak.bc $interpreter.bc
+    log 1 diff stak.bc $interpreter.bc
   done
 done
