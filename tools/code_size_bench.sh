4a4bc61928a00f3b43b3167d2bba4378d40dc6f2
Yota Toyama <raviqqe@gmail.com>
2025-03-26 17:26:18 -0700

    Fix

--- tools/code_size_bench.sh
+++ tools/code_size_bench.sh
@@ -13,7 +13,5 @@ comby -in-place '... mod tests { ... }' '' .rs
 cargo fmt --all
 
 for cargo_file in $(find . -name Cargo.toml | sort); do
-  directory=$(dirname $cargo_file)
-
-  log tokei $directory
+  log tokei $(dirname $cargo_file)
 done
