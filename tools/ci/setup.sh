c0b0124fcf38881165102179a1a3b532c8bbbdfe
Yota Toyama <raviqqe@gmail.com>
2025-03-30 14:05:50 -0700

    Fix

--- tools/ci/setup.sh
+++ tools/ci/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-set -ex
+set -e
 
 [ -n "$CI" ]
 
-if [ $(uname) = Linux -a $(uname -p) = arm ]; then
+if [ $(uname) = Linux -a $(uname -p) = aarch64 ]; then
   sudo apt install chibi-scheme guile-3.0 lua5.4
 else
   brew install chibi-scheme gauche guile lua@5.4 pkgconf uutils-coreutils uutils-findutils
