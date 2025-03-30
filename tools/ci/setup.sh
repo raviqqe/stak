d8ca9f7f59ec14228be5a6ef74f2d2abf7935f1a
Yota Toyama <raviqqe@gmail.com>
2025-03-30 13:29:53 -0700

    Fix

--- tools/ci/setup.sh
+++ tools/ci/setup.sh
@@ -8,8 +8,6 @@ else
   brew install chibi-scheme gauche guile lua@5.4 pkgconf uutils-coreutils uutils-findutils
 fi
 
-# spell-checker: disable-next-line
-go install github.com/shenwei356/rush@latest
 cargo install stak
 
 echo LD_LIBRARY_PATH=$(brew --prefix lua@5.4)/lib:$LD_LIBRARY_PATH >>$GITHUB_ENV
