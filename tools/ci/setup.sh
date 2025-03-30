60ee6e2da89a8da10fcaa41d9823521b81eb333d
Yota Toyama <raviqqe@gmail.com>
2025-03-30 14:01:48 -0700

    Fix

--- tools/ci/setup.sh
+++ tools/ci/setup.sh
@@ -8,8 +8,7 @@ if [ $(uname) = Linux -a $(uname -p) = arm ]; then
   sudo apt install chibi-scheme guile-3.0 lua5.4
 else
   brew install chibi-scheme gauche guile lua@5.4 pkgconf uutils-coreutils uutils-findutils
+  echo LD_LIBRARY_PATH=$(brew --prefix lua@5.4)/lib:$LD_LIBRARY_PATH >>$GITHUB_ENV
 fi
 
 cargo install stak
-
-echo LD_LIBRARY_PATH=$(brew --prefix lua@5.4)/lib:$LD_LIBRARY_PATH >>$GITHUB_ENV
