24734d4631e9483dd36ebccea89b5345d82d5408
Yota Toyama <raviqqe@gmail.com>
2025-04-02 19:25:56 -0700

    Add hello world initialization benchmark

--- bench/benches/embed.rs
+++ bench/benches/embed.rs
@@ -14,6 +14,7 @@ use stak::{
 };
 
 static EMPTY_MODULE: UniversalModule = include_module!("empty/main.scm");
+static HELLO_MODULE: UniversalModule = include_module!("hello/main.scm");
 
 // Interesting observation here is that startup latencies get higher
 // significantly as we increase heap sizes. So we should keep bytecode sizes of
@@ -46,10 +47,24 @@ fn lua_empty(bencher: &mut Bencher) {
     })
 }
 
+fn stak_hello(bencher: &mut Bencher) {
+    bencher.iter(|| {
+        run::<{ 1 << 8 }>(black_box(&HELLO_MODULE)).unwrap();
+    })
+}
+
+fn lua_hello(bencher: &mut Bencher) {
+    bencher.iter(|| {
+        Lua::new().load(black_box("")).exec().unwrap();
+    })
+}
+
 fn embed(criterion: &mut Criterion) {
     for (name, benchmark) in [
         ("stak_empty", stak_empty as fn(&mut Bencher)),
         ("lua_empty", lua_empty),
+        ("stak_hello", stak_hello),
+        ("lua_hello", lua_hello),
     ] {
         criterion.bench_function(&format!("embed_{name}"), benchmark);
     }
