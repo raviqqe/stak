80806102d5fd580119de4386eae81c8f612548d9
Yota Toyama <raviqqe@gmail.com>
2025-04-05 17:26:23 -0700

    Fix typo

--- vm/src/exception.rs
+++ vm/src/exception.rs
@@ -1,7 +1,7 @@
 use crate::Error;
 use core::error;
 
-/// A exception.
+/// An exception.
 pub trait Exception: From<Error> + error::Error {
     /// Returns `true` if an error is critical and not recoverable.
     fn is_critical(&self) -> bool;
