97b75c610bdb0fed0db03c84cfc79a2f506e31ff
Yota Toyama <raviqqe@gmail.com>
2025-04-09 16:42:36 -0700

    Fix

--- vm/src/vm.rs
+++ vm/src/vm.rs
@@ -465,7 +465,7 @@ impl<'a, T: PrimitiveSet> Vm<'a, T> {
                 let cdr = self.memory.pop();
                 let car = self.memory.top();
                 let tag = Self::decode_integer_tail(input, head >> 3, TAG_BASE)?;
-                self.memory.set_raw_car(cons, car);
+                self.memory.set_car(cons, car);
                 self.memory.set_raw_cdr(cons, cdr.set_tag(tag as _));
                 self.memory.set_top(cons.into());
             } else {
