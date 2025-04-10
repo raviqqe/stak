19622462cb331f5f862a82167ee0d890df4ba544
Yota Toyama <raviqqe@gmail.com>
2025-04-09 19:28:54 -0700

    Validate bytecode decoder in Rust

--- vm/src/vm.rs
+++ vm/src/vm.rs
@@ -426,6 +426,8 @@ impl<'a, T: PrimitiveSet> Vm<'a, T> {
     fn decode_ribs(&mut self, input: &mut impl Iterator<Item = u8>) -> Result<Cons, Error> {
         while let Some(head) = input.next() {
             if head & 1 == 0 {
+                debug_assert_ne!(self.memory.stack(), NEVER);
+
                 let cdr = self.memory.pop();
                 let cons = self
                     .memory
@@ -435,10 +437,15 @@ impl<'a, T: PrimitiveSet> Vm<'a, T> {
                 let head = head >> 2;
 
                 if head == 0 {
+                    // TODO Validate the `code` field.
+                    // debug_assert_ne!(self.memory.code(), NEVER);
+
                     let value = self.memory.top();
                     let cons = self.memory.cons(value, self.memory.code())?;
                     self.memory.set_code(cons);
                 } else {
+                    debug_assert_ne!(self.memory.code(), NEVER);
+
                     let integer = Self::decode_integer_tail(input, head - 1, SHARE_BASE)?;
                     let index = integer >> 1;
 
@@ -461,6 +468,8 @@ impl<'a, T: PrimitiveSet> Vm<'a, T> {
                     self.memory.push(value)?;
                 }
             } else if head & 0b100 == 0 {
+                debug_assert_ne!(self.memory.stack(), NEVER);
+
                 let cdr = self.memory.pop();
                 let car = self.memory.pop();
                 let tag = Self::decode_integer_tail(input, head >> 3, TAG_BASE)?;
