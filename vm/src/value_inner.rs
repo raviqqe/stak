913200ff808ad22ac5c8555f06add2171126648e
Yota Toyama <raviqqe@gmail.com>
2025-04-14 17:51:29 -0700

    Fix

--- vm/src/value_inner.rs
+++ vm/src/value_inner.rs
@@ -15,3 +15,17 @@ item::feature!(if ("float62") {
 } else {
     pub(crate) use integer63::*;
 });
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const MAXIMUM_CONS: u64 = 424242;
+
+    #[test]
+    fn cons() {
+        for cons in 0..MAXIMUM_CONS {
+            assert_eq!(unbox_cons(box_cons(cons)), cons);
+        }
+    }
+}
