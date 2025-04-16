ecd9d2dd9b27287d1687895b93cc08ca12ce4cbe
Yota Toyama <raviqqe@gmail.com>
2025-04-15 18:12:44 -0700

    Fix

--- device/src/device/libc/stdio.rs
+++ device/src/device/libc/stdio.rs
@@ -23,7 +23,16 @@ impl Read for Stdin {
         let mut bytes = [0];
 
         Ok(
-            if io::read(stdin(), &mut bytes).map_err(|_| LibcError::Stdin)? == 1 {
+            if io::read(
+                #[allow(unused_unsafe)]
+                unsafe {
+                    stdin()
+                },
+                &mut bytes,
+            )
+            .map_err(|_| LibcError::Stdin)?
+                == 1
+            {
                 Some(bytes[0])
             } else {
                 None
@@ -47,7 +56,14 @@ impl Write for Stdout {
     type Error = LibcError;
 
     fn write(&mut self, byte: u8) -> Result<(), Self::Error> {
-        write(stdout(), byte, LibcError::Stdout)
+        write(
+            #[allow(unused_unsafe)]
+            unsafe {
+                stdout()
+            },
+            byte,
+            LibcError::Stdout,
+        )
     }
 }
 
@@ -66,7 +82,14 @@ impl Write for Stderr {
     type Error = LibcError;
 
     fn write(&mut self, byte: u8) -> Result<(), Self::Error> {
-        write(stderr(), byte, LibcError::Stderr)
+        write(
+            #[allow(unused_unsafe)]
+            unsafe {
+                stderr()
+            },
+            byte,
+            LibcError::Stderr,
+        )
     }
 }
 
