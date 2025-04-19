f5299c50933a838d399cd68585022051b316c897
Yota Toyama <raviqqe@gmail.com>
2025-04-18 18:29:56 -0700

    Fix

--- doc/src/components/CodeEditor.tsx
+++ doc/src/components/CodeEditor.tsx
@@ -11,13 +11,42 @@ export const CodeEditor = ({ id, value, onChange }: Props): JSX.Element => {
   id = id ?? createUniqueId();
 
   onMount(() => {
+    self.MonacoEnvironment = {
+      getWorker: (workerId: string, label: string) => {
+        const getWorkerModule = (_moduleUrl: string, label: string) => {
+          const url = self.MonacoEnvironment?.getWorkerUrl?.(workerId, label);
+
+          if (!url) {
+            throw new Error("No worker URL");
+          }
+
+          return new Worker(url, {
+            name: label,
+            type: "module",
+          });
+        };
+
+        switch (label) {
+          case "scheme":
+            return getWorkerModule(
+              "/monaco-editor/esm/vs/language/scheme/scheme.worker?worker",
+              label,
+            );
+          default:
+            return getWorkerModule(
+              "/monaco-editor/esm/vs/editor/editor.worker?worker",
+              label,
+            );
+        }
+      },
+    };
     const element = document.getElementById(id);
 
     if (!element) {
       throw new Error("editor element not found");
     }
 
-    monaco.editor.create(element, {
+    const editor = monaco.editor.create(element, {
       value: "function hello() {\n\talert('Hello world!');\n}",
       language: "javascript",
     });
