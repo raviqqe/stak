797dda56d96cc90b3445e7474cd17301fee91a04
Yota Toyama <raviqqe@gmail.com>
2025-04-18 20:00:15 -0700

    Track changes in code editor

--- doc/src/components/CodeEditor.tsx
+++ doc/src/components/CodeEditor.tsx
@@ -1,6 +1,8 @@
 import { editor } from "monaco-editor";
 import { type JSX, createMemo, createUniqueId, onMount } from "solid-js";
 
+const language = "scheme";
+
 interface Props {
   class?: string;
   id?: string;
@@ -20,12 +22,16 @@ export const CodeEditor = (props: Props): JSX.Element => {
 
     editor.create(element, {
       automaticLayout: true,
-      language: "scheme",
+      language,
       lineNumbers: "off",
       minimap: { enabled: false },
       theme: "vs-dark",
       value: props.value,
     });
+
+    for (const model of editor.getModels()) {
+      model.onDidChangeContent(() => props.onChange(model.getValue()));
+    }
   });
 
   return <div class={props.class} id={id()} />;
