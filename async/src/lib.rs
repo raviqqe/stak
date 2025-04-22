e6546c1900761e2e939c2b5ba352c6816058b139
Yota Toyama <raviqqe@gmail.com>
2025-04-22 16:46:48 -0700

    Fix

--- async/src/lib.rs
+++ async/src/lib.rs
@@ -1,22 +1,25 @@
 //! Asynchronous context for Stak Scheme.
 
-use bumpalo::Bump;
-use stak_vm::Error;
+use allocator_api2::{alloc::Allocator, boxed::Box};
+use stak_vm::Value;
 
 /// An asynchronous context.
-#[derive(Debug, Default)]
-pub struct AsyncContext {
-    allocator: Bump,
+#[derive(Debug)]
+pub struct AsyncContext<A: Allocator, E> {
+    allocator: A,
+    r#yield: fn(Box<dyn Future<Output = Value>, A>) -> E,
 }
 
-impl AsyncContext {
+impl<A: Allocator, E> AsyncContext<A, E> {
     /// Creates a context.
-    pub fn new() -> Self {
-        Self::default()
+    pub fn new(allocator: A, r#yield: fn(Box<dyn Future<Output = Value>, A>) -> E) -> Self {
+        Self { allocator, r#yield }
     }
 
     /// Yields a future.
-    pub fn r#yield<T: Future>(&self, future: T) -> Result<(), Error> {
-        Err(Error::Yield)
+    pub fn r#yield<T: Future<Output = Value>>(&self, future: T) -> Result<(), E> {
+        let future = Box::<dyn Future<Output = Value>, A>::new_in(future, self.allocator);
+
+        Err((self.r#yield)(future))
     }
 }
