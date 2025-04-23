285b62601655300bb3343e4bf6840371d8c16a35
Yota Toyama <raviqqe@gmail.com>
2025-04-22 19:06:39 -0700

    Fix

--- doc/src/components/CodeEditor.tsx
+++ doc/src/components/CodeEditor.tsx
@@ -1,5 +1,12 @@
 import { editor } from "monaco-editor";
-import { type JSX, createMemo, createUniqueId, onMount } from "solid-js";
+import {
+  type JSX,
+  createEffect,
+  createMemo,
+  createSignal,
+  createUniqueId,
+  onMount,
+} from "solid-js";
 
 interface Props {
   class?: string;
@@ -10,6 +17,9 @@ interface Props {
 
 export const CodeEditor = (props: Props): JSX.Element => {
   const id = createMemo(() => props.id ?? createUniqueId());
+  const [instance, setInstance] = createSignal<
+    ReturnType<typeof editor.create> | undefined
+  >();
 
   onMount(() => {
     const element = document.getElementById(id());
@@ -18,16 +28,20 @@ export const CodeEditor = (props: Props): JSX.Element => {
       throw new Error("Editor element not found");
     }
 
-    editor.create(element, {
-      automaticLayout: true,
-      language: "scheme",
-      lineNumbers: "off",
-      minimap: { enabled: false },
-      theme: "vs-dark",
-      value: props.value,
-    });
+    setInstance(
+      editor.create(element, {
+        automaticLayout: true,
+        language: "scheme",
+        lineNumbers: "off",
+        minimap: { enabled: false },
+        theme: "vs-dark",
+        value: props.value,
+      }),
+    );
+  });
 
-    for (const model of editor.getModels()) {
+  createEffect(() => {
+    for (const model of instance()?.getModel() ?? []) {
       model.onDidChangeContent(() => props.onChange(model.getValue()));
     }
   });
