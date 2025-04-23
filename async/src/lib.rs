f0e111aad1fde8c5db4d016c27edf41eb14a1918
Yota Toyama <raviqqe@gmail.com>
2025-04-22 17:06:36 -0700

    Fix

--- async/src/lib.rs
+++ async/src/lib.rs
@@ -1,26 +1,28 @@
 //! Asynchronous context for Stak Scheme.
 
 use allocator_api2::{alloc::Allocator, boxed::Box};
+use core::pin::Pin;
 use stak_vm::Value;
 
 /// An asynchronous context.
 #[derive(Debug)]
 pub struct AsyncContext<A: Allocator, E> {
     allocator: A,
-    r#yield: fn(Box<dyn Future<Output = Value>, A>) -> E,
+    r#yield: fn(Pin<Box<dyn Future<Output = Value>, A>>) -> E,
 }
 
 impl<A: Allocator, E> AsyncContext<A, E> {
     /// Creates a context.
-    pub fn new(allocator: A, r#yield: fn(Box<dyn Future<Output = Value>, A>) -> E) -> Self {
+    pub fn new(allocator: A, r#yield: fn(Pin<Box<dyn Future<Output = Value>, A>>) -> E) -> Self {
         Self { allocator, r#yield }
     }
 
     /// Yields a future.
-    pub fn r#yield<T: Future<Output = Value>>(&self, future: T) -> Result<(), E> {
-        Err((self.r#yield)(
-            Box::<dyn Future<Output = Value>, A>::new_in(future, self.allocator),
-        ))
+    pub fn r#yield(&self, future: impl Future<Output = Value> + Sized) -> Result<(), E> {
+        let future: Pin<Box<dyn Future<Output = Value>, A>> =
+            Box::pin_in(async move { future.await }, self.allocator);
+
+        Err((self.r#yield)(future))
     }
 }
 
