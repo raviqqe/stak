9a692f9e2f6cbc111cbc1a2ab27bec77c2e39319
Yota Toyama <raviqqe@gmail.com>
2025-04-28 19:13:05 -0700

    Fix

--- doc/src/application/repl/worker.ts
+++ doc/src/application/repl/worker.ts
@@ -1,14 +1,29 @@
 import init, { repl } from "@raviqqe/stak";
 
-await init({});
-
-const input = [];
-
-addEventListener("message", (event) => {
-  input.push(event.data);
+const input = new ReadableStream<number>({
+  start: (controller) =>
+    addEventListener("message", (event: MessageEvent<Uint8Array>) => {
+      for (const byte of event.data) {
+        controller.enqueue(byte);
+      }
+    }),
 });
-const foo = {
-  readInput: () => {},
-  writeOutput: (byte: number) => postMessage(new Uint8Array([byte])),
-};
+const reader = input.getReader();
+
+(window as unknown as { readInput: () => Promise<number> }).readInput =
+  async () => {
+    const result = await reader.read();
+
+    if (result.done) {
+      throw new Error("Input stream closed");
+    }
+
+    return result.value;
+  };
+
+(
+  window as unknown as { writeOutput: (byte: number) => Promise<void> }
+).writeOutput = async (byte: number) => postMessage(new Uint8Array([byte]));
+
+await init({});
 await repl(2 ** 22);
