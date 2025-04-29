fd8b0db2d27713be84b502750daf895005b17394
Yota Toyama <raviqqe@gmail.com>
2025-04-28 19:30:26 -0700

    Fix

--- doc/src/components/ReplDemo.tsx
+++ doc/src/components/ReplDemo.tsx
@@ -1,10 +1,7 @@
 import { useStore } from "@nanostores/solid";
 import { createEffect, createSignal, onMount, type JSX } from "solid-js";
-import styles from "./DemoForm.module.css";
-import { Field } from "./Field";
 import { CodeEditor } from "./CodeEditor";
-import { Label } from "./Label";
-import * as store from "../stores/repl.js"
+import * as store from "../stores/repl.js";
 
 export const ReplDemo = (): JSX.Element => {
   const output = useStore(store.output);
@@ -15,30 +12,23 @@ export const ReplDemo = (): JSX.Element => {
     store.input.set(
       new ReadableStream({
         start: (controller) => {
-          setPutInput(() => (bytes) => controller.enqueue(bytes))
-        }
-      }))
-  })
+          setPutInput(() => (bytes) => controller.enqueue(bytes));
+        },
+      }),
+    );
+  });
 
   createEffect(async () => {
     for await (const chunk of output()) {
-      setValue((value) => value + chunk)
+      setValue((value) => value + chunk);
     }
-  })
-
-
+  });
 
   return (
-    <form class={styles.root}>
-      <Field>
-        <Label for="source">Program</Label>
-        <CodeEditor
-          class={styles.textArea}
-          id="source"
-          onChange={(source) => putInput()?.(new TextEncoder().encode(source))}
-          value={value()}
-        />
-      </Field>
-    </form>
+    <CodeEditor
+      id="source"
+      onChange={(source) => putInput()?.(new TextEncoder().encode(source))}
+      value={value()}
+    />
   );
 };
