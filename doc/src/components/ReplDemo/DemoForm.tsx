f4067f6671a3a68062d47fc019310e4304ea9371
Yota Toyama <raviqqe@gmail.com>
2025-04-29 19:39:28 -0700

    Fix

--- doc/src/components/ReplDemo/DemoForm.tsx
+++ doc/src/components/ReplDemo/DemoForm.tsx
@@ -1,22 +1,38 @@
 import { useStore } from "@nanostores/solid";
-import { type JSX, createEffect, createSignal, onMount } from "solid-js";
+import {
+  type JSX,
+  createEffect,
+  createMemo,
+  createSignal,
+  onMount,
+} from "solid-js";
 import * as store from "../../stores/repl.js";
 import { CodeEditor } from "../CodeEditor";
 import styles from "./DemoForm.module.css";
 
 export const DemoForm = (): JSX.Element => {
   const output = useStore(store.output);
-  const [putInput, setPutInput] = createSignal<(bytes: Uint8Array) => void>();
+  const [sendInput, setSendInput] = createSignal<(bytes: Uint8Array) => void>();
   const [value, setValue] = createSignal("");
+  const putInput = createMemo(() => (input: string) => {
+    setValue((value) => value + input);
+    sendInput()?.(new TextEncoder().encode(input));
+  });
 
   onMount(() => {
     store.input.set(
       new ReadableStream({
         start: (controller) => {
-          setPutInput(() => (bytes) => controller.enqueue(bytes));
+          setSendInput(() => (bytes) => controller.enqueue(bytes));
         },
       }),
     );
+  });
+
+  createEffect(() => {
+    if (value() == "> ") {
+      putInput()?.("(import (scheme base))\n");
+    }
   });
 
   createEffect(() => {
@@ -40,8 +56,7 @@ export const DemoForm = (): JSX.Element => {
           return;
         }
 
-        setValue((value) => value + input);
-        putInput()?.(new TextEncoder().encode(input));
+        putInput()?.(input);
       }}
       value={value()}
     />
