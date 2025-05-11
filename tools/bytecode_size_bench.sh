51aff5659a5c2acb4963a65dd354c3b8d4dce70b
Yota Toyama <raviqqe@gmail.com>
2025-05-10 19:16:36 -0700

    Fix

---
+++ tools/bytecode_size_bench.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+. $(dirname $0)/utility.sh
+
+cd $(dirname $0)/..
+
+cargo build --release
+
+export PATH=$PWD/target/release:$PATH
+
+for file in $(list_scheme_files); do
+  cat prelude.scm $file | stak-compile >main.bc
+  echo '>>>' $file $(stat -f %z main.bc)
+done
+
+npx prettier --write snapshots
+git diff --exit-code
