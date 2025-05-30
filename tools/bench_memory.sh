651b6a4c058e87adabf2d21766de03fde73118fe
Yota Toyama <raviqqe@gmail.com>
2025-05-29 20:37:55 -0700

    Fix

---
+++ tools/bench_memory.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+
+set -ex
+
+build_binary() {
+  (
+    cd $1
+    shift 1
+    cargo build --release
+    cargo build --release "$@"
+  )
+}
+
+setup() {
+  [ $# -le 1 ]
+
+  feature=$1
+
+  brew install chibi-scheme gambit-scheme gauche guile lua micropython mruby ruby
+  cargo install hyperfine
+
+  case $feature in
+  i63)
+    build_options='--no-default-features --features std'
+    ;;
+  f62)
+    build_options='--no-default-features --features std,float62'
+    ;;
+  esac
+
+  build_binary . -p stak -p stak-interpret $build_options
+  build_binary cmd/minimal -p mstak -p mstak-interpret
+
+  export PATH=$PWD/target/release:$PWD/cmd/minimal/target/release:$PATH
+
+  for file in bench/src/*/main.scm; do
+    cat prelude.scm $file | stak-compile >${file%.scm}.bc
+  done
+}
+
+feature=
+
+while getopts f: option; do
+  case $option in
+  f)
+    feature=$OPTARG
+    ;;
+  esac
+done
+
+shift $(expr $OPTIND - 1)
+
+filter=.
+
+if [ $# -gt 0 ]; then
+  filter="$@"
+fi
+
+cd $(dirname $0)/..
+
+. tools/utility.sh
+
+setup $feature
+
+result_directory=$PWD/tmp/bench
+mkdir -p $result_directory
+
+cd bench/src
+
+for file in $(ls */main.scm | sort | grep $filter); do
+  base=${file%.scm}
+
+  scripts="stak $file,mstak $file,stak-interpret $base.bc,mstak-interpret $base.bc,chibi-scheme $file,gosh $file,guile $file"
+  reference=
+
+  if [ $(dirname $base) != eval ]; then
+    scripts="$scripts,gsi $file"
+  fi
+
+  if [ -r $base.py ]; then
+    reference="python3 $base.py"
+    scripts="micropython $base.py,ruby $base.rb,mruby $base.rb,lua $base.lua,$scripts"
+  fi
+
+  hyperfine \
+    --shell none \
+    --warmup 5 \
+    --export-markdown $result_directory/$(dirname $base).md \
+    --input ../../compile.scm \
+    ${reference:+--reference "$reference"} \
+    -L script "$scripts" \
+    "{script}"
+done
