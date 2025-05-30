65de685e8f5c729c37223de54adbcb8ca1505489
Yota Toyama <raviqqe@gmail.com>
2025-05-29 20:42:21 -0700

    Fix

--- tools/bench.sh
+++ tools/bench.sh
@@ -2,42 +2,6 @@
 
 set -ex
 
-build_binary() {
-  (
-    cd $1
-    shift 1
-    cargo build --release
-    cargo build --release "$@"
-  )
-}
-
-setup() {
-  [ $# -le 1 ]
-
-  feature=$1
-
-  brew install chibi-scheme gambit-scheme gauche guile lua micropython mruby ruby
-  cargo install hyperfine
-
-  case $feature in
-  i63)
-    build_options='--no-default-features --features std'
-    ;;
-  f62)
-    build_options='--no-default-features --features std,float62'
-    ;;
-  esac
-
-  build_binary . -p stak -p stak-interpret $build_options
-  build_binary cmd/minimal -p mstak -p mstak-interpret
-
-  export PATH=$PWD/target/release:$PWD/cmd/minimal/target/release:$PATH
-
-  for file in bench/src/*/main.scm; do
-    cat prelude.scm $file | stak-compile >${file%.scm}.bc
-  done
-}
-
 feature=
 
 while getopts f: option; do
@@ -60,7 +24,7 @@ cd $(dirname $0)/..
 
 . tools/utility.sh
 
-setup $feature
+setup_bench $feature
 
 result_directory=$PWD/tmp/bench
 mkdir -p $result_directory
