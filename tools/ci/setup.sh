433183d54906d6f8e187b79adc928bd6ba8e30f9
Yota Toyama <raviqqe@gmail.com>
2025-06-02 19:02:04 -0700

    Fix

--- tools/ci/setup.sh
+++ tools/ci/setup.sh
@@ -6,3 +6,9 @@ brew install lua@5.4 pkgconf uutils-coreutils uutils-findutils
 cargo install stak
 
 echo LD_LIBRARY_PATH=$(brew --prefix lua@5.4)/lib:$LD_LIBRARY_PATH >>$GITHUB_ENV
+
+git clone https://gitlab.com/jobol/tr7
+(
+  cd tr7
+  make install
+)
