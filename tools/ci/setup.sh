e87a30a0af83b1ab57b3363c94535638a1928460
Yota Toyama <raviqqe@gmail.com>
2025-06-02 19:22:21 -0700

    Fix

--- tools/ci/setup.sh
+++ tools/ci/setup.sh
@@ -10,5 +10,6 @@ echo LD_LIBRARY_PATH=$(brew --prefix lua@5.4)/lib:$LD_LIBRARY_PATH >>$GITHUB_ENV
 git clone https://gitlab.com/jobol/tr7 /tmp/tr7
 (
   cd /tmp/tr7
-  make install-bin
+  make
+  cp tr7i /usr/local/bin
 )
