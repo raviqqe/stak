4ca6aa21f5816ad048518d1355df7ac9a810c0c6
Yota Toyama <raviqqe@gmail.com>
2025-06-11 18:58:45 -0700

    Fix

--- compile.scm
+++ compile.scm
@@ -1670,31 +1670,42 @@
 
           ; Library system
 
+          (define (append-imports old-imports new-imports)
+           (fold-left
+            (lambda (names name)
+             (if (member name names)
+              names
+              (cons name names)))
+            old-imports
+            new-imports))
+
           (define expand-libraries
            (let ((context
                   (make-library-context
                    (map-values (lambda (exports) (make-library exports '() '())) ($$libraries))
                    '())))
             (lambda (imports symbol-table expression)
-             (define (import-sets)
-              (map parse-import-set imports))
-
              (case (maybe-car expression)
               ((define-library)
                (add-library-definition! context expression)
-               (values #f '()))
+               (values #f imports))
               ((import)
-               (values
-                (cons
-                 '$$begin
-                 (append
-                  (expand-library-bodies context (map car (import-sets)))
-                  (list #f)))
-                (cdr expression)))
+               (let ((imports (append-imports imports (cdr expression))))
+                (values
+                 (cons
+                  '$$begin
+                  (append
+                   (expand-library-bodies
+                    context
+                    (map car (map parse-import-set imports)))
+                   (list #f)))
+                 imports)))
               (else
                (values
                 (resolve-environment-symbols
-                 (let ((names (collect-imported-names context (import-sets))))
+                 (let ((names (collect-imported-names
+                               context
+                               (map parse-import-set imports))))
                   (lambda (name)
                    (cond
                     ((assq name names) =>
@@ -1702,7 +1713,7 @@
                     (else
                      (string->symbol (symbol->string name) symbol-table)))))
                  expression)
-                '()))))))
+                imports))))))
 
           ; Macro system
 
