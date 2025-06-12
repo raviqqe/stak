0a3f6e5f2083e81a7433e1f54cc68cdcc2290568
Yota Toyama <raviqqe@gmail.com>
2025-06-11 18:34:28 -0700

    Extend `(scheme char)` library

--- prelude.scm
+++ prelude.scm
@@ -2170,8 +2170,46 @@
         ("space" . #\space)
         ("tab" . #\tab)))
 
+    (define (char=? ch1 ch2) (eqv? ch1 ch2))
+    (define (char<? ch1 ch2) (##< (##field0 ch1) (##field0 ch2)))
+    (define (char>? ch1 ch2) (##< (##field0 ch2) (##field0 ch1)))
+    (define (char<=? ch1 ch2) (not (char>? ch1 ch2)))
+    (define (char>=? ch1 ch2) (not (char<? ch1 ch2)))
+
+    (define (char-ci=? ch1 ch2) (char=? (char-upcase ch1) (char-upcase ch2)))
+    (define (char-ci<? ch1 ch2) (char<? (char-upcase ch1) (char-upcase ch2)))
+    (define (char-ci>? ch1 ch2) (char>? (char-upcase ch1) (char-upcase ch2)))
+    (define (char-ci<=? ch1 ch2) (not (char-ci>? ch1 ch2)))
+    (define (char-ci>=? ch1 ch2) (not (char-ci<? ch1 ch2)))
+
+    (define (char-alphabetic? ch)
+      (or (char-lower-case? ch)
+        (char-upper-case? ch)))
+
+    (define (char-numeric? ch)
+      (and (##< 47 (##field0 ch)) ;; #\0
+        (##< (##field0 ch) 58))) ;; #\9
+
     (define (char-whitespace? x)
-      (memv x '(#\newline #\return #\space #\tab)))))
+      (memv x '(#\newline #\return #\space #\tab)))
+
+    (define (char-lower-case? ch)
+      (and (##< 96 (##field0 ch)) ;; #\a
+        (##< (##field0 ch) 123))) ;; #\z
+
+    (define (char-upper-case? ch)
+      (and (##< 64 (##field0 ch)) ;; #\A
+        (##< (##field0 ch) 91))) ;; #\Z
+
+    (define (char-upcase ch)
+      (if (char-lower-case? ch)
+        (integer->char (##- (##field0 ch) 32)) ; (- (##field0 #\A) (##field0 #\a))
+        ch))
+
+    (define (char-downcase ch)
+      (if (char-upper-case? ch)
+        (integer->char (##+ (##field0 ch) 32)) ; (- (##field0 #\a) (##field0 #\A))
+        ch))))
 
 (define-library (scheme read)
   (export read)
