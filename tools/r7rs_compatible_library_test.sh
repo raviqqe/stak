0b516c3eddd19bc6b63f4e127291bc20479b9508
Yota Toyama <raviqqe@gmail.com>
2025-07-06 11:44:03 -0700

    Fix

--- tools/r7rs_compatible_library_test.sh
+++ tools/r7rs_compatible_library_test.sh
@@ -6,9 +6,10 @@ directory=$(dirname $0)
 library_directory=$directory/r7rs
 
 for file in $(ls $library_directory); do
+  echo library: $file
   for name in $(cat $library_directory/$file | grep -v '^\*$'); do
     if ! grep $name $directory/../prelude.scm >/dev/null; then
-      echo missing symbol: $name
+      echo "\t$name"
     fi
   done
 done
