0fb33df96479fd860f03dab5fd713fcca264701d
Yota Toyama <raviqqe@gmail.com>
2025-07-06 11:44:38 -0700

    Fix

--- tools/r7rs_compatible_library_test.sh
+++ tools/r7rs_compatible_library_test.sh
@@ -6,10 +6,10 @@ directory=$(dirname $0)
 library_directory=$directory/r7rs
 
 for file in $(ls $library_directory); do
-  echo library: $file
+  echo $file
   for name in $(cat $library_directory/$file | grep -v '^\*$'); do
     if ! grep $name $directory/../prelude.scm >/dev/null; then
-      echo "\t$name"
+      echo "  $name"
     fi
   done
 done
