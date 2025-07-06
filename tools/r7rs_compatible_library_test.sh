31d6ab488999a37c36401c638dbc6d227413c7e4
Yota Toyama <raviqqe@gmail.com>
2025-07-06 12:21:00 -0700

    Fix

--- tools/r7rs_compatible_library_test.sh
+++ tools/r7rs_compatible_library_test.sh
@@ -8,7 +8,7 @@ library_directory=$directory/r7rs
 for file in $(ls $library_directory); do
   echo $(echo ${file%.scm} | sed s/_/-/g)
 
-  for name in $(cat $library_directory/$file | grep -v '^\*$'); do
+  for name in $(cat $library_directory/$file | grep -v -e '^\*$' -e include); do
     if ! grep $name $directory/../prelude.scm >/dev/null; then
       echo "  $name"
     fi
