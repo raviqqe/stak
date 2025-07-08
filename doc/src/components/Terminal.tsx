85b606baea3f9a28693269698ca2bc594ec80bc9
Yota Toyama <raviqqe@gmail.com>
2025-07-07 19:24:38 -0700

    Fix

--- doc/src/components/Terminal.tsx
+++ doc/src/components/Terminal.tsx
@@ -1,9 +1,9 @@
-import { onMount, type JSX } from "solid-js";
+import { createEffect, onMount, type JSX } from "solid-js";
 import * as xterm from "@xterm/xterm";
 
 interface Props {
-  input: ReadableStream;
-  output: ReadableStream;
+  input: ReadableStream<Uint8Array>;
+  output: ReadableStream<Uint8Array>;
 }
 
 export const Terminal = (props: Props): JSX.Element => {
@@ -14,6 +14,26 @@ export const Terminal = (props: Props): JSX.Element => {
     if (element) {
       terminal.open(element);
     }
+  });
+
+  createEffect(() => {
+    const decoder = new TextDecoder();
+
+    void (async (input: ReadableStream<Uint8Array>) => {
+      for await (const data of input) {
+        terminal.input(decoder.decode(data));
+      }
+    })(props.input);
+  });
+
+  createEffect(() => {
+    const decoder = new TextDecoder();
+
+    void (async (output: ReadableStream<Uint8Array>) => {
+      for await (const data of output) {
+        terminal.write(decoder.decode(data));
+      }
+    })(props.output);
   });
 
   return (
