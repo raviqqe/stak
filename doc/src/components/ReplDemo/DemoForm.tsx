c3c4b6ee37917847163a8598e8e19399eb5b6d3c
Yota Toyama <raviqqe@gmail.com>
2025-07-11 11:21:53 +0900

    REPL in Xterm.js (#2644)

--- doc/src/components/ReplDemo/DemoForm.tsx
+++ doc/src/components/ReplDemo/DemoForm.tsx
@@ -1,93 +1,40 @@
 import { useStore } from "@nanostores/solid";
-import {
-  createEffect,
-  createMemo,
-  createSignal,
-  type JSX,
-  onMount,
-} from "solid-js";
+import { type JSX, onMount } from "solid-js";
 import * as store from "../../stores/repl.js";
-import { CodeEditor } from "../CodeEditor";
 import { Field } from "../Field.js";
 import { Label } from "../Label.js";
+import { Terminal } from "../Terminal.js";
 import styles from "./DemoForm.module.css";
 
-const prompt = "> ";
-const promptPattern = new RegExp(prompt, "g");
 const source = [
   "(import (scheme base) (scheme write))",
   '(write-string "Hello, world!\\n")',
-  "(define (fibonacci x) (if (< x 2) x (+ (fibonacci (- x 1)) (fibonacci (- x 2)))))",
+  "(define (fibonacci x)",
+  "\t(if (< x 2)",
+  "\t\tx",
+  "\t\t(+",
+  "\t\t\t(fibonacci (- x 1))",
+  "\t\t\t(fibonacci (- x 2))))) ",
   "(fibonacci 10)",
 ];
 
 export const DemoForm = (): JSX.Element => {
+  const input = new TransformStream<string, string>();
   const output = useStore(store.output);
-  const [sendInput, setSendInput] = createSignal<(bytes: Uint8Array) => void>();
-  const [value, setValue] = createSignal("");
-  const putInput = createMemo(() => {
-    const send = sendInput();
 
-    return (input: string) => {
-      setValue((value) => value + input);
-      send?.(new TextEncoder().encode(input));
-    };
-  });
-
-  onMount(() => {
-    store.input.set(
-      new ReadableStream({
-        start: (controller) => {
-          setSendInput(() => (bytes) => controller.enqueue(bytes));
-        },
-      }),
-    );
-  });
-
-  createEffect(() => {
-    const put = putInput();
-
-    for (const [index, line] of source.entries()) {
-      if (
-        value().match(promptPattern)?.length === index + 1 &&
-        value().endsWith(prompt)
-      ) {
-        put(`${line}\n`);
-      }
-    }
-  });
-
-  createEffect(() => {
-    void (async (output) => {
-      const decoder = new TextDecoder();
-
-      for await (const chunk of output) {
-        setValue((value) => value + decoder.decode(chunk));
-      }
-    })(output());
-  });
+  onMount(() =>
+    store.input.set(input.readable.pipeThrough(new TextEncoderStream())),
+  );
 
   return (
     <form class={styles.root}>
       <Field style={{ flex: 1 }}>
         <Label for="repl">Try it out!</Label>
-        <CodeEditor
-          autoBrackets={false}
-          class={styles.repl}
+        <Terminal
           id="repl"
-          onChange={(newValue) => {
-            const input = newValue.slice(value().length);
-
-            if (
-              !input.includes("\n") &&
-              input.match("(")?.length === input.match(")")?.length
-            ) {
-              return;
-            }
-
-            putInput()(input);
-          }}
-          value={value()}
+          initialInput={source}
+          input={input.writable}
+          output={output().pipeThrough(new TextDecoderStream())}
         />
       </Field>
     </form>
---
+++ doc/src/components/Terminal.tsx
@@ -0,0 +1,89 @@
+import * as xterm from "@xterm/xterm";
+import { createEffect, type JSX, onMount } from "solid-js";
+import "@xterm/xterm/css/xterm.css";
+import { createResizeObserver } from "@solid-primitives/resize-observer";
+import { FitAddon } from "@xterm/addon-fit";
+import { delay } from "es-toolkit";
+import styles from "./Terminal.module.css";
+
+const inputDelay = 100;
+const terminalOptions: xterm.ITerminalOptions = {
+  lineHeight: 1.1,
+  tabStopWidth: 2,
+};
+
+interface Props {
+  id?: string;
+  initialInput?: string[];
+  input: WritableStream<string>;
+  output: ReadableStream<string>;
+}
+
+export const Terminal = (props: Props): JSX.Element => {
+  const terminal = new xterm.Terminal(terminalOptions);
+  const fitAddon = new FitAddon();
+  terminal.loadAddon(fitAddon);
+
+  let element: HTMLDivElement | null = null;
+
+  onMount(() => {
+    if (!element) {
+      return;
+    }
+
+    terminal.open(element);
+    fitAddon.fit();
+    createResizeObserver(element, () => fitAddon.fit());
+  });
+
+  const line: string[] = [];
+
+  createEffect(() => {
+    const writer = props.input.getWriter();
+
+    terminal.onData(async (data) => {
+      if (data === "\r") {
+        await writer.write([...line.splice(0), "\n"].join(""));
+        terminal.write("\r\n");
+      } else if (data === "\x7f") {
+        if (line.length) {
+          line.pop();
+          terminal.write("\b \b");
+        }
+      } else {
+        line.push(data);
+        terminal.write(data);
+      }
+    });
+
+    void (async (initialInput: string[]) => {
+      for (const line of initialInput) {
+        await delay(inputDelay);
+
+        for (const character of line) {
+          terminal.input(character);
+        }
+
+        terminal.input("\r");
+      }
+    })(props.initialInput ?? []);
+  });
+
+  createEffect(() => {
+    void (async (output: ReadableStream<string>) => {
+      for await (const data of output) {
+        terminal.write(data === "\n" ? "\r\n" : data);
+      }
+    })(props.output);
+  });
+
+  return (
+    <div
+      class={styles.root}
+      id={props.id}
+      ref={(value) => {
+        element = value;
+      }}
+    />
+  );
+};
