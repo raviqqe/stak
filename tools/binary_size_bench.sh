0b3dc6c942c55c046b2708f48a54460f16296b58
Yota Toyama <raviqqe@gmail.com>
2025-07-13 12:02:09 -0700

    Fix

--- tools/binary_size_bench.sh
+++ tools/binary_size_bench.sh
@@ -2,17 +2,31 @@
 
 set -e
 
-[ -n "$CI" ]
+build_chibi() (
+  cd tmp
+  git clone https://github.com/ashinn/chibi-scheme
+  cd chibi-scheme
+  make chibi-scheme-static
+)
+
+build_stak() (
+  cargo build --release
+
+  (
+    cd cmd/minimal
+    cargo build --release
+  )
+)
 
 . $(dirname $0)/utility.sh
 
 cd $(dirname $0)/..
+mkdir -p tmp
 
-cargo build --release
+build_chibi
+build_stak
 
-export PATH=$PWD/target/release:$PATH
-
-for file in $(list_scheme_files); do
-  cat prelude.scm $file | stak-compile >main.bc
-  echo '>>>' "$file:\t$(wc -c <main.bc)"
-done
+ls -l \
+  target/release/stak \
+  cmd/minimal/target/release/mstak \
+  tmp/chibi-scheme/chibi-scheme-static
