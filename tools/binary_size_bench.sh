21b8824f4f085441958ff9dd083498432bf4c1b4
Yota Toyama <raviqqe@gmail.com>
2025-07-12 21:15:36 -0700

    Binary size benchmarks

---
+++ tools/binary_size_bench.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+. $(dirname $0)/utility.sh
+
+cd $(dirname $0)/..
+
+cargo build --release
+
+export PATH=$PWD/target/release:$PATH
+
+for file in $(list_scheme_files); do
+  cat prelude.scm $file | stak-compile >main.bc
+  echo '>>>' "$file:\t$(wc -c <main.bc)"
+done
