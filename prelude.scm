033da5c9f7f8122a4a9500597e0ec7a5fea2ad78
Yota Toyama <raviqqe@gmail.com>
2025-07-26 18:05:20 -0700

    `read-string` procedure (#2704)

--- prelude.scm
+++ prelude.scm
@@ -1580,6 +1580,7 @@
     read-char
     peek-char
     char-ready?
+    read-string
 
     write-u8
     write-char
@@ -1781,6 +1782,16 @@
         (or
           (not (eof-object? (peek-char port)))
           (eof-object? (peek-u8 port)))))
+
+    (define (read-string count . rest)
+      (define port (get-input-port rest))
+
+      (list->string
+        (let loop ((count count))
+          (let ((x (read-char port)))
+            (if (or (eof-object? x) (zero? count))
+              '()
+              (cons x (loop (- count 1))))))))
 
     ; Write
 
@@ -2409,6 +2420,7 @@
     read-char
     peek-char
     char-ready?
+    read-string
 
     write-u8
     write-char
@@ -2673,7 +2685,10 @@
 (define-library (scheme read)
   (export read)
 
-  (import (scheme base) (scheme char) (only (stak base) boolean-or))
+  (import
+    (scheme base)
+    (scheme char)
+    (only (stak base) boolean-or))
 
   (begin
     (define (read . rest)
