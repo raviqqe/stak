e8d93f282b6a8a2d20892506701dc9e1c7ae4517
Yota Toyama <raviqqe@gmail.com>
2025-08-02 12:32:16 -0700

    Fix

--- tools/ci/setup.sh
+++ tools/ci/setup.sh
@@ -7,7 +7,7 @@ cargo install stak
 # Download the `agoa` command by running it.
 # https://github.com/actions/setup-go/issues/358
 go tool agoa --version
-go tool gherkin-format --version
+go tool gherkin-format -version
 go tool gherkin2markdown --version
 
 echo LD_LIBRARY_PATH=$(brew --prefix lua@5.4)/lib:$LD_LIBRARY_PATH >>$GITHUB_ENV
