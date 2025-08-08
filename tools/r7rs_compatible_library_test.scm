0199e1037761c9cd1d9cf2966902488e09db8a2a
Yota Toyama <raviqqe@gmail.com>
2025-08-07 21:04:48 -0700

    Fix

--- tools/r7rs_compatible_library_test.scm
+++ tools/r7rs_compatible_library_test.scm
@@ -1,49 +1,45 @@
 (import
   (scheme base)
+  (scheme cxr)
   (scheme file)
   (scheme read)
   (scheme write)
   (srfi 1))
 
-(define basenames
-  '(base
-    case-lambda
-    char
-    complex
-    cxr
-    eval
-    file
-    inexact
-    lazy
-    load
-    process-context
-    read
-    repl
-    time
-    write))
-
 (define (read-libraries)
-  (filter
-    (lambda (value)
-      (eq? (car value) 'define-library))
-    (with-input-from-file "prelude.scm"
+  (map
+    (lambda (library)
+      (cons
+        (cadr library)
+        (append-map
+          (lambda (clause)
+            (if (and
+                 (pair? clause)
+                 (eq? (car clause) 'export))
+              (cdr clause)
+              '()))
+          library)))
+    (filter
+      (lambda (value)
+        (and
+          (eq? (car value) 'define-library)
+          (eq? (caadr value) 'scheme)))
+      (with-input-from-file "prelude.scm"
+        (lambda ()
+          (let loop ((value (read)))
+            (if (eof-object? value)
+              '()
+              (cons value (loop (read))))))))))
+
+(for-each
+  (lambda (library)
+    (with-input-from-file (string-append
+                           "tools/r7rs/"
+                           (symbol->string (cadar library))
+                           ".scm")
       (lambda ()
-        (let loop ((value (read)))
-          (if (eof-object? value)
-            '()
-            (cons value (loop (read)))))))))
-
-; (for-each
-;   (lambda (basename)
-;     (with-input-from-file (string-append
-;                            "tools/r7rs/"
-;                            (symbol->string basename)
-;                            ".scm")
-;       (lambda ()
-;         (do ((value (read) (read)))
-;           ((eof-object? value))
-;           (write value)
-;           (newline)))))
-;   basenames)
-
-(write (read-libraries))
+        (do ((value (read) (read)))
+          ((eof-object? value))
+          (write value)
+          (newline)))))
+  (read-libraries))
