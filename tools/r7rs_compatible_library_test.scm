20c91e09641b210a74abe75d032d14b745ee74ab
Yota Toyama <raviqqe@gmail.com>
2025-08-07 20:06:48 -0700

    Fxi

--- tools/r7rs_compatible_library_test.scm
+++ tools/r7rs_compatible_library_test.scm
@@ -1,16 +1,16 @@
-#!/bin/sh
-
-set -e
-
-directory=$(dirname $0)
-library_directory=$directory/r7rs
-
-for file in $(ls $library_directory); do
-  echo $(echo ${file%.scm} | sed s/_/-/g)
-
-  for name in $(cat $library_directory/$file | grep -v -e '^\*$' -e include); do
-    if ! grep $name $directory/../prelude.scm >/dev/null; then
-      echo "  $name"
-    fi
-  done
-done
+(define basenames
+  '(base
+    case-lambda
+    char
+    complex
+    cxr
+    eval
+    file
+    inexact
+    lazy
+    load
+    process-context
+    read
+    repl
+    time
+    write))
