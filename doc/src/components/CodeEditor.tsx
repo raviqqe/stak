ece838fd3bdffabc725c0e938880f69ac42a717b
Yota Toyama <raviqqe@gmail.com>
2025-08-15 23:19:03 -0700

    Fix

--- doc/src/components/CodeEditor.tsx
+++ doc/src/components/CodeEditor.tsx
@@ -1,8 +1,7 @@
-import { Editor } from "monza-editor";
-import { createEffect, type JSX, onMount } from "solid-js";
+import { Editor } from "@monza-editor/solid";
+import { type JSX } from "solid-js";
 
 interface Props {
-  autoBrackets?: boolean;
   class?: string;
   id?: string;
   onChange: (text: string) => void;
@@ -10,31 +9,5 @@ interface Props {
 }
 
 export const CodeEditor = (props: Props): JSX.Element => {
-  onMount(() => {
-    instance = editor.create(element, {
-      autoClosingBrackets: props.autoBrackets === false ? "never" : undefined,
-      automaticLayout: true,
-      language: "scheme",
-      lineNumbers: "off",
-      minimap: { enabled: false },
-      theme: "vs-dark",
-      value: props.value,
-    });
-    const model = instance.getModel();
-    const onChange = props.onChange;
-
-    model?.onDidChangeContent(() => onChange(model.getValue()));
-  });
-
-  createEffect(() => {
-    if (props.value) {
-      instance?.setValue(props.value);
-      instance?.setPosition({
-        column: Number.POSITIVE_INFINITY,
-        lineNumber: Number.POSITIVE_INFINITY,
-      });
-    }
-  });
-
-  return <Editor class={props.class} />;
+  return <Editor class={props.class} {...props} />;
 };
