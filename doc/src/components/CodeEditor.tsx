4b0b7be6d3675617d30a839487e9308f036f3b9a
Yota Toyama <raviqqe@gmail.com>
2025-08-16 11:48:50 -0700

    Fix

--- doc/src/components/CodeEditor.tsx
+++ doc/src/components/CodeEditor.tsx
@@ -2,6 +2,13 @@ import { Editor } from "@monza-editor/solid";
 import classNames from "classnames";
 import type { JSX } from "solid-js";
 import styles from "./CodeEditor.module.css";
+import { createHighlighterCore, createJavaScriptRegexEngine } from "shiki";
+
+const highlighter = await createHighlighterCore({
+  langs: [import("@shikijs/langs/scheme")],
+  themes: [import("@shikijs/themes/nord")],
+  engine: createJavaScriptRegexEngine(),
+});
 
 interface Props {
   class?: string;
@@ -10,11 +17,19 @@ interface Props {
   value?: string;
 }
 
-export const CodeEditor = (props: Props): JSX.Element => (
-  <Editor
-    {...props}
-    class={classNames(styles.main, props.class)}
-    onChange={(event) => props.onChange(event.target.value)}
-    onHighlight={(text: string) => text}
-  />
-);
+export const CodeEditor = (props: Props): JSX.Element => {
+  return (
+    <Editor
+      {...props}
+      class={classNames(styles.main, props.class)}
+      onChange={(event) => props.onChange(event.target.value)}
+      onHighlight={(text) =>
+        highlighter.codeToHtml(text, {
+          lang: "scheme",
+          structure: "inline",
+          theme: "nord",
+        })
+      }
+    />
+  );
+};
