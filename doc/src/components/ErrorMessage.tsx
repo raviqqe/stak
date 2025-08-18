e0f5dfac3a0a97ec063215d6d44dde236d2b866f
Yota Toyama <raviqqe@gmail.com>
2025-08-18 00:28:20 -0700

    fix

--- doc/src/components/ErrorMessage.tsx
+++ doc/src/components/ErrorMessage.tsx
@@ -1,6 +1,6 @@
 import type { FunctionComponent } from "preact";
 import styles from "./ErrorMessage.module.css";
 
-export const ErrorMessage: FunctionComponent = (props) => (
-  <>{props.children && <p class={styles.root}>{props.children}</p>}</>
+export const ErrorMessage: FunctionComponent = ({ children }) => (
+  <>{children && <p class={styles.root}>{children}</p>}</>
 );
