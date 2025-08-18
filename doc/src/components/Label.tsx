e858baefa5bfcccb6f566e11d57c0602d5048b5e
Yota Toyama <raviqqe@gmail.com>
2025-08-18 00:32:36 -0700

    Fix

--- doc/src/components/Label.tsx
+++ doc/src/components/Label.tsx
@@ -1,8 +1,11 @@
 import type { FunctionComponent } from "preact";
 import styles from "./Label.module.css";
 
-export const Label: FunctionComponent<{ for: string }> = (props) => (
+export const Label: FunctionComponent<{ for: string }> = ({
+  children,
+  ...props
+}) => (
   <label class={styles.root} for={props.for}>
-    {props.children}
+    {children}
   </label>
 );
--- doc/src/components/Link.tsx
+++ doc/src/components/Link.tsx
@@ -1,8 +1,11 @@
 import type { FunctionComponent } from "preact";
 import styles from "./Link.module.css";
 
-export const Link: FunctionComponent<{ href: string }> = (props) => (
-  <a class={styles.root} href={props.href} rel="noreferrer" target="_blank">
-    {props.children}
+export const Link: FunctionComponent<{ href: string }> = ({
+  children,
+  href,
+}) => (
+  <a class={styles.root} href={href} rel="noreferrer" target="_blank">
+    {children}
   </a>
 );
