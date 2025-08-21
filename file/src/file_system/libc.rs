1a1a3c54f3691dda2b4e2781a3fdad95397e3105
Yota Toyama <raviqqe@gmail.com>
2025-08-21 07:35:41 -0700

    Fix build

--- file/src/file_system/libc.rs
+++ file/src/file_system/libc.rs
@@ -1,6 +1,6 @@
 use super::{FileDescriptor, FileError, FileSystem, utility::decode_path};
 use core::ffi::CStr;
-use heapless::{FnvIndexMap, Vec};
+use heapless::{IndexMap, Vec};
 use rustix::{
     fd::{AsFd, BorrowedFd, OwnedFd},
     fs::{self, Access, Mode, OFlags},
@@ -30,7 +30,7 @@ impl AsRef<CStr> for CString {
 #[derive(Debug, Default)]
 pub struct LibcFileSystem<const N: usize = DEFAULT_FILE_CAPACITY> {
     descriptor: FileDescriptor,
-    files: FnvIndexMap<FileDescriptor, OwnedFd, N>,
+    files: IndexMap<FileDescriptor, OwnedFd, N>,
 }
 
 impl LibcFileSystem {
