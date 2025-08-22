5b1f68b5dc2b71ffddf6ab93ca4c67d0174753a0
Yota Toyama <raviqqe@gmail.com>
2025-08-21 18:35:54 -0700

    fix

--- tools/binary_size_bench.sh
+++ tools/binary_size_bench.sh
@@ -37,7 +37,7 @@ build_chibi() (
   git_clone https://github.com/ashinn/chibi-scheme
   cd chibi-scheme
   # spell-checker: disable-next-line
-  make DSEXP_USE_NO_FEATURES=1 chibi-scheme-static
+  make SEXP_USE_NO_FEATURES=1 chibi-scheme-static
 )
 
 build_stak() (
