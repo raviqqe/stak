0edb655b37206607108b0417e8ced79682659652
Yota Toyama <raviqqe@gmail.com>
2025-08-31 16:39:44 -0700

    fix

--- tools/build_wasm.sh
+++ tools/build_wasm.sh
@@ -6,9 +6,8 @@ cargo install wasm-pack
 
 cd wasm
 
-cargo build
-wasm-pack build --release --target web -- --features async
+wasm-pack build --target web -- --features async
 jq '. + input' pkg/package.json package.json >/tmp/package.json
 mv /tmp/package.json pkg
 
-wasm-pack pack wasm
+wasm-pack pack
