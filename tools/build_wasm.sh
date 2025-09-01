27a0dd2d004f62ec1455478317e29df72dab0212
Yota Toyama <raviqqe@gmail.com>
2025-08-31 17:51:29 -0700

    Fix wasm release (#2855)

---
+++ tools/build_wasm.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+cargo install wasm-pack
+
+cd wasm
+
+wasm-pack build --target web -- --features async
+jq '. + input' pkg/package.json package.template.json >/tmp/package.json
+mv /tmp/package.json pkg
+
+wasm-pack pack
