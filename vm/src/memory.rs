c683c9aead76b11a9148ca7c36bf67b35c113ad8
Yota Toyama <raviqqe@gmail.com>
2025-09-05 20:01:17 -0700

    Fix

--- vm/src/memory.rs
+++ vm/src/memory.rs
@@ -568,7 +568,8 @@ pub trait Heap: AsRef<[Value]> + AsMut<[Value]> {}
 
 impl Heap for &mut [Value] {}
 
-impl<const N: usize> Heap for [Value; N] {}
+// TODO
+// impl<const N: usize> Heap for [Value; N] {}
 
 #[cfg(feature = "alloc")]
 impl Heap for alloc::vec::Vec<Value> {}
