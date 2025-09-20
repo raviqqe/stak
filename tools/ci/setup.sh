5a94014ef45263cb2cef4a7d8294dd316c517780
Yota Toyama <raviqqe@gmail.com>
2025-09-19 23:22:31 -0700

    Refactor Go command setup

--- tools/ci/setup.sh
+++ tools/ci/setup.sh
@@ -4,12 +4,12 @@ set -e
 
 brew install lua@5.4 pkgconf uutils-coreutils uutils-findutils
 cargo install stak
-# Download the `agoa` command by running it.
+
+# Download Go commands by running it.
 # https://github.com/actions/setup-go/issues/358
-go tool agoa --version
-go tool gherkin-format -version
-go tool gherkin2markdown --version
-go tool muffet --version
+for command in agoa gherkin-format gherkin2markdown muffet; do
+  go tool agoa --version
+done
 
 echo LD_LIBRARY_PATH=$(brew --prefix lua@5.4)/lib:$LD_LIBRARY_PATH >>$GITHUB_ENV
 
