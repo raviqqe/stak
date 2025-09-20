4b275c7ae6fa903ef5948f4eda3e4326f1094059
Yota Toyama <raviqqe@gmail.com>
2025-09-20 14:47:21 -0700

    Fix

--- tools/ci/setup.sh
+++ tools/ci/setup.sh
@@ -8,7 +8,7 @@ cargo install stak
 # Download Go commands by running it.
 # https://github.com/actions/setup-go/issues/358
 for command in agoa gherkin-format gherkin2markdown muffet; do
-  go tool agoa --version
+  go tool $command --version
 done
 
 echo LD_LIBRARY_PATH=$(brew --prefix lua@5.4)/lib:$LD_LIBRARY_PATH >>$GITHUB_ENV
