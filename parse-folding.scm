5ca5702ce7dc4dcd05d9da800f7bed99b389b1e8
Yota Toyama <raviqqe@gmail.com>
2025-10-01 18:56:52 -0700

    Fix

--- parse-folding.scm
+++ parse-folding.scm
@@ -8,8 +8,12 @@
     (lambda (line) line)
     (let loop ()
       (let ((line (read-line)))
-        (if (eof-object? line)
-          '()
-          (cons line (loop)))))))
+        (cond
+          ((eof-object? line)
+            '())
+          ((or (equal? line "") (eqv? (string-ref line 0) #\#))
+            (loop))
+          (else
+            (cons line (loop))))))))
 
 (write (read-all))
