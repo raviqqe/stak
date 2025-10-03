a900cae57a19d1f94d4b1d7249fd2d426ee71108
Yota Toyama <raviqqe@gmail.com>
2025-10-02 19:52:26 -0700

    Fix

--- tools/build_unicode.sh
+++ tools/build_unicode.sh
@@ -7,4 +7,4 @@ cd $(dirname $0)/..
 mkdir -p tmp
 
 curl -fsSL https://raw.githubusercontent.com/unicode-org/icu/24ebdccde39af10d8564fb7d50ad7f11a26e86b9/icu4c/source/data/unidata/CaseFolding.txt >tmp/folding.txt
-stak parse-folding.scm <tmp/folding.txt >tmp/folding.scm
+gosh parse-folding.scm <tmp/folding.txt >tmp/folding.scm
