2e80ea65b5e6212c4375755b65216aa836160709
Yota Toyama <raviqqe@gmail.com>
2025-10-03 08:50:07 -0700

    Fix

--- tools/build_unicode.sh
+++ tools/build_unicode.sh
@@ -6,5 +6,9 @@ cd $(dirname $0)/..
 
 mkdir -p tmp
 
+cargo build --profile release_test
+
+export PATH=$PWD/target/release_test:$PATH
+
 curl -fsSL https://raw.githubusercontent.com/unicode-org/icu/24ebdccde39af10d8564fb7d50ad7f11a26e86b9/icu4c/source/data/unidata/CaseFolding.txt >tmp/folding.txt
 gosh parse-folding.scm <tmp/folding.txt >tmp/folding.scm
