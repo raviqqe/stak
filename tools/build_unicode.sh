a31441afc693444f22131b043787766b003579e6
Yota Toyama <raviqqe@gmail.com>
2025-10-03 09:01:05 -0700

    Fix

--- tools/build_unicode.sh
+++ tools/build_unicode.sh
@@ -11,4 +11,4 @@ cargo build --profile release_test
 export PATH=$PWD/target/release_test:$PATH
 
 curl -fsSL https://raw.githubusercontent.com/unicode-org/icu/24ebdccde39af10d8564fb7d50ad7f11a26e86b9/icu4c/source/data/unidata/CaseFolding.txt >tmp/folding.txt
-gosh parse-folding.scm <tmp/folding.txt >tmp/folding.scm
+stak parse-folding.scm <tmp/folding.txt >tmp/folding.scm
