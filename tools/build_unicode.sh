97588e1e8bdaf8d60815696aeb49de58fbd5521c
Yota Toyama <raviqqe@gmail.com>
2025-10-05 23:36:33 -0700

    Fix

--- tools/build_unicode.sh
+++ tools/build_unicode.sh
@@ -2,6 +2,12 @@
 
 set -ex
 
+version=release-77-1
+
+fetch_data() {
+  curl -fsSL https://raw.githubusercontent.com/unicode-org/icu/$version/icu4c/source/data/unidata/$1.txt
+}
+
 cd $(dirname $0)/..
 
 mkdir -p tmp
@@ -10,5 +16,8 @@ cargo build --profile release_test
 
 export PATH=$PWD/target/release_test:$PATH
 
-curl -fsSL https://raw.githubusercontent.com/unicode-org/icu/24ebdccde39af10d8564fb7d50ad7f11a26e86b9/icu4c/source/data/unidata/CaseFolding.txt >tmp/folding.txt
-stak parse-folding.scm <tmp/folding.txt >tmp/folding.scm
+for base in CaseFolding UnicodeData; do
+  fetch_data $base >tmp/$base.txt
+done
+
+stak parse-unicode.scm fold <tmp/CaseFolding.txt >tmp/fold.scm
