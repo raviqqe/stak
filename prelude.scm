82a79520128c88376cddf9d0673ccca8d179b290
Yota Toyama <raviqqe@gmail.com>
2025-10-27 21:13:58 -0700

    Fix

--- prelude.scm
+++ prelude.scm
@@ -4006,7 +4006,23 @@
     (set! space-table
       (append
         space-table
-        '()))))
+        '(5760
+          8192
+          8193
+          8194
+          8195
+          8196
+          8197
+          8198
+          8199
+          8200
+          8201
+          8202
+          8232
+          8233
+          8239
+          8287
+          12288)))))
 
 (define-library (scheme char)
   (export
