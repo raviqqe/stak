00013995198f7aeb1a5548b4910b13216d1fb043
Yota Toyama <raviqqe@gmail.com>
2025-10-29 06:36:05 -0700

    Fix

--- compile-unicode.scm
+++ compile-unicode.scm
@@ -109,7 +109,7 @@
       (parse-case-records
         (read-case-records column)))))
 
-(define (compile-case-table column)
+(define (compile-unmapped-case-table column)
   (group-records
     (differentiate-records
       (parse-case-records
