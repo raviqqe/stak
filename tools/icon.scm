78968968bf00ff75eb34f4a2a7008b416a6ba81d
Yota Toyama <raviqqe@gmail.com>
2025-11-07 20:22:55 -0800

    Fix

---
+++ tools/icon.scm
@@ -0,0 +1,15 @@
+(display
+  (render
+    ((svg ((view-box "0 0 20 20") (xmlns "http://www.w3.org/2000/svg"))
+        (style
+          (text
+            (dominant-baseline "central")
+            (font-size: "18px")
+            (text-anchor "middle")
+            (padding-top "2px")))
+        (text ((x "50%") (y "50%")) λ)
+        (text ((x "50%") (y "50%")) 𝛌)
+        (text ((x "50%") (y "50%")) 𝜆)
+        (text ((x "50%") (y "50%")) 𝝀)
+        (text ((x "50%") (y "50%")) 𝝺)
+        (text ((x "50%") (y "50%")) 𝞴)))))
