de265431729fae5d63daa5fd4a3bc9fbe1dcf322
Yota Toyama <raviqqe@gmail.com>
2025-11-08 17:18:54 -0800

    Update icon

--- icon.scm
+++ icon.scm
@@ -70,6 +70,7 @@
         (font-size "20px")
         (font-weight "bold")
         (text-anchor "middle")
+        (text-shadow "1rem 1rem 1rem rgba(0, 0, 0, 0.2)")
         (transform-box "fill-box")
         (transform-origin "center")))
       ,@(let loop ((chars lambda-chars) (index 0))
