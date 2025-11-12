aaca6e8a74e4b3d37cad5cae510d1367d20fb35a
Yota Toyama <raviqqe@gmail.com>
2025-11-11 19:37:52 -0800

    Fix

--- lzss/src/lib.rs
+++ lzss/src/lib.rs
@@ -73,8 +73,10 @@ impl<const W: usize, I: Iterator<Item = u8>> Iterator for LzssDecompressionItera
     fn next(&mut self) -> Option<Self::Item> {
         if self.length > 0 {
             let x = self.buffer.get(self.offset as _)?;
+
             self.buffer.push(x);
             self.length -= 1;
+
             Some(x)
         } else {
             let x = self.iterator.next()?;
