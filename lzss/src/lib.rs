36f9c70d3145e0bf2fc2cddd1ea75c556c9dbe5b
Yota Toyama <raviqqe@gmail.com>
2025-11-14 20:31:57 -0800

    fix

--- lzss/src/lib.rs
+++ lzss/src/lib.rs
@@ -17,19 +17,21 @@ const MINIMUM_LENGTH: usize = 2;
 const MAXIMUM_LENGTH: usize = u8::MAX as _;
 
 /// LZSS compression iterator.
-pub struct LzssCompressionIterator<const W: usize, I: Iterator<Item = u8>> {
+pub struct LzssCompressionIterator<const B: usize, I: Iterator<Item = u8>> {
     iterator: I,
     // TODO Specify a separate buffer size for look-ahead?
-    buffer: RingBuffer<W>,
+    buffer: RingBuffer<B>,
     ahead: usize,
     next: Option<u8>,
 }
 
-impl<const W: usize, I: Iterator<Item = u8>> LzssCompressionIterator<W, I> {
+impl<const B: usize, I: Iterator<Item = u8>> LzssCompressionIterator<B, I> {
+    const WINDOW_SIZE: usize = B - MAXIMUM_LENGTH;
+
     fn new(iterator: I) -> Self {
         Self {
             iterator,
-            buffer: RingBuffer::<W>::default(),
+            buffer: Default::default(),
             ahead: Default::default(),
             next: Default::default(),
         }
@@ -37,7 +39,7 @@ impl<const W: usize, I: Iterator<Item = u8>> LzssCompressionIterator<W, I> {
 
     fn next(&mut self) -> Option<u8> {
         if self.ahead > 0 {
-            let x = self.buffer.get(W - self.ahead);
+            let x = self.buffer.get(B - self.ahead);
             self.ahead -= 1;
             x
         } else if let Some(x) = self.iterator.next() {
@@ -50,7 +52,7 @@ impl<const W: usize, I: Iterator<Item = u8>> LzssCompressionIterator<W, I> {
 
     fn peek(&mut self, index: usize) -> Option<u8> {
         if index < self.ahead {
-            return self.buffer.get(W - self.ahead + index);
+            return self.buffer.get(B - self.ahead + index);
         }
 
         let mut x = 0;
@@ -65,7 +67,7 @@ impl<const W: usize, I: Iterator<Item = u8>> LzssCompressionIterator<W, I> {
     }
 }
 
-impl<const W: usize, I: Iterator<Item = u8>> Iterator for LzssCompressionIterator<W, I> {
+impl<const B: usize, I: Iterator<Item = u8>> Iterator for LzssCompressionIterator<B, I> {
     type Item = u8;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -73,15 +75,24 @@ impl<const W: usize, I: Iterator<Item = u8>> Iterator for LzssCompressionIterato
             self.next = None;
             Some(x)
         } else {
-            let x = self.next()?;
-
             // TODO Prevent reading uninitialized bytes in a buffer?
-            let (n, m) = (0..W)
+            let (n, m) = (0..Self::WINDOW_SIZE)
                 .map(|i| {
                     let mut j = 0;
 
-                    // TODO `self.buffer.get(W - self.ahead - 1 - i + j)`?
-                    while j < MAXIMUM_LENGTH && self.buffer.get(W - i + j) == self.peek(j) {
+                    dbg!(
+                        B,
+                        self.ahead,
+                        1,
+                        i,
+                        j,
+                        self.buffer.get(2 * B - self.ahead - 1 - i + j),
+                        self.peek(j)
+                    );
+
+                    while j < MAXIMUM_LENGTH
+                        && self.buffer.get(2 * B - self.ahead - 1 - i + j) == self.peek(j)
+                    {
                         j += 1;
                     }
 
@@ -95,7 +106,7 @@ impl<const W: usize, I: Iterator<Item = u8>> Iterator for LzssCompressionIterato
 
                 Some((n as u8) << 1 | 1)
             } else {
-                Some(x << 1)
+                Some(self.next()? << 1)
             }
         }
     }
@@ -175,7 +186,8 @@ mod tests {
     use alloc::vec::Vec;
     use pretty_assertions::assert_eq;
 
-    const WINDOW_SIZE: usize = 64;
+    const WINDOW_SIZE: usize = 8;
+    const BUFFER_SIZE: usize = WINDOW_SIZE + MAXIMUM_LENGTH;
 
     #[test]
     fn compress_and_decompress() {
@@ -184,7 +196,7 @@ mod tests {
         assert_eq!(
             data.iter()
                 .copied()
-                .compress::<{ WINDOW_SIZE + MAXIMUM_LENGTH }>()
+                .compress::<BUFFER_SIZE>()
                 .decompress::<WINDOW_SIZE>()
                 .collect::<Vec<u8>>(),
             data
@@ -197,7 +209,8 @@ mod tests {
 
         #[test]
         fn next() {
-            let mut iterator = LzssCompressionIterator::<64, _>::new([1, 2, 3].into_iter());
+            let mut iterator =
+                LzssCompressionIterator::<BUFFER_SIZE, _>::new([1, 2, 3].into_iter());
 
             assert_eq!(iterator.next(), Some(1));
             assert_eq!(iterator.next(), Some(2));
@@ -208,7 +221,7 @@ mod tests {
         #[test]
         fn peek() {
             let mut iterator =
-                LzssCompressionIterator::<64, _>::new([1, 2, 3, 4, 5, 6].into_iter());
+                LzssCompressionIterator::<BUFFER_SIZE, _>::new([1, 2, 3, 4, 5, 6].into_iter());
 
             assert_eq!(iterator.peek(0), Some(1));
 
@@ -230,7 +243,7 @@ mod tests {
             assert_eq!(
                 [1].iter()
                     .copied()
-                    .compress::<WINDOW_SIZE>()
+                    .compress::<BUFFER_SIZE>()
                     .collect::<Vec<_>>(),
                 [2]
             );
@@ -242,7 +255,7 @@ mod tests {
                 [1, 2]
                     .iter()
                     .copied()
-                    .compress::<WINDOW_SIZE>()
+                    .compress::<BUFFER_SIZE>()
                     .collect::<Vec<_>>(),
                 [2, 4]
             );
@@ -254,7 +267,7 @@ mod tests {
                 [1, 2, 3]
                     .iter()
                     .copied()
-                    .compress::<WINDOW_SIZE>()
+                    .compress::<BUFFER_SIZE>()
                     .collect::<Vec<_>>(),
                 [2, 4, 6]
             );
@@ -266,7 +279,7 @@ mod tests {
                 [42, 42, 42]
                     .iter()
                     .copied()
-                    .compress::<WINDOW_SIZE>()
+                    .compress::<BUFFER_SIZE>()
                     .collect::<Vec<_>>(),
                 [42, 0, 2]
             );
@@ -278,7 +291,7 @@ mod tests {
                 [42, 42, 42, 42, 7, 7, 7]
                     .iter()
                     .copied()
-                    .compress::<WINDOW_SIZE>()
+                    .compress::<BUFFER_SIZE>()
                     .collect::<Vec<_>>(),
                 [42, 0, 3, 7, 0, 2]
             );
