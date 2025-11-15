32e741a9ddac635bd22f4aa10836f9dffbcc026d
Yota Toyama <raviqqe@gmail.com>
2025-11-15 14:35:01 -0800

    Fix

--- lzss/src/main.rs
+++ lzss/src/main.rs
@@ -4,9 +4,45 @@ use stak_lzss::{Lzss, MAX_LENGTH};
 use std::error::Error;
 use std::io::{Read, Write, stdin, stdout};
 
-const WINDOW_SIZE: usize = 256;
+const WINDOW_SIZE: usize = 127;
+
+#[derive(clap::Parser)]
+#[command(about, version)]
+struct Arguments {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(clap::Subcommand)]
+enum Command {
+    /// Compresses data.
+    Compress,
+    /// Decompresses data.
+    Decompress,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
+    match Arguments::parse() {
+        Command::Compress => compress(),
+        Command::Decompress => decompress(),
+    }
+}
+
+fn compress() -> Result<(), Box<dyn Error>> {
+    let mut data = vec![];
+
+    stdin().read_to_end(&mut data)?;
+    stdout().write(
+        &data
+            .into_iter()
+            .compress::<{ WINDOW_SIZE + MAX_LENGTH }>()
+            .collect::<Vec<_>>(),
+    )?;
+
+    Ok(())
+}
+
+fn decompress() -> Result<(), Box<dyn Error>> {
     let mut data = vec![];
 
     stdin().read_to_end(&mut data)?;
