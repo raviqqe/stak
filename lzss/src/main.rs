1232a50292c86b7b5ad0c66cd94c32dec69d32f5
Yota Toyama <raviqqe@gmail.com>
2025-11-15 14:24:22 -0800

    Fix

--- lzss/src/main.rs
+++ lzss/src/main.rs
@@ -1,7 +1,21 @@
-use stak_lzss::Lzss;
-use std::io::{Write, stdin, stdout};
-use std::error::Error;
+//! An LZSS compressor for ASCII characters.
 
-fn main() -> Result<(), Box<dyn Error>>{
-    stdout().write(stdin().read()?.compress())
+use stak_lzss::{Lzss, MAX_LENGTH};
+use std::error::Error;
+use std::io::{Read, Write, stdin, stdout};
+
+const WINDOW_SIZE: usize = 256;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut data = vec![];
+
+    stdin().read_to_end(&mut data)?;
+    stdout().write(
+        &data
+            .into_iter()
+            .compress::<{ WINDOW_SIZE + MAX_LENGTH }>()
+            .collect::<Vec<_>>(),
+    )?;
+
+    Ok(())
 }
