28930b8c90e568bf2514a0dc82601151d5886403
Yota Toyama <raviqqe@gmail.com>
2025-11-15 14:16:54 -0800

    fix

--- lzss/src/main.rs
+++ lzss/src/main.rs
@@ -1,6 +1,7 @@
 use stak_lzss::Lzss;
 use std::io::{Write, stdin, stdout};
+use std::error::Error;
 
-fn main() {
-    stdout().write(stdin().compress())
+fn main() -> Result<(), Box<dyn Error>>{
+    stdout().write(stdin().read()?.compress())
 }
