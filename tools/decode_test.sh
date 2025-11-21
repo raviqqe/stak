f45ff11c900759bc6b523c729aca67088ba6bead
Yota Toyama <raviqqe@gmail.com>
2025-11-20 20:33:17 -0800

    Fix

--- tools/decode_test.sh
+++ tools/decode_test.sh
@@ -12,10 +12,15 @@ export PATH=$PWD/target/release:$PATH
 
 for file in $(list_scheme_files); do
   echo FILE $file
-  snapshot_file=snapshots/${file%.scm}.md
-  mkdir -p $(dirname $snapshot_file)
-  cat prelude.scm $file | stak-compile --shake-tree >main.bc
-  stak-decode <main.bc >$snapshot_file
+
+  base=snapshots/${file%.scm}
+
+  mkdir -p $(dirname $base)
+  cat prelude.scm $file |
+    stak-compile --shake-tree |
+    stak-lzss compress |
+    stak-lzss decompress >$base.bc
+  stak-decode <$base.bc >$base.md
 done
 
 npx prettier --write snapshots
