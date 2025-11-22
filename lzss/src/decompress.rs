0945071a86b7a94a5401f6e1d94943b2c55dcdaa
Yota Toyama <raviqqe@gmail.com>
2025-11-22 13:08:46 -0800

    fxi

--- lzss/src/decompress.rs
+++ lzss/src/decompress.rs
@@ -1,11 +1,5 @@
 use crate::ring_buffer::RingBuffer;
 
-const MIN_LENGTH: usize = 2;
-/// The maximum match length.
-pub const MAX_LENGTH: usize = u8::MAX as _;
-/// The default window size.
-pub const DEFAULT_WINDOW_SIZE: usize = (1 << 7) - 1;
-
 /// LZSS decompression iterator.
 pub struct LzssDecompressionIterator<const W: usize, I: Iterator<Item = u8>> {
     iterator: I,
@@ -15,7 +9,7 @@ pub struct LzssDecompressionIterator<const W: usize, I: Iterator<Item = u8>> {
 }
 
 impl<const W: usize, I: Iterator<Item = u8>> LzssDecompressionIterator<W, I> {
-    fn new(iterator: I) -> Self {
+    pub fn new(iterator: I) -> Self {
         Self {
             iterator,
             buffer: Default::default(),
@@ -59,12 +53,12 @@ mod tests {
     use alloc::vec::Vec;
     use core::iter::repeat;
     use pretty_assertions::assert_eq;
-    use quickcheck_macros::quickcheck;
 
     #[test]
     fn repetition() {
         assert_eq!(
-            [2, 4, 6, 8, 7, 5].decompress::<8>().collect::<Vec<_>>(),
+            LzssDecompressionIterator::<8, _>::new([2, 4, 6, 8, 7, 5].into_iter())
+                .collect::<Vec<_>>(),
             [1, 2, 3, 4, 1, 2, 3, 4, 1]
         );
     }
@@ -72,8 +66,7 @@ mod tests {
     #[test]
     fn repetitions() {
         assert_eq!(
-            [2, 4, 6, 8, 7, 5, 10, 12, 3, 3]
-                .decompress::<8>()
+            LzssDecompressionIterator::<8, _>::new([2, 4, 6, 8, 7, 5, 10, 12, 3, 3].into_iter())
                 .collect::<Vec<_>>(),
             [1, 2, 3, 4, 1, 2, 3, 4, 1, 5, 6, 5, 6, 5]
         );
@@ -82,7 +75,7 @@ mod tests {
     #[test]
     fn max_length() {
         assert_eq!(
-            [84, 1, 255].decompress::<1>().collect::<Vec<_>>(),
+            LzssDecompressionIterator::<1, _>::new([84, 1, 255].into_iter()).collect::<Vec<_>>(),
             repeat(42).take(256).collect::<Vec<_>>()
         );
     }
@@ -90,10 +83,7 @@ mod tests {
     #[test]
     fn max_offset() {
         assert_eq!(
-            (0..128)
-                .map(|x| x << 1)
-                .chain([255, 128])
-                .decompress::<128>()
+            LzssDecompressionIterator::<128, _>::new((0..128).map(|x| x << 1).chain([255, 128]))
                 .collect::<Vec<_>>(),
             (0..128).chain(0..128).collect::<Vec<_>>()
         );
