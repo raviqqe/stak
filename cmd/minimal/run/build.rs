773a42580c352e0db11dfff3efcdd13e795083fd
Yota Toyama <raviqqe@gmail.com>
2025-11-30 13:35:17 -0800

    Fix

--- cmd/minimal/run/build.rs
+++
@@ -1,9 +0,0 @@
-//! A build script.
-
-use std::env::var;
-
-fn main() {
-    if var("CARGO_CFG_TARGET_ENV").ok().as_deref() == Some("musl") {
-        println!("cargo::rustc-link-lib=c");
-    }
-}
