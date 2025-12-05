db79792f881ad535cba03f14a1334464dc3ca460
Yota Toyama <raviqqe@gmail.com>
2025-12-04 20:55:29 -0800

    Fix

--- tools/format-metrics.scm
+++ tools/format-metrics.scm
@@ -1,6 +1,8 @@
 (import
   (scheme base)
+  (scheme cxr)
   (scheme write)
+  (scheme process-context)
   (srfi 13)
   (srfi 180))
 
@@ -13,4 +15,24 @@
           (cons (car tokens) (cadr tokens)))
         (read-metrics)))))
 
-(json-write (read-metrics))
+(define arguments (command-line))
+
+(json-write
+  `((key . ,(cadr arguments))
+    (name . ,(caddr arguments))
+    (metrics . ,(list->vector
+                 (map
+                   (lambda (pair)
+                     `((key . ,(car pair))
+                       (name . ,(string-map
+                                 (lambda (char)
+                                   (case char
+                                     ((#\. #\-)
+                                       #\_)
+                                     (else
+                                       char)))
+                                 (car pair)))
+                       (value . ,(string->number (cdr pair)))
+                       (unit . "bytes")))
+                   (read-metrics))))
+    (acceptables . #())))
