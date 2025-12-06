db922ea59b28d92018fd72ad230c8b8a52c43199
Yota Toyama <raviqqe@gmail.com>
2025-12-06 11:23:27 -0800

    Fix

--- prelude.scm
+++ prelude.scm
@@ -2296,7 +2296,7 @@
     (define dummy-procedure (lambda () #f))
 
     (define (call/cc receiver)
-      (let ((continuation (cadr (cdr (close dummy-procedure))))
+      (let ((continuation (car (cddr (close dummy-procedure))))
             (point current-point))
         (receiver
           (lambda (argument)
