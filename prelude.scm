adadf7cc32d9c56730668a6fc3ff5e43eb9c8940
Yota Toyama <raviqqe@gmail.com>
2025-12-06 19:08:28 -0800

    Fix

--- prelude.scm
+++ prelude.scm
@@ -2556,7 +2556,7 @@
 
     (set! backtrace
       (lambda ()
-        (cdr
+        (cddr
           (let loop ((stack (cdr (close (lambda () #f)))))
             (cond
               ((null? stack)
