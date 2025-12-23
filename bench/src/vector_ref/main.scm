7ece138a4d09e65246abe4fab4bcd76dc7b13ef8
Yota Toyama <raviqqe@gmail.com>
2025-12-22 19:17:12 -0800

    Fix

--- bench/src/vector_ref/main.scm
+++ bench/src/vector_ref/main.scm
@@ -2,5 +2,14 @@
 
 (define count 4096)
 
-(for-each
-  (write (vector-ref foo)))
+(define xs #())
+
+(define xs
+  (do ((index 0 (+ index 1))
+       (xs #() (vector-append xs index)))
+    ((= index count)
+      xs)))
+
+(do ((index 0 (+ index 1)))
+  ((= index count))
+  (write (vector-ref index)))
