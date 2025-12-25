bb00cea51caf68ecf8b9d784eccb11991c001070
Yota Toyama <raviqqe@gmail.com>
2025-12-24 16:52:11 -0800

    Fix

--- vm/src/value_inner/float62.rs
+++ vm/src/value_inner/float62.rs
@@ -75,11 +75,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn calculate_power() {
+    fn zero_power() {
         assert_eq!(to_f64(power(from_i64(2), from_i64(0))), 1.0);
         assert_eq!(to_f64(power(from_f64(2.0), from_i64(0))), 1.0);
         assert_eq!(to_f64(power(from_f64(2.0), from_f64(0.0))), 1.0);
+    }
 
+    #[test]
+    fn non_zero_power() {
         assert_eq!(to_f64(power(from_f64(2.0), from_f64(3.0))), 8.0);
         assert_eq!(to_f64(power(from_f64(3.0), from_f64(5.0))), 243.0);
     }
