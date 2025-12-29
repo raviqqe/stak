a3fc7fa2204d4281d36581fe5c4ebae1ae47f3f5
Yota Toyama <raviqqe@gmail.com>
2025-12-29 07:47:35 -0800

    Parallelize self-host test (#3275)

--- tools/self_host_test.sh
+++ tools/self_host_test.sh
@@ -8,40 +8,45 @@ stage_count=3
 
 profile=release_test
 target=target/$profile
+directory=tmp/self_host
 
-run_stage() (
+compile() (
   if [ $1 -eq 0 ]; then
     log stak compile.scm
   else
-    log $target/stak-interpret stage$1.bc
+    log $target/stak-interpret $directory/stage$1.bc
   fi
-)
-
-artifact_path() (
-  echo tmp/stage$1.$2
 )
 
 cd $(dirname $0)/..
 
-mkdir -p tmp
+mkdir -p $directory
 cargo build --profile $profile
 
 for stage in $(seq 0 $(expr $stage_count - 1)); do
-  cat prelude.scm compile.scm | run_stage $stage >stage$(expr $stage + 1).bc
+  cat prelude.scm compile.scm | compile $stage >$directory/stage$(expr $stage + 1).bc
 done
 
-for file in $(list_scheme_files); do
+test_file() (
+  file=$1
+
   echo FILE $file
 
-  for stage in $(seq 0 $stage_count); do
-    bytecode_file=$(artifact_path $stage bc)
+  sub_directory=$directory/${file%.*}
 
-    cat prelude.scm $file | run_stage $stage >$bytecode_file
+  mkdir -p $sub_directory
+
+  bytecode_file() (
+    echo $sub_directory/stage$1.bc
+  )
+
+  for stage in $(seq 0 $stage_count); do
+    cat prelude.scm $file | compile $stage >$(bytecode_file $stage)
   done
 
   for stage in $(seq 0 $(expr $stage_count - 1)); do
-    for extension in bc; do
-      log diff $(artifact_path $stage $extension) $(artifact_path $(expr $stage + 1) $extension)
-    done
+    log diff $(bytecode_file $stage) $(bytecode_file $(expr $stage + 1))
   done
-done
+)
+
+list_scheme_files | parallel test_file
