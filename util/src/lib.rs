4eaf405a4f057b2310769b61c9446ae68bd0fde3
Yota Toyama <raviqqe@gmail.com>
2026-05-28 20:18:29 -0700

    Fix

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -8,8 +8,8 @@ pub mod __private {
     pub use winter_maybe_async::maybe_await;
 }
 
-/// Blocks on a value, awaiting it when asynchronous operations are compiled in
-/// by `winter-maybe-async`, or returning it as is otherwise.
+/// Blocks on a future if an `async` feature in on, or returns a given value as
+/// it is otherwise.
 #[macro_export]
 macro_rules! block_on {
     ($value:expr) => {
