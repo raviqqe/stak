4e44d368dfba9e4af0e6b9438b81b13a7717bd9a
Yota Toyama <raviqqe@gmail.com>
2026-07-04 19:14:14 -0700

    Fix

--- vm/src/number.rs
+++ vm/src/number.rs
@@ -237,7 +237,7 @@ mod tests {
                     Ok(Number::from_f64(f64::INFINITY))
                 );
             },
-            not(feature = "float") => {
+            _ => {
                 assert_eq!(
                     Number::from_i64(1).divide(Number::from_i64(0)),
                     Err(Error::DivisionByZero)
