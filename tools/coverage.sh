92e3727080cf4fda5378c392e27ab8f3beac1b78
Yota Toyama <raviqqe@gmail.com>
2026-07-31 20:13:07 -0700

    Refactor coverage script

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-rustup component add llvm-tools-preview
+rustup component add llvm-tools
 
 cargo install --locked cargo-llvm-cov
 cargo llvm-cov --workspace --profile release --lcov --output-path lcov.info
