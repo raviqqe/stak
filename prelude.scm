03d80d1fc62847354074f120867230aa00dc2ae5
Yota Toyama <raviqqe@gmail.com>
2026-08-15 14:55:27 -0700

    Refactor

--- prelude.scm
+++ prelude.scm
@@ -6663,14 +6663,13 @@
 
     (define (open-file output)
       (lambda (path)
-        (let* ((descriptor ($open-file (string->code-points path) output))
-               (close (lambda () ($close-file descriptor))))
-          (if output
-            (make-output-port
-              (lambda (x) ($write-file descriptor x))
-              (lambda () ($flush-file descriptor))
-              close)
-            (make-input-port (lambda () ($read-file descriptor)) close)))))
+        (let ((descriptor ($open-file (string->code-points path) output)))
+          (make-port
+            (lambda () ($read-file descriptor))
+            (lambda (x) ($write-file descriptor x))
+            (lambda () ($flush-file descriptor))
+            (lambda () ($close-file descriptor))
+            '()))))
 
     (define open-input-file (open-file #f))
     (define open-output-file (open-file #t))
