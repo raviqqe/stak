e5d222d0afe21ba066e30370cb63227c69938079
Yota Toyama <raviqqe@gmail.com>
2026-08-16 15:10:29 -0700

    Fix

--- prelude.scm
+++ prelude.scm
@@ -2208,12 +2208,12 @@
             (integer->char x))
           (else
             (let loop ((mask 64) (y x))
-              (let ((x (read-u8 port)))
+              (let ((x (peek-u8 port)))
                 (if (or (eof-object? x) (< x 128) (> x 192))
                   (integer->char (remainder y mask))
                   (loop
                     (* mask 32)
-                    (+ (* y 64) (remainder x 64))))))))))
+                    (+ (* y 64) (remainder (read-u8 port) 64))))))))))
 
     (define (peek-char . rest)
       (let* ((port (get-input-port rest))
