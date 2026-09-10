7e25f2308f944854c2dce05d6031398d15586608
Yota Toyama <raviqqe@gmail.com>
2026-09-09 18:54:10 -0700

    Refactor

--- module/src/lib.rs
+++ module/src/lib.rs
@@ -16,39 +16,42 @@ pub use module::Module;
 pub use r#static::*;
 pub use universal::*;
 
-/// Includes bytecode of a R7RS Scheme module built by the
-/// [`stak_build`](https://docs.rs/stak-build) crate.
-///
-/// With the `hot-reload` feature, the module is reloaded whenever its bytecode
-/// file changes.
-///
-/// See the [`stak`](https://docs.rs/stak) crate's documentation for full examples.
-#[cfg(feature = "hot-reload")]
-#[macro_export]
-macro_rules! include_module {
-    ($path:literal) => {{
-        static MODULE: $crate::HotReloadModule =
-            $crate::HotReloadModule::new(concat!(env!("OUT_DIR"), "/src/", $path));
+cfg_select! {
+    feature = "hot-reload" => {
+        /// Includes bytecode of a R7RS Scheme module built by the
+        /// [`stak_build`](https://docs.rs/stak-build) crate.
+        ///
+        /// With the `hot-reload` feature, the module is reloaded whenever its
+        /// bytecode file changes.
+        ///
+        /// See the [`stak`](https://docs.rs/stak) crate's documentation for full
+        /// examples.
+        #[macro_export]
+        macro_rules! include_module {
+            ($path:literal) => {{
+                static MODULE: $crate::HotReloadModule =
+                    $crate::HotReloadModule::new(concat!(env!("OUT_DIR"), "/src/", $path));
 
-        $crate::UniversalModule::HotReload(&MODULE)
-    }};
-}
-
-/// Includes bytecode of a R7RS Scheme module built by the
-/// [`stak_build`](https://docs.rs/stak-build) crate.
-///
-/// With the `hot-reload` feature, the module is reloaded whenever its bytecode
-/// file changes.
-///
-/// See the [`stak`](https://docs.rs/stak) crate's documentation for full examples.
-#[cfg(not(feature = "hot-reload"))]
-#[macro_export]
-macro_rules! include_module {
-    ($path:literal) => {
-        $crate::UniversalModule::Static($crate::StaticModule::new(include_bytes!(concat!(
-            env!("OUT_DIR"),
-            "/src/",
-            $path
-        ))))
-    };
+                $crate::UniversalModule::HotReload(&MODULE)
+            }};
+        }
+    }
+    _ => {
+        /// Includes bytecode of a R7RS Scheme module built by the
+        /// [`stak_build`](https://docs.rs/stak-build) crate.
+        ///
+        /// With the `hot-reload` feature, the module is reloaded whenever its
+        /// bytecode file changes.
+        ///
+        /// See the [`stak`](https://docs.rs/stak) crate's documentation for full
+        /// examples.
+        #[macro_export]
+        macro_rules! include_module {
+            ($path:literal) => {
+                $crate::UniversalModule::Static($crate::StaticModule::new(include_bytes!(
+                    concat!(env!("OUT_DIR"), "/src/", $path)
+                )))
+            };
+        }
+    }
 }
