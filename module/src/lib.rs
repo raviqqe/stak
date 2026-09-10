d1def7cb724dd82a0614be50ea0a8f119347ea5d
Yota Toyama <raviqqe@gmail.com>
2026-09-09 18:58:34 -0700

    Fix

--- module/src/lib.rs
+++ module/src/lib.rs
@@ -16,18 +16,25 @@ pub use module::Module;
 pub use r#static::*;
 pub use universal::*;
 
+/// Includes bytecode of a R7RS Scheme module built by the
+/// [`stak_build`](https://docs.rs/stak-build) crate.
+///
+/// With the `hot-reload` feature, the module is reloaded whenever its bytecode
+/// file changes.
+///
+/// See the [`stak`](https://docs.rs/stak) crate's documentation for full examples.
+#[macro_export]
+macro_rules! include_module {
+    ($path:literal) => {
+        $crate::__include_module!($path)
+    };
+}
+
 cfg_select! {
     feature = "hot-reload" => {
-        /// Includes bytecode of a R7RS Scheme module built by the
-        /// [`stak_build`](https://docs.rs/stak-build) crate.
-        ///
-        /// With the `hot-reload` feature, the module is reloaded whenever its
-        /// bytecode file changes.
-        ///
-        /// See the [`stak`](https://docs.rs/stak) crate's documentation for full
-        /// examples.
+        #[doc(hidden)]
         #[macro_export]
-        macro_rules! include_module {
+        macro_rules! __include_module {
             ($path:literal) => {{
                 static MODULE: $crate::HotReloadModule =
                     $crate::HotReloadModule::new(concat!(env!("OUT_DIR"), "/src/", $path));
@@ -37,16 +44,9 @@ cfg_select! {
         }
     }
     _ => {
-        /// Includes bytecode of a R7RS Scheme module built by the
-        /// [`stak_build`](https://docs.rs/stak-build) crate.
-        ///
-        /// With the `hot-reload` feature, the module is reloaded whenever its
-        /// bytecode file changes.
-        ///
-        /// See the [`stak`](https://docs.rs/stak) crate's documentation for full
-        /// examples.
+        #[doc(hidden)]
         #[macro_export]
-        macro_rules! include_module {
+        macro_rules! __include_module {
             ($path:literal) => {
                 $crate::UniversalModule::Static($crate::StaticModule::new(include_bytes!(
                     concat!(env!("OUT_DIR"), "/src/", $path)
